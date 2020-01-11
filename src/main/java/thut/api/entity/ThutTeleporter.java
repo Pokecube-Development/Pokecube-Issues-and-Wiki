1c8063fa3d2cb284f99f5ecd78d3104fe5ca26ab
Thutmose <elpatricimo@gmail.com>
2020-01-11 14:46:29 -0500

    Fixes mob/player teleporters

--- src/main/java/thut/api/entity/ThutTeleporter.java
+++ src/main/java/thut/api/entity/ThutTeleporter.java
@@ -2,25 +2,102 @@ package thut.api.entity;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.TickEvent.WorldTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import thut.api.maths.Vector4;
 
 public class ThutTeleporter
 {
+    private static class TransferTicker
+    {
+        private final Entity      entity;
+        private final ServerWorld destWorld;
+        private final Vector4     dest;
+
+        public TransferTicker(ServerWorld destWorld, Entity entity, Vector4 dest)
+        {
+            this.entity = entity;
+            this.dest = dest;
+            this.destWorld = destWorld;
+            MinecraftForge.EVENT_BUS.register(this);
+        }
+
+        @SubscribeEvent
+        public void TickEvent(WorldTickEvent event)
+        {
+            if (event.world == entity.getEntityWorld() && event.phase == Phase.END)
+            {
+                MinecraftForge.EVENT_BUS.unregister(this);
+                transferMob(destWorld, dest, entity);
+            }
+        }
+    }
+
     public static void transferTo(Entity entity, Vector4 dest)
     {
         if (entity.getEntityWorld() instanceof ServerWorld)
         {
-            moveMob(entity, dest);
-            if (dest.w == entity.dimension.getId()) { return; }
+            if (dest.w == entity.dimension.getId())
+            {
+                moveMob(entity, dest);
+                return;
+            }
+            ServerWorld destWorld = entity.getServer().getWorld(DimensionType.getById((int) dest.w));
             if (entity instanceof ServerPlayerEntity)
             {
+
                 ServerPlayerEntity player = (ServerPlayerEntity) entity;
-                ServerWorld destWorld = player.server.getWorld(DimensionType.getById((int) dest.w));
                 player.teleport(destWorld, dest.x, dest.y, dest.z, entity.rotationYaw, entity.rotationPitch);
             }
+            else
+            {
+                // Schedule the transfer for end of tick.
+                new TransferTicker(destWorld, entity, dest);
+            }
         }
+    }
+
+    private static void transferMob(ServerWorld destWorld, Vector4 dest, Entity entity)
+    {
+        ServerWorld serverworld = (ServerWorld) entity.getEntityWorld();
+        entity.dimension = destWorld.dimension.getType();
+        removeMob(serverworld, entity, true); // Forge: The player
+                                              // entity itself is moved,
+                                              // and not cloned. So we
+                                              // need to keep the data
+                                              // alive with no matching
+                                              // invalidate call later.
+        entity.revive();
+        entity.setLocationAndAngles(dest.x, dest.y, dest.z, entity.rotationYaw, entity.rotationPitch);
+        entity.setWorld(destWorld);
+        addMob(destWorld, entity);
+    }
+
+    private static void addMob(ServerWorld world, Entity entity)
+    {
+        if (net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(
+                new net.minecraftforge.event.entity.EntityJoinWorldEvent(entity, world))) return;
+        IChunk ichunk = world.getChunk(MathHelper.floor(entity.posX / 16.0D), MathHelper.floor(entity.posZ / 16.0D),
+                ChunkStatus.FULL, true);
+        if (ichunk instanceof Chunk)
+        {
+            ichunk.addEntity(entity);
+        }
+        world.addEntityIfNotDuplicate(entity);
+    }
+
+    private static void removeMob(ServerWorld world, Entity entity, boolean keepData)
+    {
+        entity.remove(keepData);
+        world.removeEntity(entity, keepData);
     }
 
     private static void moveMob(Entity entity, Vector4 dest)
--- src/main/java/thut/core/common/entity/Transporter.java
+++ src/main/java/thut/core/common/entity/Transporter.java
@@ -1,15 +1,16 @@
 package thut.core.common.entity;
 
 import net.minecraft.entity.Entity;
+import thut.api.entity.ThutTeleporter;
 import thut.api.maths.Vector3;
+import thut.api.maths.Vector4;
 
 public class Transporter
 {
 
     public static void teleportEntity(Entity mob, Vector3 loc, int dim, boolean b)
     {
-        // TODO Auto-generated method stub
-
+        ThutTeleporter.transferTo(mob, new Vector4(loc.x, loc.y, loc.z, dim));
     }
 
 }
