82b1d543379786b24fbdaef3b968dafc09a260b2
Thutmose <elpatricimo@gmail.com>
2020-02-15 01:30:20 -0500

    fixes pc and bag search features

--- src/main/java/pokecube/core/client/gui/blocks/PC.java
+++ src/main/java/pokecube/core/client/gui/blocks/PC.java
@@ -3,11 +3,13 @@ package pokecube.core.client.gui.blocks;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
 
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import pokecube.core.PokecubeCore;
@@ -16,6 +18,7 @@ import pokecube.core.inventory.pc.PCContainer;
 import pokecube.core.inventory.pc.PCInventory;
 import pokecube.core.inventory.pc.PCSlot;
 import pokecube.core.network.packets.PacketPC;
+import thut.core.common.ThutCore;
 
 public class PC<T extends PCContainer> extends ContainerScreen<T>
 {
@@ -25,12 +28,12 @@ public class PC<T extends PCContainer> extends ContainerScreen<T>
     TextFieldWidget textFieldBoxName;
     TextFieldWidget textFieldSearch;
 
-    String autoOn  = I18n.format("block.pc.autoon");
-    String autoOff = I18n.format("block.pc.autooff");
+    String          autoOn  = I18n.format("block.pc.autoon");
+    String          autoOff = I18n.format("block.pc.autooff");
 
-    private String boxName = "1";
-    boolean        bound   = false;
-    boolean        release = false;
+    private String  boxName = "1";
+    boolean         bound   = false;
+    boolean         release = false;
 
     public PC(final T container, final PlayerInventory ivplay, final ITextComponent name)
     {
@@ -138,8 +141,8 @@ public class PC<T extends PCContainer> extends ContainerScreen<T>
             this.container.updateInventoryPages((byte) -1, this.minecraft.player.inventory);
             this.textFieldSelectedBox.setText(this.container.getPageNb());
         }));
-        this.textFieldSelectedBox = new TextFieldWidget(this.font, this.width / 2 - xOffset - 70, this.height / 2
-                - yOffset - 121, 25, 10, this.page);
+        this.textFieldSelectedBox = new TextFieldWidget(this.font, this.width / 2 - xOffset - 70,
+                this.height / 2 - yOffset - 121, 25, 10, this.page);
 
         if (!this.bound)
         {
@@ -165,27 +168,27 @@ public class PC<T extends PCContainer> extends ContainerScreen<T>
         {
             if (!this.bound) this.addButton(new Button(this.width / 2 - xOffset - 137, this.height / 2 - yOffset - 85,
                     50, 20, I18n.format("block.pc.option.private"), b ->
-            {
-                // TODO bind.
-                // this.container.pcTile.toggleBound();
-                this.minecraft.player.closeScreen();
-            }));
+                    {
+                        // TODO bind.
+                        // this.container.pcTile.toggleBound();
+                        this.minecraft.player.closeScreen();
+                    }));
             else
             {
-                this.addButton(new Button(this.width / 2 - xOffset - 137, this.height / 2 - yOffset - 125, 50, 20, I18n
-                        .format("block.pc.option.public"), b ->
-                {
-                    // TODO bind.
-                    // this.container.pcTile.toggleBound();
-                    this.minecraft.player.closeScreen();
-                }));
-                this.addButton(new Button(this.width / 2 - xOffset - 137, this.height / 2 - yOffset - 105, 50, 20, I18n
-                        .format("block.pc.option.bind"), b ->
-                {
-                    // TODO bind.
-                    // this.container.pcTile.setBoundOwner(this.minecraft.player);
-                    this.minecraft.player.closeScreen();
-                }));
+                this.addButton(new Button(this.width / 2 - xOffset - 137, this.height / 2 - yOffset - 125, 50, 20,
+                        I18n.format("block.pc.option.public"), b ->
+                        {
+                            // TODO bind.
+                            // this.container.pcTile.toggleBound();
+                            this.minecraft.player.closeScreen();
+                        }));
+                this.addButton(new Button(this.width / 2 - xOffset - 137, this.height / 2 - yOffset - 105, 50, 20,
+                        I18n.format("block.pc.option.bind"), b ->
+                        {
+                            // TODO bind.
+                            // this.container.pcTile.setBoundOwner(this.minecraft.player);
+                            this.minecraft.player.closeScreen();
+                        }));
             }
         }
         else this.addButton(new Button(this.width / 2 - xOffset - 137, this.height / 2 - yOffset - 125, 0, 0, "", b ->
@@ -196,68 +199,68 @@ public class PC<T extends PCContainer> extends ContainerScreen<T>
         }));
         if (!this.bound)
         {
-            this.addButton(new Button(this.width / 2 - xOffset - 81, this.height / 2 - yOffset + 10, 50, 10, I18n
-                    .format("block.pc.option.release"), b ->
-            {
-                this.release = !this.release;
-                if (!this.release && this.container.release)
-                {
-                    this.container.toRelease = new boolean[54];
-                    for (int i = 0; i < 54; i++)
+            this.addButton(new Button(this.width / 2 - xOffset - 81, this.height / 2 - yOffset + 10, 50, 10,
+                    I18n.format("block.pc.option.release"), b ->
                     {
-                        final int index = i;
-                        final PCSlot slot = (PCSlot) this.container.inventorySlots.get(index);
-                        slot.release = false;
-                    }
-                }
-                else for (int i = 0; i < 54; i++)
-                {
-                    final int index = i;
-                    final PCSlot slot = (PCSlot) this.container.inventorySlots.get(index);
-                    slot.release = true;
-                }
-                this.container.release = this.release;
-                final PacketPC packet = new PacketPC(PacketPC.RELEASE, this.container.inv.owner);
-                packet.data.putBoolean("T", true);
-                packet.data.putBoolean("R", this.release);
-                PokecubeCore.packets.sendToServer(packet);
-                if (this.release)
-                {
-                    this.buttons.get(6).active = this.release;
-                    this.buttons.get(6).visible = this.release;
-                }
-                else
-                {
-                    this.buttons.get(6).active = this.release;
-                    this.buttons.get(6).visible = this.release;
-                }
-            }));
-            this.addButton(new Button(this.width / 2 - xOffset - 31, this.height / 2 - yOffset + 10, 50, 10, I18n
-                    .format("block.pc.option.confirm"), b ->
-            {
-                this.release = !this.release;
-                this.container.setRelease(this.release);
-                if (this.release)
-                {
-                    this.buttons.get(6).active = this.release;
-                    this.buttons.get(6).visible = this.release;
-                }
-                else
-                {
-                    this.buttons.get(6).active = this.release;
-                    this.buttons.get(6).visible = this.release;
-                }
-                this.minecraft.player.closeScreen();
-            }));
+                        this.release = !this.release;
+                        if (!this.release && this.container.release)
+                        {
+                            this.container.toRelease = new boolean[54];
+                            for (int i = 0; i < 54; i++)
+                            {
+                                final int index = i;
+                                final PCSlot slot = (PCSlot) this.container.inventorySlots.get(index);
+                                slot.release = false;
+                            }
+                        }
+                        else for (int i = 0; i < 54; i++)
+                        {
+                            final int index = i;
+                            final PCSlot slot = (PCSlot) this.container.inventorySlots.get(index);
+                            slot.release = true;
+                        }
+                        this.container.release = this.release;
+                        final PacketPC packet = new PacketPC(PacketPC.RELEASE, this.container.inv.owner);
+                        packet.data.putBoolean("T", true);
+                        packet.data.putBoolean("R", this.release);
+                        PokecubeCore.packets.sendToServer(packet);
+                        if (this.release)
+                        {
+                            this.buttons.get(6).active = this.release;
+                            this.buttons.get(6).visible = this.release;
+                        }
+                        else
+                        {
+                            this.buttons.get(6).active = this.release;
+                            this.buttons.get(6).visible = this.release;
+                        }
+                    }));
+            this.addButton(new Button(this.width / 2 - xOffset - 31, this.height / 2 - yOffset + 10, 50, 10,
+                    I18n.format("block.pc.option.confirm"), b ->
+                    {
+                        this.release = !this.release;
+                        this.container.setRelease(this.release);
+                        if (this.release)
+                        {
+                            this.buttons.get(6).active = this.release;
+                            this.buttons.get(6).visible = this.release;
+                        }
+                        else
+                        {
+                            this.buttons.get(6).active = this.release;
+                            this.buttons.get(6).visible = this.release;
+                        }
+                        this.minecraft.player.closeScreen();
+                    }));
             this.buttons.get(6).visible = false;
             this.buttons.get(6).active = false;
         }
 
-        this.textFieldBoxName = new TextFieldWidget(this.font, this.width / 2 - xOffset - 80, this.height / 2 - yOffset
-                + 0, 100, 10, this.boxName);
+        this.textFieldBoxName = new TextFieldWidget(this.font, this.width / 2 - xOffset - 80,
+                this.height / 2 - yOffset + 0, 100, 10, this.boxName);
         this.textFieldBoxName.enableBackgroundDrawing = false;
-        this.textFieldSearch = new TextFieldWidget(this.font, this.width / 2 - xOffset - 10, this.height / 2 - yOffset
-                - 121, 90, 10, "");
+        this.textFieldSearch = new TextFieldWidget(this.font, this.width / 2 - xOffset - 10,
+                this.height / 2 - yOffset - 121, 90, 10, "");
 
         this.addButton(this.textFieldSelectedBox);
         this.addButton(this.textFieldBoxName);
@@ -267,10 +270,8 @@ public class PC<T extends PCContainer> extends ContainerScreen<T>
         this.textFieldBoxName.text = this.boxName;
     }
 
-    /**
-     * Called when the screen is unloaded. Used to disable keyboard repeat
-     * events
-     */
+    /** Called when the screen is unloaded. Used to disable keyboard repeat
+     * events */
     @Override
     public void onClose()
     {
@@ -281,55 +282,26 @@ public class PC<T extends PCContainer> extends ContainerScreen<T>
     public void render(final int mouseX, final int mouseY, final float f)
     {
         this.renderBackground();
-        // this.textFieldSelectedBox.drawTextBox();
-        //
-        // if (!this.bound) this.textFieldSearch.drawTextBox();
-        //
-        // if (this.textFieldBoxName.visible)
-        // this.textFieldBoxName.drawTextBox();
-        // final float zLevel = 1000;
         super.render(mouseX, mouseY, f);
-        // FIXME PC Search feature.
-        // for (int i = 0; i < 54; i++)
-        // if (!this.textFieldSearch.getText().isEmpty())
-        // {
-        // final ItemStack stack = this.container.inv.getStackInSlot(i + 54 *
-        // this.container.inv.getPage());
-        // if (stack.isEmpty()) continue;
-        // final int x = i % 9 * 18 + this.width / 2 - 80;
-        // final int y = i / 9 * 18 + this.height / 2 - 96;
-        // // System.out.println(this.textFieldSearch.getText() + " " + i +
-        // // " " + stack);
-        // RenderHelper.disableStandardItemLighting();
-        // final String name = stack == null ? "" :
-        // stack.getDisplayName().getFormattedText();
-        // if (name.isEmpty() ||
-        // !name.toLowerCase(java.util.Locale.ENGLISH).contains(this.textFieldSearch
-        // .getText().toLowerCase(java.util.Locale.ENGLISH)))
-        // {
-        //
-        // GlStateManager.disableLighting();
-        // GlStateManager.disableDepthTest();
-        // GlStateManager.colorMask(true, true, true, false);
-        // final int slotColor = 0x55FF0000;
-        // AbstractGui.fill(x, y, x + 16, y + 16, slotColor);
-        // GlStateManager.colorMask(true, true, true, true);
-        // GlStateManager.enableLighting();
-        // GlStateManager.enableDepthTest();
-        // }
-        // else
-        // {
-        // GlStateManager.disableLighting();
-        // GlStateManager.disableDepthTest();
-        // GlStateManager.colorMask(true, true, true, false);
-        // final int slotColor = 0x5500FF00;
-        // AbstractGui.fill(x, y, x + 16, y + 16, slotColor);
-        // GlStateManager.colorMask(true, true, true, true);
-        // GlStateManager.enableLighting();
-        // GlStateManager.enableDepthTest();
-        // }
-        // RenderHelper.enableGUIStandardItemLighting();
-        // }
+        for (int i = 0; i < 54; i++)
+            if (!this.textFieldSearch.getText().isEmpty())
+            {
+                final ItemStack stack = this.container.inv.getStackInSlot(i + 54 * this.container.inv.getPage());
+                if (stack.isEmpty()) continue;
+                final int x = i % 9 * 18 + this.width / 2 - 80;
+                final int y = i / 9 * 18 + this.height / 2 - 96;
+                final String name = stack == null ? "" : stack.getDisplayName().getFormattedText();
+                if (name.isEmpty() || !ThutCore.trim(name).contains(ThutCore.trim(this.textFieldSearch.getText())))
+                {
+                    final int slotColor = 0x55FF0000;
+                    AbstractGui.fill(x, y, x + 16, y + 16, slotColor);
+                }
+                else
+                {
+                    final int slotColor = 0x5500FF00;
+                    AbstractGui.fill(x, y, x + 16, y + 16, slotColor);
+                }
+            }
         this.renderHoveredToolTip(mouseX, mouseY);
     }
 
