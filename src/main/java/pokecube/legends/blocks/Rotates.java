26604cc443434b095fa6b630e725e46d75ab47a0
WenXin2 <dwnewville@gmail.com>
2020-03-14 19:25:20 -0500

    Adds Waterlogging

--- src/main/java/pokecube/legends/blocks/Rotates.java
+++ src/main/java/pokecube/legends/blocks/Rotates.java
@@ -5,55 +5,70 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.ToolType;
 
+import org.antlr.v4.runtime.misc.NotNull;
+
 public class Rotates extends BlockBase
 {
-
-    public Rotates(final String name, final Material material, final float hardnessresistance,
-            final SoundType sound)
-    {
-        super(name, material, hardnessresistance, sound);
-        this.setDefaultState(this.stateContainer.getBaseState().with(HorizontalBlock.HORIZONTAL_FACING,
-                Direction.NORTH));
-    }
+    private static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    private static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 
     public Rotates(final String name, final Material material, final float hardness, final float resistance,
             final SoundType sound)
     {
         super(name, material, hardness, resistance, sound);
-        this.setDefaultState(this.stateContainer.getBaseState().with(HorizontalBlock.HORIZONTAL_FACING,
-                Direction.NORTH));
+        this.setDefaultState(this.stateContainer.getBaseState()
+          .with(FACING, Direction.NORTH)
+          .with(WATERLOGGED, false));
     }
 
     public Rotates(final String name, final Material material, final ToolType tool, final int level)
     {
         super(name, material, tool, level);
-        this.setDefaultState(this.stateContainer.getBaseState().with(HorizontalBlock.HORIZONTAL_FACING,
-                Direction.NORTH));
+        this.setDefaultState(this.stateContainer.getBaseState()
+          .with(FACING, Direction.NORTH)
+          .with(WATERLOGGED, false));
     }
 
     public Rotates(final String name, final Properties props)
     {
         super(name, props);
-        this.setDefaultState(this.stateContainer.getBaseState().with(HorizontalBlock.HORIZONTAL_FACING,
-                Direction.NORTH));
+        this.setDefaultState(this.stateContainer.getBaseState()
+          .with(FACING, Direction.NORTH)
+          .with(WATERLOGGED, false));
     }
 
     @Override
     protected void fillStateContainer(final StateContainer.Builder<Block, BlockState> builder)
     {
-        builder.add(HorizontalBlock.HORIZONTAL_FACING);
+        builder.add(FACING, WATERLOGGED);
     }
 
     @Override
     public BlockState getStateForPlacement(final BlockItemUseContext context)
     {
-        return this.getDefaultState().with(HorizontalBlock.HORIZONTAL_FACING, context.getPlacementHorizontalFacing()
-                .getOpposite());
+        IFluidState ifluidstate = context.getWorld().getFluidState(context.getPos());
+        return this.getDefaultState()
+          .with(FACING, context.getPlacementHorizontalFacing().getOpposite())
+          .with(WATERLOGGED, ifluidstate.isTagged(FluidTags.WATER) && ifluidstate.getLevel() == 8);
     }
 
+    //Adds Waterlogging
+    @NotNull
+    @SuppressWarnings("deprecation")
+    @Override
+    public IFluidState getFluidState(BlockState state)
+    {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+    }
 }
