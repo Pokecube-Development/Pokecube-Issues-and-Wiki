794533997015a3de4fe990a5c2d7da1208c7f8c7
Thutmose <elpatricimo@gmail.com>
2020-05-02 23:22:56 -0400

    Update PokemobHasParts.java
    removes some code which was preventing large pokemobs from moving correctly.
    
    This needs to be looked into more properly later.
    
    This should address #310 for now

--- src/main/java/pokecube/core/entity/pokemobs/helper/PokemobHasParts.java
+++ src/main/java/pokecube/core/entity/pokemobs/helper/PokemobHasParts.java
@@ -15,13 +15,13 @@ import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import pokecube.core.PokecubeCore;
 import thut.api.entity.ICompoundMob;
-import thut.api.terrain.TerrainManager;
 
 public abstract class PokemobHasParts extends PokemobCombat implements ICompoundMob
 {
     private final PokemobPart[] parts;
-    int                         numWide = 0;
-    int                         numTall = 0;
+
+    int numWide = 0;
+    int numTall = 0;
 
     public PokemobHasParts(final EntityType<? extends ShoulderRidingEntity> type, final World worldIn)
     {
@@ -52,6 +52,8 @@ public abstract class PokemobHasParts extends PokemobCombat implements ICompound
                 .pushableBy(this));
         if (!list.isEmpty())
         {
+            list.removeIf(e -> e instanceof PokemobPart && ((PokemobPart) e).getBase() == this);
+
             final int i = this.world.getGameRules().getInt(GameRules.MAX_ENTITY_CRAMMING);
             if (i > 0 && list.size() > i - 1 && this.rand.nextInt(4) == 0)
             {
@@ -113,26 +115,36 @@ public abstract class PokemobHasParts extends PokemobCombat implements ICompound
             super.move(typeIn, pos);
             return;
         }
-        final float s = this.pokemobCap.getSize();
-        final double dist = Math.max(this.pokemobCap.getPokedexEntry().width * s, this.pokemobCap
-                .getPokedexEntry().length * s) + 8;
-        Vec3d toMove = pos;
-        if (TerrainManager.isAreaLoaded(this.getEntityWorld(), this.getPosition(), dist))
-        {
-            for (final PokemobPart p : this.parts)
-            {
-                // Only consider top and bottom slabs for this.
-                if (pos.y < 0 && p.shift.getY() > 0) continue;
-                if (pos.y > 0 && p.shift.getY() < this.numTall - 1) continue;
-                final Vec3d posA = p.getPositionVec();
-                p.move(typeIn, toMove);
-                final Vec3d posB = p.getPositionVec();
-                toMove = posB.subtract(posA);
-            }
-            super.move(typeIn, toMove);
-            this.updatePartsPos();
-        }
-        else super.move(typeIn, toMove);
+        // TODO get this working properly for large mobs.
+        // final boolean applyPartLimits = false;
+        // final float s = this.pokemobCap.getSize();
+        // final double dist = Math.max(this.pokemobCap.getPokedexEntry().width
+        // * s, this.pokemobCap
+        // .getPokedexEntry().length * s) + 8;
+        // Vec3d toMove = pos;
+        // if (TerrainManager.isAreaLoaded(this.getEntityWorld(),
+        // this.getPosition(), dist) && applyPartLimits)
+        // {
+        // boolean partsOnGround = false;
+        // for (final PokemobPart p : this.parts)
+        // {
+        // // Only consider top and bottom slabs for this.
+        // if (pos.y < 0 && p.shift.getY() > 0) continue;
+        // if (pos.y > 0 && p.shift.getY() < this.numTall - 1) continue;
+        //
+        // final Vec3d posA = p.getPositionVec();
+        // p.move(typeIn, pos);
+        // partsOnGround = partsOnGround || p.onGround;
+        // final Vec3d posB = p.getPositionVec();
+        // toMove = posB.subtract(posA);
+        // System.out.println(pos + " " + toMove);
+        // }
+        // this.setMotion(toMove);
+        // super.move(typeIn, toMove);
+        // this.updatePartsPos();
+        // }
+        // else
+        super.move(typeIn, pos);
     }
 
     @Override
