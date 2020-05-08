d2747e884c41904d58e4fbaf2fe160c51f77aa21
Thutmose <elpatricimo@gmail.com>
2020-05-08 00:51:50 -0400

    Update Rayquaza.java
    fixes some typos in here

--- src/main/java/pokecube/legends/conditions/Rayquaza.java
+++ src/main/java/pokecube/legends/conditions/Rayquaza.java
@@ -9,25 +9,25 @@ import pokecube.core.interfaces.IPokemob;
 public class Rayquaza extends Condition
 {
     @Override
-    public boolean canCapture(Entity trainer, IPokemob pokemon)
+    public boolean canCapture(final Entity trainer, final IPokemob pokemon)
     {
-        if (!canCapture(trainer)) return false;
-        boolean kyogre = CaptureStats.getTotalNumberOfPokemobCaughtBy(trainer.getUniqueID(),
-                Database.getEntry("kyogre")) > 0;
-        boolean groudon = CaptureStats.getTotalNumberOfPokemobCaughtBy(trainer.getUniqueID(),
-                Database.getEntry("groudon")) > 0;
-                
-        String name = "Kyogre, Groudon";
-                
-        if ((Kyogre && Groudon)) return true;
+        if (!this.canCapture(trainer)) return false;
+        final boolean kyogre = CaptureStats.getTotalNumberOfPokemobCaughtBy(trainer.getUniqueID(), Database.getEntry(
+                "kyogre")) > 0;
+        final boolean groudon = CaptureStats.getTotalNumberOfPokemobCaughtBy(trainer.getUniqueID(), Database.getEntry(
+                "groudon")) > 0;
+
+        final String name = "Kyogre, Groudon";
+
+        if (kyogre && groudon) return true;
         if (pokemon != null && !trainer.getEntityWorld().isRemote)
         {
-            sendNoTrust(trainer);
-            sendLegendExtra(trainer, name);
+            this.sendNoTrust(trainer);
+            this.sendLegendExtra(trainer, name);
         }
         return false;
     }
-    
+
     @Override
     public PokedexEntry getEntry()
     {
