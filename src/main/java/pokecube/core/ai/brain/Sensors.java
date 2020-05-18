d3c700fefee4c49004abdbab12d1fcbc42674248
Thutmose <elpatricimo@gmail.com>
2020-05-18 18:06:34 -0400

    Update Sensors.java
    adjustments to this method typing

--- src/main/java/pokecube/core/ai/brain/Sensors.java
+++ src/main/java/pokecube/core/ai/brain/Sensors.java
@@ -1,13 +1,12 @@
 package pokecube.core.ai.brain;
 
-import net.minecraft.entity.ai.brain.sensor.Sensor;
 import net.minecraft.entity.ai.brain.sensor.SensorType;
 import net.minecraftforge.event.RegistryEvent.Register;
 
 public class Sensors
 {
 
-    public static void register(final Register<SensorType<? extends Sensor<?>>> event)
+    public static void register(final Register<SensorType<?>> event)
     {
 
     }
