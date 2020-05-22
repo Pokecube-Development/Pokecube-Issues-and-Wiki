32e452e7ec570b7a8bfa59179ed6dd90b5ba2ad4
Thutmose <elpatricimo@gmail.com>
2020-05-22 07:44:28 -0400

    some more 1.15.2 specific changes

--- src/main/java/pokecube/compat/thutessentials/Impl.java
+++ src/main/java/pokecube/compat/thutessentials/Impl.java
@@ -1,7 +1,9 @@
 package pokecube.compat.thutessentials;
 
 import java.util.Collection;
+import java.util.UUID;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.MinecraftForge;
@@ -13,6 +15,12 @@ import pokecube.core.database.SpawnBiomeMatcher.MatchResult;
 import pokecube.core.database.SpawnBiomeMatcher.SpawnCheck;
 import pokecube.core.database.SpawnBiomeMatcher.StructureMatcher;
 import pokecube.core.events.pokemob.SpawnCheckEvent;
+import pokecube.core.handlers.TeamManager;
+import pokecube.core.handlers.TeamManager.ITeamProvider;
+import thut.api.IOwnable;
+import thut.api.OwnableCaps;
+import thut.essentials.land.LandManager;
+import thut.essentials.land.LandManager.LandTeam;
 import thut.essentials.util.world.IHasStructures;
 import thut.essentials.util.world.WorldStructures;
 
@@ -40,6 +48,44 @@ public class Impl
         }
     }
 
+    private static class TeamProvider implements ITeamProvider
+    {
+        private final ITeamProvider provider;
+
+        public TeamProvider(final ITeamProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        @Override
+        public String getTeam(final Entity entityIn)
+        {
+            final IOwnable ownable = OwnableCaps.getOwnable(entityIn);
+            UUID id = ownable != null ? ownable.getOwnerId() : null;
+            if (id == null) id = entityIn.getUniqueID();
+            final LandTeam team = LandManager.getTeam(id);
+            if (team != LandManager.getDefaultTeam()) return team.teamName;
+            return this.provider.getTeam(entityIn);
+        }
+
+        @Override
+        public boolean areAllied(final String team, final Entity target)
+        {
+            final LandTeam teamA = LandManager.getInstance().getTeam(team, false);
+            final String targTeam = this.getTeam(target);
+            final LandTeam teamB = LandManager.getInstance().getTeam(targTeam, false);
+            if (teamA != null && teamB != null)
+            {
+                if (teamA == teamB) return true;
+                if (teamA.isAlly(teamB)) return true;
+                if (teamA.isAlly(target.getUniqueID())) return true;
+                return false;
+            }
+            return this.provider.areAllied(team, target);
+        }
+
+    }
+
     public static void register()
     {
         PokecubeCore.LOGGER.debug("Registering ThutEssentials Support");
@@ -50,6 +96,7 @@ public class Impl
     public static void initMatcher(final SpawnCheckEvent.Init event)
     {
         event.matcher._structs = StructureMatcher.or(new StructChecker(), event.matcher._structs);
+        TeamManager.provider = new TeamProvider(TeamManager.provider);
     }
 
 }
