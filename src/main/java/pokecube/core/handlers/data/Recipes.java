ecfee31e9aedba21485dd8003fea146dff1f1aa9
Thutmose <elpatricimo@gmail.com>
2020-05-27 22:34:14 -0400

    switches to vanilla datagens for making the loot tables
    this makes it easier to add loot tables for added blocks.
    
    Also added loot tables for shearing berry leaves

---
+++ src/main/java/pokecube/core/handlers/data/Recipes.java
@@ -0,0 +1,54 @@
+package pokecube.core.handlers.data;
+
+import java.util.function.Consumer;
+
+import net.minecraft.block.Block;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapelessRecipeBuilder;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.ConditionalRecipe;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.minecraftforge.common.crafting.conditions.TrueCondition;
+import pokecube.core.handlers.ItemGenerator;
+
+public class Recipes extends RecipeProvider implements IConditionBuilder
+{
+
+    public Recipes(final DataGenerator generatorIn)
+    {
+        super(generatorIn);
+    }
+
+    protected void addConvertRecipe(final Consumer<IFinishedRecipe> consumer, final Block from, final Block to,
+            final int number)
+    {
+        final ResourceLocation id = new ResourceLocation(to.getRegistryName().getNamespace(), from.getRegistryName()
+                .getPath() + "-" + to.getRegistryName().getPath());
+        //@formatter:off
+        ConditionalRecipe.builder()
+        .addCondition(TrueCondition.INSTANCE)
+        .addRecipe(
+            ShapelessRecipeBuilder.shapelessRecipe(to, number)
+            .addIngredient(from)
+            .setGroup(to.getRegistryName().getNamespace())
+            .addCriterion("has_from", this.hasItem(from))
+            ::build
+        )
+        .build(consumer, id);
+        //@formatter:on
+    }
+
+    @Override
+    protected void registerRecipes(final Consumer<IFinishedRecipe> consumer)
+    {
+        for (final String s : ItemGenerator.logs.keySet())
+        {
+            final Block from = ItemGenerator.logs.get(s);
+            final Block to = ItemGenerator.planks.get(s);
+            final int number = 4;
+            this.addConvertRecipe(consumer, from, to, number);
+        }
+    }
+}
