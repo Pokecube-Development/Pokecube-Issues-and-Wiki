4e77ad120fdbf5a7682b389bacfb1ba078adc659
NelkGames <samuel40nelk@hotmail.com>
2020-06-24 15:41:47 -0300

    Update Files (Mobs and Legends) (#457)
    Update:
    updated database with new content: Kubfu, Urshifu and Zarude;
    new movements added;
    new items added:
    Galar Cuff-item for Slowpoke Galar.
    Parchment Darkness/Water- item for Kubfu.
    Gigant Shard- item that we can use to apply G-Max to pokemon.
    Gigantamax Urshifu, Blastoise, Venusaur, Inteleon, Cinderace and Rillaboom.
    new textures for PC, TM Machine and Trade Machine.
    
    Finished Abilities:
    Morpeko;
    Darmanitan;
    Aegislash.
    
    Reworks - Third part of the texture update;
    Arceus;
    Kyurem + Black version;
    Dusknoir;
    Swak;
    Throh;
    Basculin Red/Blue;
    Braviary;
    Camerupt;
    Empoleon;
    Seviper.
    
    Spawn adjustment:
    Nickit
    Wooloo

--- src/main/java/pokecube/mobs/abilities/z/ZenMode.java
+++ src/main/java/pokecube/mobs/abilities/z/ZenMode.java
@@ -1,8 +1,52 @@
 package pokecube.mobs.abilities.z;
 
+import pokecube.core.database.Database;
+import pokecube.core.database.PokedexEntry;
 import pokecube.core.database.abilities.Ability;
+import pokecube.core.interfaces.IPokemob;
 
 public class ZenMode extends Ability
 {
-    // TODO Implement this.
+	private static PokedexEntry baseNormal;
+    private static PokedexEntry zenmodeNormal;
+    
+    private static PokedexEntry baseGalar;
+    private static PokedexEntry zenmodeGalar;
+
+    
+    private static boolean      noTurnBase = false;
+
+    @Override
+    public void onUpdate(IPokemob mob)
+    {
+        if (ZenMode.noTurnBase) return;
+        if (ZenMode.baseNormal == null)
+        {
+        	ZenMode.baseNormal = Database.getEntry("Darmanitan");
+        	ZenMode.zenmodeNormal = Database.getEntry("Darmanitan Zen");
+        	ZenMode.baseGalar = Database.getEntry("Darmanitan Galar");
+        	ZenMode.zenmodeGalar = Database.getEntry("Darmanitan Zen Galar");
+        	ZenMode.noTurnBase = ZenMode.baseNormal == null || ZenMode.zenmodeNormal == null; 
+            if (ZenMode.noTurnBase) return;
+        }
+        
+        final PokedexEntry mobs = mob.getPokedexEntry();
+        if ((mobs == ZenMode.baseNormal || mobs == ZenMode.zenmodeNormal))
+        {
+	        if (mob.getEntity().getHealth() < (mob.getEntity().getMaxHealth() / 2))
+	        {
+	            if (mobs == ZenMode.baseNormal) mob.setPokedexEntry(ZenMode.zenmodeNormal);
+	        }
+        else if (mobs == ZenMode.zenmodeNormal) mob.setPokedexEntry(ZenMode.baseNormal);
+        }
+        
+        if ((mobs == ZenMode.baseGalar || mobs == ZenMode.zenmodeGalar))
+        {
+	        if (mob.getEntity().getHealth() < (mob.getEntity().getMaxHealth() / 2))
+	        {
+	            if (mobs == ZenMode.baseGalar) mob.setPokedexEntry(ZenMode.zenmodeGalar);
+	        }
+        else if (mobs == ZenMode.zenmodeGalar) mob.setPokedexEntry(ZenMode.baseGalar);
+        }
+    }
 }
