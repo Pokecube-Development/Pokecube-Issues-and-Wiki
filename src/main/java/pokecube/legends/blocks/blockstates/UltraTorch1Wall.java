8b6e597de57e52356b205d312ef5a01718c8eb5b
Nelk <samuel40nelk@hotmail.com>
2020-08-04 17:59:03 -0300

    Update Extra

--- src/main/java/pokecube/legends/blocks/blockstates/UltraTorch1Wall.java
+++ src/main/java/pokecube/legends/blocks/blockstates/UltraTorch1Wall.java
@@ -6,6 +6,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.WallTorchBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
@@ -17,12 +18,13 @@ public class UltraTorch1Wall extends WallTorchBlock
     	super(Properties.create(Material.MISCELLANEOUS).hardnessAndResistance(0.0f).lightValue(6));
 	}
     
-	@OnlyIn(Dist.CLIENT)
-    @Override
+    @OnlyIn(Dist.CLIENT)
     public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
-        double d0 = (double)pos.getX() + 0.5D;
-        double d1 = (double)pos.getY() + 0.7D;
-        double d2 = (double)pos.getZ() + 0.5D;
-        worldIn.addParticle(ParticleTypes.DRAGON_BREATH, d0, d1, d2, 0.0D, 0.2D, 0.0D);
+       Direction direction = stateIn.get(HORIZONTAL_FACING);
+       double d0 = (double)pos.getX() + 0.5D;
+       double d1 = (double)pos.getY() + 0.7D;
+       double d2 = (double)pos.getZ() + 0.5D;
+       Direction direction1 = direction.getOpposite();
+       worldIn.addParticle(ParticleTypes.DRAGON_BREATH, d0 + 0.27D * (double)direction1.getXOffset(), d1 + 0.22D, d2 + 0.27D * (double)direction1.getZOffset(), 0.0D, 0.0D, 0.0D);
     }
 }
