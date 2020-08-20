49e79cdd4bc30f1644ccd342ea67827b687e9ffc
Thutmose <elpatricimo@gmail.com>
2020-08-20 15:56:09 -0400

    Update Levitate.java
    fixes levitate also invalidating the users's attacks

--- src/main/java/pokecube/mobs/abilities/l/Levitate.java
+++ src/main/java/pokecube/mobs/abilities/l/Levitate.java
@@ -8,14 +8,14 @@ import pokecube.core.utils.PokeType;
 public class Levitate extends Ability
 {
     @Override
-    public int beforeDamage(IPokemob mob, MovePacket move, int damage)
+    public int beforeDamage(final IPokemob mob, final MovePacket move, final int damage)
     {
-        if (move.getMove().getType(move.attacker) == PokeType.getType("ground")) return 0;
+        if (move.getMove().getType(move.attacker) == PokeType.getType("ground") && move.attacked == mob) return 0;
         return super.beforeDamage(mob, move, damage);
     }
 
     @Override
-    public void onMoveUse(IPokemob mob, MovePacket move)
+    public void onMoveUse(final IPokemob mob, final MovePacket move)
     {
         if (move.attacker == mob || !move.pre || move.attacker == move.attacked) return;
         if (move.getMove().getType(move.attacker) == PokeType.getType("ground")) move.canceled = true;
