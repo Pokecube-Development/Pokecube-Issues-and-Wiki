4fffdc74a9ce8d49a1117ffc5b025403ccd8e7d3
Thutmose <elpatricimo@gmail.com>
2020-12-12 15:03:55 -0500

    re-adds hooks for letting addons override AI more easily

--- src/main/java/pokecube/core/events/pokemob/InitAIEvent.java
+++ src/main/java/pokecube/core/events/pokemob/InitAIEvent.java
@@ -1,17 +1,20 @@
 package pokecube.core.events.pokemob;
 
+import java.util.List;
+
 import net.minecraftforge.event.entity.EntityEvent;
 import pokecube.core.interfaces.IPokemob;
+import thut.api.entity.ai.IAIRunnable;
 
 /**
- * Called after initiating the pokemob's AI. fired on the
+ * Called during initiating of the pokemob's AI. fired on the
  * PokecubeCore.POKEMOB_BUS
  */
 public class InitAIEvent extends EntityEvent
 {
     private final IPokemob pokemob;
 
-    public InitAIEvent(IPokemob entity)
+    protected InitAIEvent(final IPokemob entity)
     {
         super(entity.getEntity());
         this.pokemob = entity;
@@ -20,5 +23,38 @@ public class InitAIEvent extends EntityEvent
     public IPokemob getPokemob()
     {
         return this.pokemob;
+    }
+
+    /**
+     * This event is called after the brain has been initialized, it is for any
+     * final adjustments that need to be done.
+     */
+    public static class Post extends InitAIEvent
+    {
+        public Post(final IPokemob entity)
+        {
+            super(entity);
+        }
+
+    }
+
+    public static class Init extends InitAIEvent
+    {
+        public static enum Type
+        {
+            IDLE, COMBAT, UTILITY;
+        }
+
+        public final Type type;
+
+        public final List<IAIRunnable> tasks;
+
+        public Init(final IPokemob entity, final Type type, final List<IAIRunnable> tasks)
+        {
+            super(entity);
+            this.type = type;
+            this.tasks = tasks;
+        }
+
     }
 }
