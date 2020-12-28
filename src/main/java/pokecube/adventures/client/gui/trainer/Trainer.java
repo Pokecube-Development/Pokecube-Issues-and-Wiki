10c98d1e517a6c8a96ec371759ce9809257ead4d
Thutmose <elpatricimo@gmail.com>
2020-12-27 21:38:24 -0500

    Update Trainer.java
    rename this

--- src/main/java/pokecube/adventures/client/gui/trainer/Trainer.java
+++ src/main/java/pokecube/adventures/client/gui/trainer/Trainer.java
@@ -12,7 +12,7 @@ import pokecube.core.client.Resources;
 
 public class Trainer extends ContainerScreen<ContainerTrainer>
 {
-    public final static ResourceLocation GUI_HEAL_TABLE = new ResourceLocation(PokecubeAdv.MODID,
+    public final static ResourceLocation TRAINER_GUI = new ResourceLocation(PokecubeAdv.MODID,
             Resources.TEXTURE_GUI_FOLDER + "trainergui.png");
 
     public Trainer(final ContainerTrainer screenContainer, final PlayerInventory inv, final ITextComponent titleIn)
@@ -25,7 +25,7 @@ public class Trainer extends ContainerScreen<ContainerTrainer>
             final int y)
     {
         // bind texture
-        this.minecraft.getTextureManager().bindTexture(Resources.GUI_HEAL_TABLE);
+        this.minecraft.getTextureManager().bindTexture(Trainer.TRAINER_GUI);
         final int j2 = (this.width - this.xSize) / 2;
         final int k2 = (this.height - this.ySize) / 2;
         this.blit(matrixStack, j2, k2, 0, 0, this.xSize, this.ySize);
