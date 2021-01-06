4d79f1e4ee5756b0c817b039833f487502039f45
Thutmose <elpatricimo@gmail.com>
2021-01-06 11:25:40 -0500

    Switched daycare tile's annoying sounds to redstone output instead
    the daycare blocks will emit horizontal redstone signals if they have daycare power, instead of making noises.

--- src/main/java/pokecube/adventures/blocks/daycare/DaycareBlock.java
+++ src/main/java/pokecube/adventures/blocks/daycare/DaycareBlock.java
@@ -2,6 +2,8 @@ package pokecube.adventures.blocks.daycare;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import pokecube.core.blocks.InteractableHorizontalBlock;
 
@@ -23,6 +25,31 @@ public class DaycareBlock extends InteractableHorizontalBlock
     public boolean hasTileEntity(final BlockState state)
     {
         return true;
+    }
+
+    @Override
+    public boolean canProvidePower(final BlockState state)
+    {
+        return true;
+    }
+
+    @Override
+    public int getWeakPower(final BlockState blockState, final IBlockReader blockAccess, final BlockPos pos, final Direction side)
+    {
+        if (side == Direction.UP || side == Direction.DOWN) return 0;
+        final TileEntity tile = blockAccess.getTileEntity(pos);
+        if (tile instanceof DaycareTile) return ((DaycareTile) tile).redstonePower;
+        return 0;
+    }
+
+    @Override
+    public int getStrongPower(final BlockState blockState, final IBlockReader blockAccess, final BlockPos pos,
+            final Direction side)
+    {
+        if (side == Direction.UP || side == Direction.DOWN) return 0;
+        final TileEntity tile = blockAccess.getTileEntity(pos);
+        if (tile instanceof DaycareTile) return ((DaycareTile) tile).redstonePower;
+        return 0;
     }
 
 }
