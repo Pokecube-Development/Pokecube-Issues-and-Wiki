f381e0e59a716998856b4c4f28a1cfa3d4976507
Thutmose <elpatricimo@gmail.com>
2021-03-01 21:52:48 -0500

    Lets daycare also check nearby subchunks
    this should help for when the daycare is placed on the edge of a chunk/chunk segment

--- src/main/java/pokecube/core/utils/EntityTools.java
+++ src/main/java/pokecube/core/utils/EntityTools.java
@@ -1,8 +1,17 @@
 package pokecube.core.utils;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ClassInheritanceMultiMap;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.IChunk;
 import net.minecraftforge.entity.PartEntity;
 import pokecube.core.interfaces.IPokemob;
 
@@ -77,5 +86,40 @@ public class EntityTools
             if (mob instanceof LivingEntity) return (LivingEntity) mob;
         }
         return in instanceof LivingEntity ? (LivingEntity) in : null;
+    }
+
+    /**
+     * This returns all nearby mobs, but using a quick lookup, note that this
+     * will miss large mobs which are centred on chunks at the edge of the given
+     * range, but outside the chunk looked, it is meant to be used for cases
+     * where quick lookup are needed, and precision is not needed.
+     *
+     * @param toFill
+     * @param centre
+     * @param range
+     * @param valid
+     */
+    public static void getNearMobsFast(final List<Entity> toFill, final World world, final BlockPos centre,
+            final int range, final Predicate<Entity> valid)
+    {
+        final int r = Math.max(range >> 4, 1);
+        // Check surrounding chunks as well
+        final int x = centre.getX() >> 4;
+        final int y = centre.getY() >> 4;
+        final int z = centre.getZ() >> 4;
+        for (int i = -r; i <= r; i++)
+            for (int j = -r; j <= r; j++)
+            {
+                // Chunk exists calls this anyway, then does a null check.
+                final IChunk c = world.getChunk(x + i, z + j, ChunkStatus.FULL, false);
+                if (!(c instanceof Chunk)) continue;
+                final Chunk chunk = (Chunk) c;
+                for (int k = -r; k <= r; k++)
+                {
+                    final ClassInheritanceMultiMap<Entity> mobs = chunk.getEntityLists()[y];
+                    toFill.addAll(mobs);
+                }
+            }
+        toFill.removeIf(valid.negate());
     }
 }
