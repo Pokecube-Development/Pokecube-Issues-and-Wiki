920363b9d587b7c4be72f669e957164daa1345ae
WenXin2 <newvillewd@yahoo.com>
2021-03-15 11:28:36 -0500

    Update PC

--- src/main/java/pokecube/core/blocks/pc/PCBlock.java
+++ src/main/java/pokecube/core/blocks/pc/PCBlock.java
@@ -20,7 +20,6 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
@@ -46,52 +45,28 @@ public class PCBlock extends HorizontalBlock implements IWaterLoggable
 
     // Precise selection box
     static
-    {// @formatter:off
-        PCBlock.PC_TOP.put(Direction.NORTH,
-                VoxelShapes.join(Block.box(0.34, 0.05, 5.61, 15.66, 16.81, 15.83),
-                        Block.box(0.26, 0.05, 9.71, 15.74, 16.94, 11.72), IBooleanFunction.OR)
-        );
-        PCBlock.PC_TOP.put(Direction.EAST,
-                VoxelShapes.join(Block.box(0.17, 0.05, 0.34, 10.39, 16.81, 15.66),
-                        Block.box(4.28, 0.05, 0.26, 6.29, 16.94, 15.74), IBooleanFunction.OR)
-        );
-        PCBlock.PC_TOP.put(Direction.SOUTH,
-                VoxelShapes.join(Block.box(0.34, 0.05, 0.17, 15.66, 16.81, 10.39),
-                        Block.box(0.26, 0.05, 4.28, 15.74, 16.94, 6.29), IBooleanFunction.OR)
-        );
-        PCBlock.PC_TOP.put(Direction.WEST,
-                VoxelShapes.join(Block.box(5.61, 0.05, 0.34, 15.83, 16.81, 15.66),
-                        Block.box(9.71, 0.05, 0.26, 11.72, 16.94, 15.74), IBooleanFunction.OR)
-        );
-        PCBlock.PC_BASE.put(Direction.NORTH,
-                VoxelShapes.join(Block.box(0.34, 0.05, 5.61, 15.66, 16.05, 15.83),
-                        VoxelShapes.join(Block.box(1.46, 0.04, 2.3, 14.54, 14.95, 6.62),
-                                VoxelShapes.join(Block.box(1.39, 12.06, 0.81, 14.61, 18.16, 7.47),
-                                        Block.box(0.26, 0.05, 9.72, 15.74, 16.049, 11.72),
-                                        IBooleanFunction.OR), IBooleanFunction.OR), IBooleanFunction.OR)
-        );
-        PCBlock.PC_BASE.put(Direction.EAST,
-                VoxelShapes.join(Block.box(0.17, 0.05, 0.34, 10.39, 16.05, 15.66),
-                        VoxelShapes.join(Block.box(9.38, 0.04, 1.46, 13.7, 14.95, 14.54),
-                                VoxelShapes.join(Block.box(8.53, 12.06, 1.39, 15.19, 18.16, 14.61),
-                                        Block.box(4.28, 0.05, 0.26, 6.28, 16.049, 15.74),
-                                        IBooleanFunction.OR), IBooleanFunction.OR), IBooleanFunction.OR)
-        );
-        PCBlock.PC_BASE.put(Direction.SOUTH,
-                VoxelShapes.join(Block.box(0.34, 0.05, 0.17, 15.66, 16.05, 10.39),
-                        VoxelShapes.join(Block.box(1.46, 0.04, 9.38, 14.54, 14.95, 13.7),
-                                VoxelShapes.join(Block.box(1.39, 12.06, 8.53, 14.61, 18.16, 15.19),
-                                        Block.box(0.26, 0.05, 4.28, 15.74, 16.049, 6.28),
-                                        IBooleanFunction.OR), IBooleanFunction.OR), IBooleanFunction.OR)
-        );
-        PCBlock.PC_BASE.put(Direction.WEST,
-                VoxelShapes.join(Block.box(5.61, 0.05, 0.34, 15.83, 16.05, 15.66),
-                        VoxelShapes.join(Block.box(2.3, 0.04, 1.46, 6.62, 14.95, 14.54),
-                                VoxelShapes.join(Block.box(0.81, 12.06, 1.39, 7.47, 18.16, 14.61),
-                                        Block.box(9.72, 0.05, 0.26, 11.72, 16.049, 15.74),
-                                        IBooleanFunction.OR), IBooleanFunction.OR), IBooleanFunction.OR)
-        );
-    }// @formatter:on
+    {
+        PC_TOP.put(Direction.NORTH,
+                Block.box(0, 0, 8, 16, 16, 16)).optimize();
+        PC_TOP.put(Direction.EAST,
+                Block.box(0, 0, 0, 8, 16, 16)).optimize();
+        PC_TOP.put(Direction.SOUTH,
+                Block.box(0, 0, 0, 16, 16, 8)).optimize();
+        PC_TOP.put(Direction.WEST,
+                Block.box(8, 0, 0, 16, 16, 16)).optimize();
+        PCBlock.PC_BASE.put(Direction.NORTH, VoxelShapes.or(
+                Block.box(0, 0, 8, 16, 16, 16),
+                Block.box(1, 0, 4, 15, 16, 8)).optimize());
+        PCBlock.PC_BASE.put(Direction.EAST, VoxelShapes.or(
+                Block.box(0, 0, 0, 8, 16, 16),
+                Block.box(8, 0, 1, 12, 16, 15)).optimize());
+        PCBlock.PC_BASE.put(Direction.SOUTH, VoxelShapes.or(
+                Block.box(0, 0, 0, 16, 16, 8),
+                Block.box(1, 0, 8, 15, 16, 12)).optimize());
+        PCBlock.PC_BASE.put(Direction.WEST, VoxelShapes.or(
+                Block.box(8, 0, 0, 16, 16, 16),
+                Block.box(4, 0, 1, 8, 16, 15)).optimize());
+    }
 
     // Precise selection box
     @Override
