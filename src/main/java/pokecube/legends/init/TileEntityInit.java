87586c83765585327726455de1c8d999db17b3f6
WenXin2 <dwnewville@gmail.com>
2021-08-22 22:29:43 -0500

    Missed a step

--- src/main/java/pokecube/legends/init/TileEntityInit.java
+++ src/main/java/pokecube/legends/init/TileEntityInit.java
@@ -15,9 +15,10 @@ public class TileEntityInit
     static
     {
     	GENERIC_BARREL_TILE = PokecubeLegends.TILES.register("generic_barrel", () -> TileEntityType.Builder.of(
-    	    GenericBarrelTile::new, BlockInit.AGED_BARREL.get(), BlockInit.CONCRETE_BARREL.get(), BlockInit.INVERTED_BARREL.get(),
-			BlockInit.TEMPORAL_BARREL.get(), BlockInit.MIRAGE_BARREL.get(), BlockInit.CORRUPTED_BARREL.get(),
-			BlockInit.DISTORTIC_BARREL.get(), BlockInit.DISTORTIC_STONE_BARREL.get()).build(null));
+    	    GenericBarrelTile::new, BlockInit.AGED_BARREL.get(), BlockInit.CONCRETE_BARREL.get(),
+			BlockInit.CONCRETE_DENSE_BARREL.get(), BlockInit.INVERTED_BARREL.get(), BlockInit.TEMPORAL_BARREL.get(),
+			BlockInit.MIRAGE_BARREL.get(), BlockInit.CORRUPTED_BARREL.get(), BlockInit.DISTORTIC_BARREL.get(),
+			BlockInit.DISTORTIC_STONE_BARREL.get()).build(null));
 		GENERIC_BOOKSHELF_EMPTY_TILE = PokecubeLegends.TILES.register("generic_bookshelf_empty", () -> TileEntityType.Builder.of(
 			GenericBookshelfEmptyTile::new, BlockInit.DISTORTIC_BOOKSHELF_EMPTY.get()).build(null));
     }
