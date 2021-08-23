c8405aa10be4d755dcdf18b640157876fc329f59
WenXin2 <dwnewville@gmail.com>
2021-08-22 22:39:11 -0500

    Missed a step again

--- src/main/java/pokecube/legends/init/TileEntityInit.java
+++ src/main/java/pokecube/legends/init/TileEntityInit.java
@@ -2,9 +2,9 @@ package pokecube.legends.init;
 
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
-import pokecube.legends.PokecubeLegends;
-import pokecube.core.tileentity.GenericBookshelfEmptyTile;
 import pokecube.core.tileentity.GenericBarrelTile;
+import pokecube.core.tileentity.GenericBookshelfEmptyTile;
+import pokecube.legends.PokecubeLegends;
 
 public class TileEntityInit 
 {
@@ -16,11 +16,12 @@ public class TileEntityInit
     {
     	GENERIC_BARREL_TILE = PokecubeLegends.TILES.register("generic_barrel", () -> TileEntityType.Builder.of(
     	    GenericBarrelTile::new, BlockInit.AGED_BARREL.get(), BlockInit.CONCRETE_BARREL.get(),
-			BlockInit.CONCRETE_DENSE_BARREL.get(), BlockInit.INVERTED_BARREL.get(), BlockInit.TEMPORAL_BARREL.get(),
-			BlockInit.MIRAGE_BARREL.get(), BlockInit.CORRUPTED_BARREL.get(), BlockInit.DISTORTIC_BARREL.get(),
-			BlockInit.DISTORTIC_STONE_BARREL.get()).build(null));
+			BlockInit.CONCRETE_DENSE_BARREL.get(), BlockInit.CORRUPTED_BARREL.get(), BlockInit.DISTORTIC_BARREL.get(),
+			BlockInit.DISTORTIC_STONE_BARREL.get(), BlockInit.INVERTED_BARREL.get(), BlockInit.MIRAGE_BARREL.get(),
+			BlockInit.TEMPORAL_BARREL.get()).build(null));
 		GENERIC_BOOKSHELF_EMPTY_TILE = PokecubeLegends.TILES.register("generic_bookshelf_empty", () -> TileEntityType.Builder.of(
-			GenericBookshelfEmptyTile::new, BlockInit.DISTORTIC_BOOKSHELF_EMPTY.get()).build(null));
+			GenericBookshelfEmptyTile::new, BlockInit.AGED_BOOKSHELF_EMPTY.get(), BlockInit.CONCRETE_BOOKSHELF_EMPTY.get(),
+			BlockInit.CONCRETE_DENSE_BOOKSHELF_EMPTY.get(), BlockInit.CORRUPTED_BOOKSHELF_EMPTY.get(), BlockInit.DISTORTIC_BOOKSHELF_EMPTY.get()).build(null));
     }
     
     public static void init() {}
