34f80a75b6f8d0ceefebc22714951aabeb0e43c2
Thutmose <Thutmose@users.noreply.github.com>
2021-09-27 18:18:08 -0400

    Update ItemStackTools.java
    some code cleanup here

--- src/main/java/thut/lib/ItemStackTools.java
+++ src/main/java/thut/lib/ItemStackTools.java
@@ -9,7 +9,8 @@ import net.minecraftforge.items.wrapper.InvWrapper;
 
 public class ItemStackTools
 {
-    public static boolean addItemStackToInventory(ItemStack itemStackIn, IInventory toAddTo, int minIndex)
+    public static boolean addItemStackToInventory(final ItemStack itemStackIn, final IInventory toAddTo,
+            final int minIndex)
     {
         return ItemStackTools.addItemStackToInventory(itemStackIn, new InvWrapper(toAddTo), minIndex);
     }
@@ -18,33 +19,34 @@ public class ItemStackTools
      * Adds the item stack to the inventory, returns false if it is
      * impossible.
      */
-    public static boolean addItemStackToInventory(ItemStack itemStackIn, IItemHandlerModifiable toAddTo, int minIndex)
+    public static boolean addItemStackToInventory(final ItemStack itemStackIn, final IItemHandlerModifiable toAddTo,
+            final int minIndex)
     {
         if (!itemStackIn.isEmpty()) try
         {
             if (itemStackIn.isDamaged())
             {
-                final int j = ItemStackTools.getFirstEmptyStack(toAddTo, minIndex);
+                final int slot = ItemStackTools.getFirstEmptyStack(toAddTo, minIndex);
 
-                if (j >= 0)
+                if (slot >= minIndex)
                 {
-                    toAddTo.setStackInSlot(j, itemStackIn.copy());
-                    toAddTo.getStackInSlot(j).setPopTime(5);
+                    toAddTo.setStackInSlot(slot, itemStackIn.copy());
+                    toAddTo.getStackInSlot(slot).setPopTime(5);
                     itemStackIn.setCount(0);
                     return true;
                 }
                 return false;
             }
-            int i;
+            int count;
 
             while (true)
             {
-                i = itemStackIn.getCount();
+                count = itemStackIn.getCount();
                 final int num = ItemStackTools.storePartialItemStack(itemStackIn, toAddTo, minIndex);
                 itemStackIn.setCount(num);
-                if (num <= 0 || num >= i) break;
+                if (num <= 0 || num >= count) break;
             }
-            return itemStackIn.getCount() < i;
+            return itemStackIn.getCount() < count;
         }
         catch (final Throwable throwable)
         {
@@ -59,36 +61,37 @@ public class ItemStackTools
         return false;
     }
 
-    private static boolean canMergeStacks(ItemStack stack1, ItemStack stack2)
+    private static boolean canMergeStacks(final ItemStack stack1, final ItemStack stack2)
     {
         return !stack1.isEmpty() && ItemStackTools.stackEqualExact(stack1, stack2) && stack1.isStackable() && stack1
                 .getCount() < stack1.getMaxStackSize();
     }
 
-    public static int getFirstEmptyStack(IInventory inventory, int minIndex)
+    public static int getFirstEmptyStack(final IInventory inventory, final int minIndex)
     {
         return ItemStackTools.getFirstEmptyStack(new InvWrapper(inventory), minIndex);
     }
 
     /** Returns the first item stack that is empty. */
-    public static int getFirstEmptyStack(IItemHandlerModifiable inventory, int minIndex)
+    public static int getFirstEmptyStack(final IItemHandlerModifiable inventory, final int minIndex)
     {
-        for (int i = minIndex; i < inventory.getSlots(); ++i)
-            if (inventory.getStackInSlot(i).isEmpty()) return i;
+        for (int index = minIndex; index < inventory.getSlots(); ++index)
+            if (inventory.getStackInSlot(index).isEmpty()) return index;
         return -1;
     }
 
     /** Checks item, NBT, and meta if the item is not damageable */
-    private static boolean stackEqualExact(ItemStack stack1, ItemStack stack2)
+    private static boolean stackEqualExact(final ItemStack stack1, final ItemStack stack2)
     {
         return stack1.getItem() == stack2.getItem() && ItemStack.tagMatches(stack1, stack2);
     }
 
     /** stores an itemstack in the users inventory */
-    private static int storeItemStack(ItemStack itemStackIn, IItemHandlerModifiable inventory, int minIndex)
+    private static int storeItemStack(final ItemStack itemStackIn, final IItemHandlerModifiable inventory,
+            final int minIndex)
     {
-        for (int i = minIndex; i < inventory.getSlots(); ++i)
-            if (ItemStackTools.canMergeStacks(inventory.getStackInSlot(i), itemStackIn)) return i;
+        for (int index = minIndex; index < inventory.getSlots(); ++index)
+            if (ItemStackTools.canMergeStacks(inventory.getStackInSlot(index), itemStackIn)) return index;
         return -1;
     }
 
@@ -96,35 +99,37 @@ public class ItemStackTools
      * This function stores as many items of an ItemStack as possible in a
      * matching slot and returns the quantity of left over items.
      */
-    private static int storePartialItemStack(ItemStack itemStackIn, IItemHandlerModifiable inventory, int minIndex)
+    private static int storePartialItemStack(final ItemStack itemStackIn, final IItemHandlerModifiable inventory,
+            final int minIndex)
     {
-        int i = itemStackIn.getCount();
-        int j = ItemStackTools.storeItemStack(itemStackIn, inventory, minIndex);
+        int count = itemStackIn.getCount();
+        int index = ItemStackTools.storeItemStack(itemStackIn, inventory, minIndex);
 
-        if (j < 0) j = ItemStackTools.getFirstEmptyStack(inventory, minIndex);
+        if (index < minIndex) index = ItemStackTools.getFirstEmptyStack(inventory, minIndex);
 
-        if (j < 0) return i;
-        ItemStack itemstack = inventory.getStackInSlot(j);
+        if (index < minIndex) return count;
+        ItemStack itemstack = inventory.getStackInSlot(index);
 
         if (itemstack.isEmpty())
         {
             itemstack = itemStackIn.copy();
             itemstack.setCount(0);
             if (itemStackIn.hasTag()) itemstack.setTag(itemStackIn.getTag().copy());
-            inventory.setStackInSlot(j, itemstack);
+            inventory.setStackInSlot(index, itemstack);
         }
 
-        int k = i;
-        final int size = inventory.getStackInSlot(j).getCount();
-        if (i > inventory.getStackInSlot(j).getMaxStackSize() - size) k = inventory.getStackInSlot(j).getMaxStackSize()
+        int remainingCount = count;
+        final int size = inventory.getStackInSlot(index).getCount();
+        if (count > inventory.getStackInSlot(index).getMaxStackSize() - size) remainingCount = inventory.getStackInSlot(
+                index).getMaxStackSize() - size;
+
+        if (remainingCount > inventory.getSlotLimit(index) - size) remainingCount = inventory.getSlotLimit(index)
                 - size;
 
-        if (k > inventory.getSlotLimit(j) - size) k = inventory.getSlotLimit(j) - size;
-
-        if (k == 0) return i;
-        i = i - k;
-        inventory.getStackInSlot(j).setCount(size + k);
-        inventory.getStackInSlot(j).setPopTime(5);
-        return i;
+        if (remainingCount == 0) return count;
+        count = count - remainingCount;
+        inventory.getStackInSlot(index).setCount(size + remainingCount);
+        inventory.getStackInSlot(index).setPopTime(5);
+        return count;
     }
 }
