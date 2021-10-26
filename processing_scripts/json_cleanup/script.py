1d89fba096647744bc6772b102b3c7250f9243da
Thutmose <Thutmose@users.noreply.github.com>
2021-10-26 17:07:12 -0400

    Update script.py
    this can now do regex find/replace to change things as needed.

--- processing_scripts/json_cleanup/script.py
+++ processing_scripts/json_cleanup/script.py
@@ -1,20 +1,55 @@
 import os
 import sys
 import json
+import re
 from glob import glob
 
 jsons = [y for x in os.walk("./inputs") for y in glob(os.path.join(x[0], '*.json'))]
 
+'''
+Here are some regex used earlier, npp refers to in Notepad++.
+
+Advancements:
+npp:
+("items": \[)(.*)("item": ")(.*?)(")
+\1\2("items": \[ ")\4(" \])
+
+Loot tables:
+
+npp:
+("predicate": {)(\s+)("item": ")(.*?)(")
+\1\2("items": \[ ")\4(" \])
+here:
+r'("predicate": {)(\s+)("item": ")(.*?)(")':
+r'\1\2"items": [ "\4" ]',
+
+'''
+
+reps = {
+r'("predicate": {)(\s+)("item": ")(.*?)(")':
+r'\1\2"items": [ "\4" ]',
+}
+
+# reps = {}
+
+replaced = 0
 
 lastdir = None
 for file in jsons:
 
-    json_in = open(file, 'r', encoding='utf-8')
-
+    json_str = ""
     try:
-        json_obj = json.loads(json_in.read())
+        json_in = open(file, 'r', encoding='utf-8')
+        json_str = json_in.read()
+        json_in.close()
+
+        for key, val in reps.items():
+            (json_str, num) = re.subn(key, val, json_str, flags=re.M)
+            replaced = replaced + num
+        json_obj = json.loads(json_str)
     except Exception as e:
         print("Error with: {}".format(file))
+        print(json_str)
         print(e)
         continue
 
@@ -31,3 +66,5 @@ for file in jsons:
     json_out = open(file, 'w', encoding='utf-8')
     json_out.write(json.dumps(json_obj, indent=2))
     json_out.close()
+
+print("Replacements: {}".format(replaced))
