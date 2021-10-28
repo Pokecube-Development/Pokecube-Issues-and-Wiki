ad811bdecac9eea34b0da225830a1e3b090293b1
WenXin2 <dwnewville@gmail.com>
2021-10-28 17:28:18 -0500

    Added the Temporal Bamboo Forest
    Oops forgot everything else :D

--- src/main/java/pokecube/legends/worldgen/WorldgenFeatures.java
+++ src/main/java/pokecube/legends/worldgen/WorldgenFeatures.java
@@ -1,5 +1,7 @@
 package pokecube.legends.worldgen;
 
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.ProbabilityFeatureConfiguration;
 import net.minecraft.world.level.levelgen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.level.levelgen.surfacebuilders.SurfaceBuilderBaseConfiguration;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -7,6 +9,7 @@ import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import pokecube.legends.Reference;
+import pokecube.legends.worldgen.features.TemporalBambooFeature;
 import pokecube.legends.worldgen.surface_builders.BlindingDeltasSurfaceBuilder;
 import pokecube.legends.worldgen.surface_builders.MirageDesertSurfaceBuilder;
 
@@ -14,14 +17,20 @@ public class WorldgenFeatures
 {
     public static final DeferredRegister<SurfaceBuilder<?>> SURFACE_BUILDERS = DeferredRegister.create(
             ForgeRegistries.SURFACE_BUILDERS, Reference.ID);
+    public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(
+            ForgeRegistries.FEATURES, Reference.ID);
 
     public static final RegistryObject<SurfaceBuilder<?>> MIRAGE_DESERT = WorldgenFeatures.SURFACE_BUILDERS.register("mirage_desert",
             () -> new MirageDesertSurfaceBuilder(SurfaceBuilderBaseConfiguration.CODEC));
     public static final RegistryObject<SurfaceBuilder<?>> BLINDING_DELTAS = WorldgenFeatures.SURFACE_BUILDERS.register("blinding_deltas",
             () -> new BlindingDeltasSurfaceBuilder(SurfaceBuilderBaseConfiguration.CODEC));
 
+    public static final RegistryObject<Feature<?>> TEMPORAL_BAMBOO = WorldgenFeatures.FEATURES.register("temporal_bamboo",
+            () -> new TemporalBambooFeature(ProbabilityFeatureConfiguration.CODEC));
+
     public static void init(final IEventBus bus)
     {
         WorldgenFeatures.SURFACE_BUILDERS.register(bus);
+        WorldgenFeatures.FEATURES.register(bus);
     }
 }
