047d90d443c84152e5be5b3622a355d42ced4049
WenXin2 <dwnewville@gmail.com>
2021-11-05 17:48:14 -0500

    Simplify hitbox code

--- src/main/java/pokecube/legends/blocks/customblocks/HeatranBlock.java
+++ src/main/java/pokecube/legends/blocks/customblocks/HeatranBlock.java
@@ -19,53 +19,24 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class HeatranBlock extends Rotates implements SimpleWaterloggedBlock
 {
-    private static final Map<Direction, VoxelShape> HEATRAN  = new HashMap<>();
     private static final DirectionProperty          FACING      = HorizontalDirectionalBlock.FACING;
     private static final BooleanProperty            WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     // Precise selection box
-    static
-    {// @formatter:off
-    	HeatranBlock.HEATRAN.put(Direction.NORTH, Shapes.or(
+    private static final VoxelShape HEATRAN = Shapes.or(
 			Block.box(3, 6, 3, 13, 10, 13),
 			Block.box(2, 10, 2, 14, 12, 14),
 			Block.box(2, 4, 2, 14, 6, 14),
 			Block.box(1, 12, 1, 15, 14, 15),
 			Block.box(1, 2, 1, 15, 4, 15),
 			Block.box(0, 14, 0, 16, 16, 16),
-			Block.box(0, 0, 0, 16, 2, 16)).optimize());
-    	HeatranBlock.HEATRAN.put(Direction.EAST, Shapes.or(
-			Block.box(3, 6, 3, 13, 10, 13),
-			Block.box(2, 10, 2, 14, 12, 14),
-			Block.box(2, 4, 2, 14, 6, 14),
-			Block.box(1, 12, 1, 15, 14, 15),
-			Block.box(1, 2, 1, 15, 4, 15),
-			Block.box(0, 14, 0, 16, 16, 16),
-			Block.box(0, 0, 0, 16, 2, 16)).optimize());
-    	HeatranBlock.HEATRAN.put(Direction.SOUTH, Shapes.or(
-			Block.box(3, 6, 3, 13, 10, 13),
-			Block.box(2, 10, 2, 14, 12, 14),
-			Block.box(2, 4, 2, 14, 6, 14),
-			Block.box(1, 12, 1, 15, 14, 15),
-			Block.box(1, 2, 1, 15, 4, 15),
-			Block.box(0, 14, 0, 16, 16, 16),
-			Block.box(0, 0, 0, 16, 2, 16)).optimize());
-    	HeatranBlock.HEATRAN.put(Direction.WEST, Shapes.or(
-			Block.box(3, 6, 3, 13, 10, 13),
-			Block.box(2, 10, 2, 14, 12, 14),
-			Block.box(2, 4, 2, 14, 6, 14),
-			Block.box(1, 12, 1, 15, 14, 15),
-			Block.box(1, 2, 1, 15, 4, 15),
-			Block.box(0, 14, 0, 16, 16, 16),
-			Block.box(0, 0, 0, 16, 2, 16)).optimize());
-    }// @formatter:on
+			Block.box(0, 0, 0, 16, 2, 16)).optimize();
 
-    // Precise selection box
     @Override
     public VoxelShape getShape(final BlockState state, final BlockGetter worldIn, final BlockPos pos,
             final CollisionContext context)
     {
-        return HeatranBlock.HEATRAN.get(state.getValue(HeatranBlock.FACING));
+        return HeatranBlock.HEATRAN;
     }
 
     public HeatranBlock(final Properties props)
