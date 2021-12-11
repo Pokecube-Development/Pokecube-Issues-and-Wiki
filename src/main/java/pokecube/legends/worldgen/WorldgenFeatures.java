bd6fdcd24423eeb3ff95467fb8b214a4ee75a51a
WenXin2 <dwnewville@gmail.com>
2021-12-10 20:24:28 -0600

    Fix height of islands

--- src/main/java/pokecube/legends/worldgen/WorldgenFeatures.java
+++ src/main/java/pokecube/legends/worldgen/WorldgenFeatures.java
@@ -27,7 +27,6 @@ import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureCo
 import net.minecraft.world.level.levelgen.placement.BiomeFilter;
 import net.minecraft.world.level.levelgen.placement.CountOnEveryLayerPlacement;
 import net.minecraft.world.level.levelgen.placement.CountPlacement;
-import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -45,10 +44,6 @@ public class WorldgenFeatures<FC extends FeatureConfiguration> extends ForgeRegi
 //            ForgeRegistries.SURFACE_BUILDERS, Reference.ID);
     public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(
             ForgeRegistries.FEATURES, Reference.ID);
-
-//    private static <C extends FeatureConfiguration, F extends Feature<C>> F register(String p_65808_, F p_65809_) {
-//       return Registry.register(Registry.FEATURE, p_65808_, p_65809_);
-//    }
 
 //    public static final RegistryObject<SurfaceBuilder<?>> BURNT_DESERT = WorldgenFeatures.SURFACE_BUILDERS.register("burnt_desert_builder",
 //            () -> new BurntDesertSurfaceBuilder(SurfaceBuilderBaseConfiguration.CODEC));
@@ -95,31 +90,10 @@ public class WorldgenFeatures<FC extends FeatureConfiguration> extends ForgeRegi
 //            () -> new TaintedSeagrassFeature(ProbabilityFeatureConfiguration.CODEC));
 //    public static final RegistryObject<Feature<?>> STRING_OF_PEARLS = WorldgenFeatures.FEATURES.register("string_of_pearls_feature",
 //            () -> new StringOfPearlsFeature(NoneFeatureConfiguration.CODEC));
-//    public static final Feature<ColumnFeatureConfiguration> DISTORTIC_STONE_BOULDERS2 = register("pokecube_legends:distortic_stone_boulders_feature", 
-//    		new DistorticStoneBouldersFeature(ColumnFeatureConfiguration.CODEC));
-//    
-//    public static final PlacedFeature ROCK_DISTORTIC_STONE_PLACEMENT = PlacementUtils.register("pokecube_legends:rock_distortic_stone", 
-//    		WorldgenFeatures.ROCK_DISTORTIC_STONE_FEATURE.placed(CountPlacement.of(2), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome()));
-//    public static final PlacedFeature RANDOM_DISTORTIC_STONE_BOULDERS_PLACEMENT = PlacementUtils.register("trees_flower_forest", 
-//    		WorldgenFeatures.RANDOM_DISTORTIC_STONE_BOULDERS_FEATURE.placed(treePlacement(PlacementUtils.countExtra(6, 0.1F, 1))));
-//    public static final PlacedFeature SMALL_DISTORTIC_STONE_BOULDERS_PLACEMENT = PlacementUtils.register("pokecube_legends:small_distortic_stone_boulders", 
-//    		WorldgenFeatures.SMALL_DISTORTIC_STONE_BOULDERS_FEATURE.placed(CountOnEveryLayerPlacement.of(4), BiomeFilter.biome()));
-//
-//    public static final ConfiguredFeature<ColumnFeatureConfiguration, ?> SMALL_DISTORTIC_STONE_BOULDERS_FEATURE = FeatureUtils.register("pokecube_legends:small_distortic_stone_boulders", 
-//    		WorldgenFeatures.DISTORTIC_STONE_BOULDERS2.configured(new ColumnFeatureConfiguration(ConstantInt.of(1), UniformInt.of(1, 4))));
-//    public static final ConfiguredFeature<RandomFeatureConfiguration, ?> RANDOM_DISTORTIC_STONE_BOULDERS_FEATURE = 
-//    		FeatureUtils.register("pokecube_legends:random_distortic_stone_boulders", Feature.RANDOM_SELECTOR.configured(
-//    				new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(ROCK_DISTORTIC_STONE_PLACEMENT, 0.015F)), ROCK_DISTORTIC_STONE_PLACEMENT)));
-//    public static final ConfiguredFeature<BlockStateConfiguration, ?> ROCK_DISTORTIC_STONE_FEATURE = FeatureUtils.register("pokecube_legends:rock_distortic_stone", 
-//    		Feature.FOREST_ROCK.configured(new BlockStateConfiguration(BlockInit.DISTORTIC_STONE.get().defaultBlockState())));
 
     public static void init(final IEventBus bus)
     {
 //        WorldgenFeatures.SURFACE_BUILDERS.register(bus);
         WorldgenFeatures.FEATURES.register(bus);
     }
-
-//    public ConfiguredFeature<FC, ?> configured(FC p_65816_) {
-//       return new ConfiguredFeature<>(this, p_65816_);
-//    }
 }
