121c36d8ebbc02e727df8cb10bc7f10c7e6be793
Thutmose <Thutmose@users.noreply.github.com>
2021-12-20 20:50:27 -0500

    more spawning adjustments

--- src/main/java/pokecube/core/database/spawns/SpawnBiomeMatcher.java
+++ src/main/java/pokecube/core/database/spawns/SpawnBiomeMatcher.java
@@ -700,7 +700,7 @@ public class SpawnBiomeMatcher // implements Predicate<SpawnCheck>
             boolean and_valid = this._and_children.size() > 0;
 
             for (final SpawnBiomeMatcher child : this._and_children) and_valid = and_valid && child.valid;
-            for (final SpawnBiomeMatcher child : this._or_children) or_valid = or_valid && child.valid;
+            for (final SpawnBiomeMatcher child : this._or_children) or_valid = or_valid || child.valid;
 
             this.valid = or_valid || and_valid;
 
--- src/main/java/pokecube/core/handlers/events/EventsHandler.java
+++ src/main/java/pokecube/core/handlers/events/EventsHandler.java
@@ -38,7 +38,6 @@ import net.minecraft.world.item.trading.Merchant;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.entity.EntityTypeTest;
-import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.event.AddReloadListenerEvent;
@@ -74,7 +73,6 @@ import pokecube.core.blocks.tms.TMTile;
 import pokecube.core.blocks.trade.TraderTile;
 import pokecube.core.commands.CommandManager;
 import pokecube.core.database.Database;
-import pokecube.core.database.PokedexEntry;
 import pokecube.core.database.pokedex.PokedexEntryLoader.SpawnRule;
 import pokecube.core.database.spawns.SpawnBiomeMatcher;
 import pokecube.core.database.spawns.SpawnCheck;
@@ -483,20 +481,6 @@ public class EventsHandler
                 SpawnBiomeMatcher m = new SpawnBiomeMatcher(entry.getValue().copy());
                 if (m.matches(check)) valid.add(entry.getKey());
             }
-
-            SpawnRule rule = SpawnBiomeMatcher.PRESETS.get("deserts").copy();
-            System.out.println(rule);
-            SpawnBiomeMatcher m = new SpawnBiomeMatcher(rule);
-            m.parse();
-            System.out.println(check.biome.location() + " " + BiomeDictionary.getTypes(check.biome) + " " + check);
-            System.out.println(m.getValidBiomes() + " " + m._validTypes);
-            System.out.println(m.matches(check));
-            
-            PokedexEntry cacnea = Database.getEntry("cacnea");
-            System.out.println(cacnea.getSpawnData().matchers.keySet().stream().findFirst().get().spawnRule);
-            System.out.println(cacnea.getSpawnData().matchers.keySet().stream().findFirst().get()._and_children);
-            System.out.println(cacnea.getSpawnData().matchers.keySet().stream().findFirst().get().matches(check));
-
             if (!valid.isEmpty())
             {
                 player.sendMessage(new TextComponent("Spawn Presets valid for here:"), player.getUUID());
