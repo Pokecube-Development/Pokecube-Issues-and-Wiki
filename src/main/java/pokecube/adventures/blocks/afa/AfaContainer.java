8f6d76e99cce0793514acf7de46c4576b95935a6
WenXin2 <dwnewville@gmail.com>
2022-01-23 11:31:12 -0600

    Move Textures (#871)
    * Rename texture directory
    
    * Rename `items` directory to `item`
    
    * Fix missing textures of slots
    
    * Revert "Fix missing textures of slots"
    
    This reverts commit bea555b08e9fe77135a3b2692388fb1640ab74d0.
    
    * Revert "Merge branch 'MoveTextures' of https://github.com/WenXin20/Pokecube-Issues-and-Wiki into MoveTextures"
    
    This reverts commit 8b8d368c5914a391f4f217ba31b6f8d1410eaa80, reversing
    changes made to ca7c476ec9779812533ff9bc80c6481fd51d553d.
    
    * Revert "Rename `items` directory to `item`"
    
    This reverts commit ca7c476ec9779812533ff9bc80c6481fd51d553d.
    
    * Rename `items` directory to `item`
    
    * Rename `blocks` directory to `block`
    
    * Update advancement
    
    * Fix missing textures of slots
    
    * Fix incorrect texture location
    
    * Fix some missing textures
    
    * Change one pixel

--- src/main/java/pokecube/adventures/blocks/afa/AfaContainer.java
+++ src/main/java/pokecube/adventures/blocks/afa/AfaContainer.java
@@ -60,10 +60,9 @@ public class AfaContainer extends BaseContainer
         final int dj = 36;
         final int i = 0;
         final int j = 0;
-
-        this.addSlot(new TexturedSlot(this.inv, 0, dj - 21 + j * 18, di + i * 18, "pokecube:items/slot_cube"));
+		
+        this.addSlot(new TexturedSlot(this.inv, 0, dj - 21 + j * 18, di + i * 18, "pokecube:gui/slot_cube"));
         this.bindPlayerInventory(invIn, -19);
-
         this.addDataSlots(this.tile.syncValues);
     }
 
--- src/main/java/pokecube/legends/fluids/DistorticWater.java
+++ src/main/java/pokecube/legends/fluids/DistorticWater.java
@@ -8,9 +8,9 @@ import pokecube.legends.init.ItemInit;
 
 public class DistorticWater
 {
-    public static final ResourceLocation DISTORTIC_WATER_STILL_TEXTURE = new ResourceLocation("pokecube_legends:blocks/distortic_water_still");
-    public static final ResourceLocation DISTORTIC_WATER_FLOWING_TEXTURE = new ResourceLocation("pokecube_legends:blocks/distortic_water_flow");
-    public static final ResourceLocation DISTORTIC_WATER_OVERLAY_TEXTURE = new ResourceLocation("pokecube_legends:blocks/distortic_water_overlay");
+    public static final ResourceLocation DISTORTIC_WATER_STILL_TEXTURE = new ResourceLocation("pokecube_legends:block/distortic_water_still");
+    public static final ResourceLocation DISTORTIC_WATER_FLOWING_TEXTURE = new ResourceLocation("pokecube_legends:block/distortic_water_flow");
+    public static final ResourceLocation DISTORTIC_WATER_OVERLAY_TEXTURE = new ResourceLocation("pokecube_legends:block/distortic_water_overlay");
 
     public static ForgeFlowingFluid.Properties makeProperties(){
         return new ForgeFlowingFluid.Properties(FluidInit.DISTORTED_WATER, FluidInit.DISTORTED_WATER_FLOWING,
