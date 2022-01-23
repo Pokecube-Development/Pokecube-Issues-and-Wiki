42c17e1eb234c04fc0fd553e35826dd181008d38
WenXin2 <dwnewville@gmail.com>
2022-01-22 19:36:14 -0600

    Fix bonemealing Azure Grass Blocks

--- src/main/java/pokecube/legends/init/FeaturesInit.java
+++ src/main/java/pokecube/legends/init/FeaturesInit.java
@@ -97,14 +97,6 @@ public class FeaturesInit
 
     public static final class Configs
     {
-//        public static final ConfiguredFeature<?, ?> PATCH_TAIGA_GRASS = FeatureUtils.register("patch_taiga_grass", Feature.RANDOM_PATCH
-//                .configured(grassPatch(new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder().add(Blocks.GRASS.defaultBlockState(), 1)
-//                        .add(Blocks.FERN.defaultBlockState(), 4)), 32)));
-//        public static final PlacedFeature GRASS_BONEMEAL = PlacementUtils.register("grass_bonemeal", VegetationFeatures.SINGLE_PIECE_OF_GRASS.onlyWhenEmpty());
-//        public static final ConfiguredFeature<SimpleBlockConfiguration, ?> SINGLE_PIECE_OF_GRASS = 
-//                FeatureUtils.register("single_piece_of_grass", Feature.SIMPLE_BLOCK
-//                        .configured(new SimpleBlockConfiguration(BlockStateProvider.simple(Blocks.GRASS.defaultBlockState()))));
-        
         public static final WeightedStateProvider FORBIDDEN_VEGETATION_PROVIDER = 
                 new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
                         .add(PlantsInit.GOLDEN_GRASS.get().defaultBlockState(), 1).add(PlantsInit.GOLDEN_FERN.get().defaultBlockState(), 4));
@@ -140,9 +132,6 @@ public class FeaturesInit
         
         public static final PlacedFeature PATCH_CORRUPTED_GRASS = PlacementUtils.register("tainted_barrens_bonemeal", 
                 FeaturesInit.Configs.TAINTED_VEGETATION_BONEMEAL.onlyWhenEmpty());
-        
-//        public static final PlacedFeature PATCH_DISTORTIC_GRASS = PlacementUtils.register("distorted_world_bonemeal", 
-//                FeaturesInit.Configs.DISTORTIC_GRASS_BONEMEAL);
     }
     
     static SimpleWeightedRandomList.Builder<BlockState> weightedBlockStateBuilder()
