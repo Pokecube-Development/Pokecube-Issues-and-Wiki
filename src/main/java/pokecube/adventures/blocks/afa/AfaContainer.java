d689ec22e6a4b4af9bc86f0f7980413fc2580e04
WenXin2 <dwnewville@gmail.com>
2022-01-22 21:51:27 -0600

    Revert "Fix missing textures of slots"
    This reverts commit bea555b08e9fe77135a3b2692388fb1640ab74d0.

--- src/main/java/pokecube/adventures/blocks/afa/AfaContainer.java
+++ src/main/java/pokecube/adventures/blocks/afa/AfaContainer.java
@@ -60,7 +60,7 @@ public class AfaContainer extends BaseContainer
         final int dj = 36;
         final int i = 0;
         final int j = 0;
-        this.addSlot(new TexturedSlot(this.inv, 0, dj - 21 + j * 18, di + i * 18, "pokecube:gui/slot_cube"));
+        this.addSlot(new TexturedSlot(this.inv, 0, dj - 21 + j * 18, di + i * 18, "pokecube:item/slot_cube"));
         this.bindPlayerInventory(invIn, -19);
 
         this.addDataSlots(this.tile.syncValues);
