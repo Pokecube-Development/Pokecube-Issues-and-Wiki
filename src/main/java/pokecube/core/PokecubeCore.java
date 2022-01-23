67c76ccb1ed64304613b6ba2db37e94fa67ed97a
Thutmose <Thutmose@users.noreply.github.com>
2022-01-23 13:24:23 -0500

    Update PokecubeCore.java
    forge finally fixed this, so removing from here

--- src/main/java/pokecube/core/PokecubeCore.java
+++ src/main/java/pokecube/core/PokecubeCore.java
@@ -30,7 +30,6 @@ import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.levelgen.GenerationStep;
@@ -432,30 +431,6 @@ public class PokecubeCore
             PointsOfInterest.postInit();
 
             BiomeDictionary.addTypes(SecretBaseDimension.BIOME_KEY, BiomeDictionary.Type.VOID);
-
-            
-            // FIXME remove this once forge does it itself.
-            BiomeDictionary.addTypes(Biomes.MEADOW, BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.PLATEAU,
-                    BiomeDictionary.Type.OVERWORLD);
-            BiomeDictionary.addTypes(Biomes.GROVE, BiomeDictionary.Type.COLD, BiomeDictionary.Type.CONIFEROUS,
-                    BiomeDictionary.Type.FOREST, BiomeDictionary.Type.SNOWY, BiomeDictionary.Type.MOUNTAIN,
-                    BiomeDictionary.Type.OVERWORLD);
-            BiomeDictionary.addTypes(Biomes.SNOWY_SLOPES, BiomeDictionary.Type.COLD, BiomeDictionary.Type.SPARSE,
-                    BiomeDictionary.Type.SNOWY, BiomeDictionary.Type.MOUNTAIN, BiomeDictionary.Type.OVERWORLD);
-            BiomeDictionary.addTypes(Biomes.JAGGED_PEAKS, BiomeDictionary.Type.COLD, BiomeDictionary.Type.SPARSE,
-                    BiomeDictionary.Type.SNOWY, BiomeDictionary.Type.MOUNTAIN, BiomeDictionary.Type.OVERWORLD);
-            BiomeDictionary.addTypes(Biomes.FROZEN_PEAKS, BiomeDictionary.Type.COLD, BiomeDictionary.Type.SPARSE,
-                    BiomeDictionary.Type.SNOWY, BiomeDictionary.Type.MOUNTAIN, BiomeDictionary.Type.OVERWORLD);
-            BiomeDictionary.addTypes(Biomes.STONY_PEAKS, BiomeDictionary.Type.HOT, BiomeDictionary.Type.MOUNTAIN,
-                    BiomeDictionary.Type.OVERWORLD);
-            BiomeDictionary.addTypes(Biomes.FROZEN_OCEAN, BiomeDictionary.Type.COLD);
-
-            BiomeDictionary.Type UNDERGROUND = BiomeDictionary.Type.getType("UNDERGROUND");
-
-            BiomeDictionary.addTypes(Biomes.LUSH_CAVES, UNDERGROUND, BiomeDictionary.Type.LUSH,
-                    BiomeDictionary.Type.WET, BiomeDictionary.Type.OVERWORLD);
-            BiomeDictionary.addTypes(Biomes.DRIPSTONE_CAVES, UNDERGROUND, BiomeDictionary.Type.SPARSE,
-                    BiomeDictionary.Type.OVERWORLD);
         });
     }
 }
