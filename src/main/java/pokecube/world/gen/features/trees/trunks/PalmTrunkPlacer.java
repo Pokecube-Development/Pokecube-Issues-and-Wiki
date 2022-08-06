03514663063123653b4e881f1a81872d7fd3abe5
WenXin2 <dwnewville@gmail.com>
2022-08-05 23:00:55 -0500

    Fixes

--- src/main/java/pokecube/world/gen/features/trees/trunks/PalmTrunkPlacer.java
+++ src/main/java/pokecube/world/gen/features/trees/trunks/PalmTrunkPlacer.java
@@ -1,8 +1,13 @@
 package pokecube.world.gen.features.trees.trunks;
 
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiConsumer;
+
 import com.google.common.collect.Lists;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.ExtraCodecs;
@@ -15,27 +20,23 @@ import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.TrunkPlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.TrunkPlacerType;
 
-import java.util.List;
-import java.util.Random;
-import java.util.function.BiConsumer;
-
 public class PalmTrunkPlacer extends TrunkPlacer
 {
-    private final int minHeightForLeaves;
-    private final IntProvider bendLength;
-    public static final Codec<PalmTrunkPlacer> CODEC = RecordCodecBuilder.create((type) ->
-    {
-        return trunkPlacerParts(type).and(type.group(ExtraCodecs.POSITIVE_INT.optionalFieldOf("min_height_for_leaves", 1)
-                .forGetter((get) ->
+    public static final Codec<PalmTrunkPlacer> CODEC = RecordCodecBuilder.create((instance) -> {
+        return trunkPlacerParts(instance).and(instance
+                .group(ExtraCodecs.POSITIVE_INT.optionalFieldOf("min_height_for_leaves", 1).forGetter((placer) ->
                 {
-                    return get.minHeightForLeaves;
-                }), IntProvider.codec(1, 64).fieldOf("bend_length").forGetter((get) ->
-        {
-            return get.bendLength;
-        }))).apply(type, PalmTrunkPlacer::new);
+                    return placer.minHeightForLeaves;
+                }), IntProvider.codec(1, 64).fieldOf("bend_length").forGetter((placer) -> {
+                    return placer.bendLength;
+                }))).apply(instance, PalmTrunkPlacer::new);
     });
 
-    public PalmTrunkPlacer(int baseHeight, int heightRandA, int heightRandB, int minHeightForLeaves, IntProvider bendLength)
+    private final int minHeightForLeaves;
+    private final IntProvider bendLength;
+
+    public PalmTrunkPlacer(int baseHeight, int heightRandA, int heightRandB, int minHeightForLeaves,
+                           IntProvider bendLength)
     {
         super(baseHeight, heightRandA, heightRandB);
         this.minHeightForLeaves = minHeightForLeaves;
@@ -49,9 +50,8 @@ public class PalmTrunkPlacer extends TrunkPlacer
     }
 
     @Override
-    public List<FoliagePlacer.FoliageAttachment> placeTrunk(LevelSimulatedReader level,
-            BiConsumer<BlockPos, BlockState> blockSetter, Random random, int freeTreeHeight, BlockPos pos,
-            TreeConfiguration config)
+    public List<FoliagePlacer.FoliageAttachment> placeTrunk(LevelSimulatedReader level, BiConsumer<BlockPos, BlockState> blockSetter,
+                                                            Random random, int freeTreeHeight, BlockPos pos, TreeConfiguration config)
     {
         Direction direction = Direction.Plane.HORIZONTAL.getRandomDirection(random);
         int i = freeTreeHeight - 1;
@@ -76,6 +76,7 @@ public class PalmTrunkPlacer extends TrunkPlacer
         }
 
         int l = this.bendLength.sample(random);
+
         for (int k = 0; k <= l; ++k)
         {
             if (TreeFeature.validTreePos(level, blockpos$mutableblockpos))
