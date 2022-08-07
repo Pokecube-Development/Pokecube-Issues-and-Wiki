1953b6f645a38cb4c78183568cbb61f5f3014b8a
Thutmose <Thutmose@users.noreply.github.com>
2022-08-06 23:55:25 -0400

    custom leaf placer for manually shaping leaves for trees

--- src/main/java/pokecube/world/gen/features/trees/foliage/FoliagePlacerTypes.java
+++ src/main/java/pokecube/world/gen/features/trees/foliage/FoliagePlacerTypes.java
@@ -6,10 +6,19 @@ import pokecube.world.PokecubeWorld;
 
 public class FoliagePlacerTypes
 {
-    public static final RegistryObject<FoliagePlacerType<PalmFoliagePlacer>> PALM_FOLIAGE_PLACER = PokecubeWorld.FOLIAGE_PLACERS
-            .register("palm_foliage_placer", () -> new FoliagePlacerType<>(PalmFoliagePlacer.CODEC));
-    public static final RegistryObject<FoliagePlacerType<PyramidFoliagePlacer>> PYRAMID_FOLIAGE_PLACER = PokecubeWorld.FOLIAGE_PLACERS
-            .register("pyramid_foliage_placer", () -> new FoliagePlacerType<>(PyramidFoliagePlacer.CODEC));
+    public static final RegistryObject<FoliagePlacerType<PalmFoliagePlacer>> PALM_FOLIAGE_PLACER;
+    public static final RegistryObject<FoliagePlacerType<PyramidFoliagePlacer>> PYRAMID_FOLIAGE_PLACER;
+    public static final RegistryObject<FoliagePlacerType<CustomShapeFoliagePlacer>> CUSTOM_SHAPE_FOLIAGE_PLACER;
+
+    static
+    {
+        PALM_FOLIAGE_PLACER = PokecubeWorld.FOLIAGE_PLACERS.register("palm_foliage_placer",
+                () -> new FoliagePlacerType<>(PalmFoliagePlacer.CODEC));
+        PYRAMID_FOLIAGE_PLACER = PokecubeWorld.FOLIAGE_PLACERS.register("pyramid_foliage_placer",
+                () -> new FoliagePlacerType<>(PyramidFoliagePlacer.CODEC));
+        CUSTOM_SHAPE_FOLIAGE_PLACER = PokecubeWorld.FOLIAGE_PLACERS.register("custom_shape_foliage_placer",
+                () -> new FoliagePlacerType<>(CustomShapeFoliagePlacer.CODEC));
+    }
 
     public static void init()
     {};
