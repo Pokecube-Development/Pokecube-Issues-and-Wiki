df31aa5a088a797812559df54f3ac81d9fe18bee
WenXin2 <dwnewville@gmail.com>
2022-08-07 00:24:36 -0500

    Remove the pyramid placer

--- src/main/java/pokecube/world/gen/features/trees/foliage/FoliagePlacerTypes.java
+++ src/main/java/pokecube/world/gen/features/trees/foliage/FoliagePlacerTypes.java
@@ -8,8 +8,6 @@ public class FoliagePlacerTypes
 {
     public static final RegistryObject<FoliagePlacerType<PalmFoliagePlacer>> PALM_FOLIAGE_PLACER = PokecubeWorld.FOLIAGE_PLACERS
             .register("palm_foliage_placer", () -> new FoliagePlacerType<>(PalmFoliagePlacer.CODEC));
-    public static final RegistryObject<FoliagePlacerType<PyramidFoliagePlacer>> PYRAMID_FOLIAGE_PLACER = PokecubeWorld.FOLIAGE_PLACERS
-            .register("pyramid_foliage_placer", () -> new FoliagePlacerType<>(PyramidFoliagePlacer.CODEC));
     public static final RegistryObject<FoliagePlacerType<RoundFoliagePlacer>> ROUND_FOLIAGE_PLACER = PokecubeWorld.FOLIAGE_PLACERS
             .register("round_foliage_placer", () -> new FoliagePlacerType<>(RoundFoliagePlacer.CODEC));
 
