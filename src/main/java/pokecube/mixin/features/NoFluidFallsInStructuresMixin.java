8dde0606227a072d4c1ed32358dc7bf35f740ade
Thutmose <Thutmose@users.noreply.github.com>
2022-08-25 21:25:03 -0400

    more adjustments to reduce flooding in team bases

---
+++ src/main/java/pokecube/mixin/features/NoFluidFallsInStructuresMixin.java
@@ -0,0 +1,39 @@
+package pokecube.mixin.features;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.StructureFeatureManager;
+import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.SpringFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.SpringConfiguration;
+import pokecube.world.WorldgenTags;
+
+@Mixin(SpringFeature.class)
+public class NoFluidFallsInStructuresMixin
+{
+
+    @Inject(method = "place(Lnet/minecraft/world/level/levelgen/feature/FeaturePlaceContext;)Z", at = @At(value = "HEAD"), cancellable = true)
+    private void repurposedstructures_noLavaInStructures(FeaturePlaceContext<SpringConfiguration> context,
+            CallbackInfoReturnable<Boolean> cir)
+    {
+        if (!(context.level() instanceof WorldGenRegionAccessor accessor)) return;
+        Registry<ConfiguredStructureFeature<?, ?>> configuredStructureFeatureRegistry = context.level().registryAccess()
+                .registryOrThrow(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY);
+        StructureFeatureManager structureFeatureManager = accessor.getStructureFeatureManager();
+        for (Holder<ConfiguredStructureFeature<?, ?>> configuredStructureFeature : configuredStructureFeatureRegistry
+                .getOrCreateTag(WorldgenTags.NO_FLUIDFALLS))
+        {
+            if (structureFeatureManager.getStructureAt(context.origin(), configuredStructureFeature.value()).isValid())
+            {
+                cir.setReturnValue(false);
+                return;
+            }
+        }
+    }
+}
