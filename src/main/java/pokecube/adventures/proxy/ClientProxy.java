bbd05754434925156938cdcfdbd262e1fbcefb05
Thutmose <Thutmose@users.noreply.github.com>
2022-08-31 18:02:56 -0400

    more work on json models for wearables
    moves the rendering into bling, for less duplicated code

--- src/main/java/pokecube/adventures/proxy/ClientProxy.java
+++ src/main/java/pokecube/adventures/proxy/ClientProxy.java
@@ -1,16 +1,10 @@
 package pokecube.adventures.proxy;
 
-import java.awt.Color;
-
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Vector3f;
 
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.item.DyeableLeatherItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -18,11 +12,9 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.NewRegistryEvent;
 import pokecube.adventures.PokecubeAdv;
-import thut.bling.client.render.Util;
+import thut.bling.client.render.Back;
 import thut.core.client.render.json.JsonModel;
-import thut.core.client.render.model.IExtendedModelPart;
 import thut.core.client.render.model.IModel;
-import thut.core.client.render.model.IModelCustom;
 import thut.wearables.EnumWearable;
 
 @OnlyIn(value = Dist.CLIENT)
@@ -42,45 +34,13 @@ public class ClientProxy extends CommonProxy
     { // We render layers based on material!
         static IModel bag;
 
-        // This is just a dummy texture for getting a fake initial renderer
-        private final ResourceLocation BAG_1 = new ResourceLocation(PokecubeAdv.MODID, "textures/hologram.png");
-
         @Override
         public void renderWearable(final PoseStack mat, final MultiBufferSource buff, final EnumWearable slot,
                 final int index, final LivingEntity wearer, final ItemStack stack, final float partialTicks,
                 final int brightness, final int overlay)
         {
             if (bag == null) bag = new JsonModel(new ResourceLocation(PokecubeAdv.MODID, "models/worn/bag.json"));
-
-            if (!(bag instanceof IModelCustom renderable)) return;
-            if (!bag.isLoaded() || !bag.isValid()) return;
-
-            float s;
-            mat.pushPose();
-            s = 1.0f;
-            mat.scale(s, -s, -s);
-            mat.mulPose(Vector3f.YP.rotationDegrees(180));
-            mat.translate(-0.5, -.6, -0.16);
-
-            Color colour;
-            if (stack.getItem() instanceof DyeableLeatherItem dyed)
-            {
-                colour = new Color(dyed.getColor(stack) + 0xFF000000);
-            }
-            else
-            {
-                colour = new Color(DyeColor.RED.getTextColor() + 0xFF000000);
-            }
-            for (final IExtendedModelPart part1 : bag.getParts().values())
-            {
-                // Overlay texture is the fixed one, the rest can be recoloured.
-                if (!part1.getMaterials().get(0).name.contains("overlay"))
-                    part1.setRGBABrO(colour.getRed(), colour.getGreen(), colour.getBlue(), 255, brightness, overlay);
-                else part1.setRGBABrO(255, 255, 255, 255, brightness, overlay);
-            }
-            final VertexConsumer buf1 = Util.makeBuilder(buff, BAG_1);
-            renderable.renderAll(mat, buf1);
-            mat.popPose();
+            Back.renderBack(mat, buff, wearer, stack, bag, null, brightness, overlay);
         }
     }
 
