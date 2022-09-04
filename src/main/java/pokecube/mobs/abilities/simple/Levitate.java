d318f64803796e280fcbf81c8cd5c56d969aaa64
Thutmose <Thutmose@users.noreply.github.com>
2022-09-04 16:20:58 -0400

    Update Levitate.java
    this should address #936

--- src/main/java/pokecube/mobs/abilities/simple/Levitate.java
+++ src/main/java/pokecube/mobs/abilities/simple/Levitate.java
@@ -10,14 +10,17 @@ public class Levitate extends Ability
     @Override
     public int beforeDamage(final IPokemob mob, final MovePacket move, final int damage)
     {
-        if (move.getMove().getType(move.attacker) == PokeType.getType("ground") && move.attacked == mob) return 0;
+        boolean weAreTarget = mob.getEntity() == move.attacked && mob.getAbility() == this;
+        if (weAreTarget && move.getMove().getType(move.attacker) == PokeType.getType("ground") && move.attacked == mob)
+            return 0;
         return super.beforeDamage(mob, move, damage);
     }
 
     @Override
     public void onMoveUse(final IPokemob mob, final MovePacket move)
     {
-        if (move.attacker == mob || !move.pre || move.attacker == move.attacked) return;
-        if (move.getMove().getType(move.attacker) == PokeType.getType("ground")) move.canceled = true;
+        if (!move.pre) return;
+        boolean weAreTarget = mob.getEntity() == move.attacked && mob.getAbility() == this;
+        if (weAreTarget && move.getMove().getType(move.attacker) == PokeType.getType("ground")) move.canceled = true;
     }
 }
