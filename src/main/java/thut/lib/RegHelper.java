12258b05235866d5a4b436d050b46fe7886d1168
Thutmose <Thutmose@users.noreply.github.com>
2022-09-12 13:11:34 -0400

    Update RegHelper.java
    fixes biome key lookup

--- src/main/java/thut/lib/RegHelper.java
+++ src/main/java/thut/lib/RegHelper.java
@@ -9,6 +9,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.ForgeRegistries.Keys;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 public class RegHelper
 {
@@ -34,7 +36,7 @@ public class RegHelper
 
     public static ResourceLocation getKey(Biome biome)
     {
-        return ForgeRegistries.BIOMES.getKey(biome);
+        return ServerLifecycleHooks.getCurrentServer().registryAccess().registryOrThrow(Keys.BIOMES).getKey(biome);
     }
 
     public static ResourceLocation getKey(ItemStack stack)
