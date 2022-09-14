cf3acfb77b57a9a8ac0abfe8cab28aae156c2b57
Thutmose <Thutmose@users.noreply.github.com>
2022-09-13 22:38:05 -0400

    better custom type support for #1016
    this moves the types jsons into data/<modid>/database/types. each file can register a number of types, and they are loaded in by priority of the file, smaller values first, and it will use the first type by the name that it sees!

--- src/main/java/pokecube/core/database/types/CombatTypeLoader.java
+++ src/main/java/pokecube/core/database/types/CombatTypeLoader.java
@@ -1,27 +1,30 @@
 package pokecube.core.database.types;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
 import pokecube.api.PokecubeAPI;
 import pokecube.api.utils.PokeType;
-import pokecube.core.PokecubeCore;
 import pokecube.core.database.resources.PackFinder;
 
 public class CombatTypeLoader
 {
-    public static class CombatTypes
+    public static class CombatTypes implements Comparable<CombatTypes>
     {
-        public JsonType[] types;
+        public int priority = 100;
+        public List<JsonType> types = new ArrayList<>();
 
         public void init()
         {
-            PokeType.typeTable = new float[this.types.length][this.types.length];
+            PokeType.typeTable = new float[this.types.size()][this.types.size()];
 
             // First add them all in as enums.
             for (final JsonType type2 : this.types)
@@ -32,13 +35,19 @@ public class CombatTypeLoader
                     PokeType.create(type.name, type.colour, type.name);
             }
 
-            for (int i = 0; i < this.types.length; i++)
+            for (int i = 0; i < this.types.size(); i++)
             {
-                final float[] arr = new float[this.types.length];
+                final float[] arr = new float[this.types.size()];
                 PokeType.typeTable[i] = arr;
-                final JsonType current = this.types[i];
-                for (int j = 0; j < this.types.length; j++) arr[j] = current.effect(this.types[j].name);
+                final JsonType current = this.types.get(i);
+                for (int j = 0; j < this.types.size(); j++) arr[j] = current.effect(this.types.get(j).name);
             }
+        }
+
+        @Override
+        public int compareTo(CombatTypes o)
+        {
+            return priority - o.priority;
         }
     }
 
@@ -67,24 +76,36 @@ public class CombatTypeLoader
         public float amount;
     }
 
-    public static ResourceLocation TYPES = new ResourceLocation(PokecubeCore.MODID, "database/types.json");
-
+    private static final String DATABASES = "database/types/";
     private static Gson gson = new Gson();
 
     public static void loadTypes()
     {
-        try
-        {
-            final BufferedReader reader = PackFinder.getReader(CombatTypeLoader.TYPES);
-            if (reader == null) throw new FileNotFoundException(CombatTypeLoader.TYPES.toString());
-            final CombatTypes types = CombatTypeLoader.gson.fromJson(reader, CombatTypes.class);
-            types.init();
-            reader.close();
-        }
-        catch (final Exception e)
-        {
-            PokecubeAPI.LOGGER.error("Error loading types.json", e);
-            throw new RuntimeException(e);
-        }
+
+        final Map<ResourceLocation, Resource> resources = PackFinder.getResources(CombatTypeLoader.DATABASES,
+                s -> s.endsWith(".json"));
+        List<CombatTypes> loaded = new ArrayList<>();
+        resources.forEach((s, r) -> {
+            try
+            {
+                BufferedReader reader = PackFinder.getReader(r);
+                CombatTypes types = CombatTypeLoader.gson.fromJson(reader, CombatTypes.class);
+                if (!types.types.isEmpty()) loaded.add(types);
+                reader.close();
+            }
+            catch (final Exception e1)
+            {
+                PokecubeAPI.LOGGER.error("Error with moves database " + s, e1);
+            }
+        });
+        loaded.sort(null);
+
+        loaded.forEach(t -> System.out.println(t.types));
+
+        CombatTypes compound = new CombatTypes();
+        Map<String, JsonType> typesMap = new HashMap<>();
+        loaded.forEach(l -> l.types.forEach(t -> typesMap.putIfAbsent(t.name, t)));
+        compound.types.addAll(typesMap.values());
+        compound.init();
     }
 }
