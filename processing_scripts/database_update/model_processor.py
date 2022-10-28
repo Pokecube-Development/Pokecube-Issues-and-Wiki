5f9da7eb42fc9d4c7850ef27386032da3f2b6c2f
Thutmose <Thutmose@users.noreply.github.com>
2022-10-28 16:54:47 -0400

    fixes for sinistea line

--- processing_scripts/database_update/model_processor.py
+++ processing_scripts/database_update/model_processor.py
@@ -1,14 +1,20 @@
 
-def _process_burmy(entry, key, model):
+def _process_tex_only(entry, key, model):
+    model['key'] = key
     model['model'] = entry.name
     model['anim'] = entry.name
-    model['key'] = key
     model['tex'] = key
 
+def _process_model_only(entry, key, model):
+    model['key'] = key
+    model['model'] = key
+    model['anim'] = entry.name
+    model['tex'] = entry.name
+
 def _process_no_custom(entry, key, model):
+    model['key'] = key
     model['model'] = entry.name
     model['anim'] = entry.name
-    model['key'] = key
     model['tex'] = entry.name
 
 def _process_arceus_silvally(entry, key, model):
@@ -20,14 +26,17 @@ def _process_arceus_silvally(entry, key, model):
 PROCESSORS = {
     'arceus': _process_arceus_silvally,
     'silvally': _process_arceus_silvally,
-    'burmy': _process_burmy,
+    'burmy': _process_tex_only,
     'genesect': _process_no_custom,
     'furfrou': _process_no_custom,
     'flabebe': _process_no_custom,
     'floette': _process_no_custom,
     'florges': _process_no_custom,
+    'sinistea': _process_model_only,
+    'polteageist': _process_model_only,
 }
 
 def process_model(entry, key, model):
     if entry.name in PROCESSORS:
         PROCESSORS[entry.name](entry, key, model)
+    return model
