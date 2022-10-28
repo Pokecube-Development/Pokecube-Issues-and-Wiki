b1778b39d12e97e10d7433b7628a2bf445d01925
Thutmose <Thutmose@users.noreply.github.com>
2022-10-28 16:02:24 -0400

    fixes genesect textures

--- processing_scripts/database_update/model_processor.py
+++ processing_scripts/database_update/model_processor.py
@@ -5,6 +5,11 @@ def _process_burmy(entry, key, model):
     model['key'] = key
     model['tex'] = key
 
+def _process_no_custom(entry, key, model):
+    model['model'] = entry.name
+    model['anim'] = entry.name
+    model['key'] = key
+    model['tex'] = entry.name
 
 def _process_arceus_silvally(entry, key, model):
     model['model'] = entry.name
@@ -16,6 +21,11 @@ PROCESSORS = {
     'arceus': _process_arceus_silvally,
     'silvally': _process_arceus_silvally,
     'burmy': _process_burmy,
+    'genesect': _process_no_custom,
+    'furfrou': _process_no_custom,
+    'flabebe': _process_no_custom,
+    'floette': _process_no_custom,
+    'florges': _process_no_custom,
 }
 
 def process_model(entry, key, model):
