f2ac6a8ea67f97c0e66eae6a5b2bf534c125101a
Thutmose <Thutmose@users.noreply.github.com>
2022-10-28 18:03:50 -0400

    Update RegistryChangeFixer.java

--- src/main/java/pokecube/core/legacy/RegistryChangeFixer.java
+++ src/main/java/pokecube/core/legacy/RegistryChangeFixer.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
