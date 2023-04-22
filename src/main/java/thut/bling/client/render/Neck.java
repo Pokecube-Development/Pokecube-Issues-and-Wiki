437bb1b420af9e9b6aaf0e31ee663e63754eb4bb
WenXin2 <dwnewville@gmail.com>
2023-04-22 01:07:23 -0500

    New Mega Pendant model

--- src/main/java/thut/bling/client/render/Neck.java
+++ src/main/java/thut/bling/client/render/Neck.java
@@ -7,21 +7,15 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import thut.core.client.render.model.IModel;
 import thut.core.client.render.model.parts.Material;
 
 public class Neck
 {
-    @OnlyIn(Dist.CLIENT)
-    public static Predicate<Material> IS_OVERLAY = m -> (m.name.contains("_overlay")
-            || m.tex != null && m.tex.getPath().contains("_overlay"));
-
     public static void renderNeck(final PoseStack mat, final MultiBufferSource buff, final LivingEntity wearer,
             final ItemStack stack, final IModel model, final int brightness, final int overlay)
     {
-        renderNeck(mat, buff, wearer, stack, model, brightness, overlay, IS_OVERLAY);
+        renderNeck(mat, buff, wearer, stack, model, brightness, overlay, material -> false);
     }
 
     public static void renderNeck(PoseStack mat, MultiBufferSource buff, LivingEntity wearer, ItemStack stack,
