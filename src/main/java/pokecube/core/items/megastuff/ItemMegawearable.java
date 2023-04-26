77371a9d093e9d00b88fffd097509f791f610481
WenXin2 <dwnewville@gmail.com>
2023-04-26 17:23:40 -0500

    Tweak default color to red

--- src/main/java/pokecube/core/items/megastuff/ItemMegawearable.java
+++ src/main/java/pokecube/core/items/megastuff/ItemMegawearable.java
@@ -67,7 +67,7 @@ public class ItemMegawearable extends Item implements DyeableLeatherItem
         if (compoundtag != null && compoundtag.contains("color", 99)) return compoundtag.getInt("color");
         if (name.equals("pendant")) return 0xFFFFFF;
         if (name.equals("tiara")) return 0x3c44aa;
-        if (name.equals("earring")) return 0xFED83D;
+        if (name.equals("earring")) return 0xB02E26;
         if (name.equals("ring")) return 0xFED83D;
         if (name.equals("glasses")) return 0x282828;
         if (name.equals("ankletzinnia")) return 0x169c9c;
