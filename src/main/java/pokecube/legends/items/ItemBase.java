61684f497a6c7003106722e1b21af9c820f7fa16
WenXin2 <dwnewville@gmail.com>
2023-04-28 16:33:07 -0500

    Fix tooltips

--- src/main/java/pokecube/legends/items/ItemBase.java
+++ src/main/java/pokecube/legends/items/ItemBase.java
@@ -71,7 +71,8 @@ public class ItemBase extends Item
     {
         if (!this.hasTooltip) return;
         String message;
-        if (Screen.hasShiftDown()) message = I18n.get("legends." + this.tooltip_block_id + ".tooltip");
+        if (Screen.hasShiftDown()) message = I18n.get("legends." + this.tooltip_block_id + ".tooltip", ChatFormatting.GOLD,
+                ChatFormatting.BOLD, ChatFormatting.RESET).translateEscapes();
         else message = I18n.get("pokecube.tooltip.advanced");
         tooltip.add(TComponent.translatable(message));
     }
