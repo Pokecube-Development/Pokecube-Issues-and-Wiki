eef671b23994e4925b05dea0407ad0bc61d58c8d
WenXin2 <dwnewville@gmail.com>
2023-09-02 22:15:33 -0500

    Update StatsInfo.java

--- src/main/java/pokecube/core/client/gui/watch/pokemob/StatsInfo.java
+++ src/main/java/pokecube/core/client/gui/watch/pokemob/StatsInfo.java
@@ -1,8 +1,6 @@
 package pokecube.core.client.gui.watch.pokemob;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.resources.ResourceLocation;
 import pokecube.api.entity.pokemob.IPokemob.Stats;
@@ -20,7 +18,7 @@ public class StatsInfo extends PokeInfoPage
     }
 
     // Default
-    private void drawBaseStats(final PoseStack mat, final int x, final int y)
+    private void drawBaseStats(final GuiGraphics graphics, final int x, final int y)
     {
         final int HP = this.parent.pokemob.getPokedexEntry().getStatHP();
         final int ATT = this.parent.pokemob.getPokedexEntry().getStatATT();
@@ -39,24 +37,24 @@ public class StatsInfo extends PokeInfoPage
         final String DS = I18n.get("pokewatch.DEFSP");
         final String S = I18n.get("pokewatch.VIT");
 
-        GuiComponent.drawString(mat, this.font, H, x + dx, statYOffSet + 18, 0xFF0000);
-        GuiComponent.drawString(mat, this.font, A, x + dx, statYOffSet + 27, 0xF08030);
-        GuiComponent.drawString(mat, this.font, D, x + dx, statYOffSet + 36, 0xF8D030);
-        GuiComponent.drawString(mat, this.font, AS, x + dx, statYOffSet + 45, 0x6890F0);
-        GuiComponent.drawString(mat, this.font, DS, x + dx, statYOffSet + 54, 0x78C850);
-        GuiComponent.drawString(mat, this.font, S, x + dx, statYOffSet + 63, 0xF85888);
+        graphics.drawString(this.font, H, x + dx, statYOffSet + 18, 0xFF0000);
+        graphics.drawString(this.font, A, x + dx, statYOffSet + 27, 0xF08030);
+        graphics.drawString(this.font, D, x + dx, statYOffSet + 36, 0xF8D030);
+        graphics.drawString(this.font, AS, x + dx, statYOffSet + 45, 0x6890F0);
+        graphics.drawString(this.font, DS, x + dx, statYOffSet + 54, 0x78C850);
+        graphics.drawString(this.font, S, x + dx, statYOffSet + 63, 0xF85888);
 
         dx = 60 + offsetX;
-        GuiComponent.drawString(mat, this.font, ": " + HP, x + dx, statYOffSet + 18, 0xFF0000);
-        GuiComponent.drawString(mat, this.font, ": " + ATT, x + dx, statYOffSet + 27, 0xF08030);
-        GuiComponent.drawString(mat, this.font, ": " + DEF, x + dx, statYOffSet + 36, 0xF8D030);
-        GuiComponent.drawString(mat, this.font, ": " + ATTSPE, x + dx, statYOffSet + 45, 0x6890F0);
-        GuiComponent.drawString(mat, this.font, ": " + DEFSPE, x + dx, statYOffSet + 54, 0x78C850);
-        GuiComponent.drawString(mat, this.font, ": " + VIT, x + dx, statYOffSet + 63, 0xF85888);
+        graphics.drawString(this.font, ": " + HP, x + dx, statYOffSet + 18, 0xFF0000);
+        graphics.drawString(this.font, ": " + ATT, x + dx, statYOffSet + 27, 0xF08030);
+        graphics.drawString(this.font, ": " + DEF, x + dx, statYOffSet + 36, 0xF8D030);
+        graphics.drawString(this.font, ": " + ATTSPE, x + dx, statYOffSet + 45, 0x6890F0);
+        graphics.drawString(this.font, ": " + DEFSPE, x + dx, statYOffSet + 54, 0x78C850);
+        graphics.drawString(this.font, ": " + VIT, x + dx, statYOffSet + 63, 0xF85888);
     }
 
     // Your Pokemob
-    private void drawInfo(final PoseStack mat, final int x, final int y)
+    private void drawInfo(final GuiGraphics graphics, final int x, final int y)
     {
         final byte[] nature = this.parent.pokemob.getNature().getStatsMod();
         int HP = this.parent.pokemob.getStat(Stats.HP, true);
@@ -90,7 +88,7 @@ public class StatsInfo extends PokeInfoPage
         for (int i = 0; i < nature.length; i++)
         {
             final int dy = 17 + i * 9;
-            GuiComponent.fill(mat, x + dx, statYOffSet + dy, x + dx + 107, statYOffSet + dy + 9, colours[i]);
+            graphics.fill(x + dx, statYOffSet + dy, x + dx + 107, statYOffSet + dy + 9, colours[i]);
         }
 
         final String H = I18n.get("pokewatch.HP");
@@ -102,21 +100,21 @@ public class StatsInfo extends PokeInfoPage
 
         final String Header = I18n.get("pokewatch.TVIVEV");
 
-        GuiComponent.drawString(mat, this.font, Header, 43 + x + dx, statYOffSet + 9, 0xFFFFFF);
-        GuiComponent.drawString(mat, this.font, H, x + dx, statYOffSet + 18, 0xFF0000);
-        GuiComponent.drawString(mat, this.font, A, x + dx, statYOffSet + 27, 0xF08030);
-        GuiComponent.drawString(mat, this.font, D, x + dx, statYOffSet + 36, 0xF8D030);
-        GuiComponent.drawString(mat, this.font, AS, x + dx, statYOffSet + 45, 0x6890F0);
-        GuiComponent.drawString(mat, this.font, DS, x + dx, statYOffSet + 54, 0x78C850);
-        GuiComponent.drawString(mat, this.font, S, x + dx, statYOffSet + 63, 0xF85888);
+        graphics.drawString(this.font, Header, 43 + x + dx, statYOffSet + 9, 0xFFFFFF);
+        graphics.drawString(this.font, H, x + dx, statYOffSet + 18, 0xFF0000);
+        graphics.drawString(this.font, A, x + dx, statYOffSet + 27, 0xF08030);
+        graphics.drawString(this.font, D, x + dx, statYOffSet + 36, 0xF8D030);
+        graphics.drawString(this.font, AS, x + dx, statYOffSet + 45, 0x6890F0);
+        graphics.drawString(this.font, DS, x + dx, statYOffSet + 54, 0x78C850);
+        graphics.drawString(this.font, S, x + dx, statYOffSet + 63, 0xF85888);
 
         dx = 56 + offsetX;
-        GuiComponent.drawString(mat, this.font, nat[0] + ": " + HP, x + dx, statYOffSet + 18, 0xFF0000);
-        GuiComponent.drawString(mat, this.font, nat[1] + ": " + ATT, x + dx, statYOffSet + 27, 0xF08030);
-        GuiComponent.drawString(mat, this.font, nat[2] + ": " + DEF, x + dx, statYOffSet + 36, 0xF8D030);
-        GuiComponent.drawString(mat, this.font, nat[3] + ": " + ATTSPE, x + dx, statYOffSet + 45, 0x6890F0);
-        GuiComponent.drawString(mat, this.font, nat[4] + ": " + DEFSPE, x + dx, statYOffSet + 54, 0x78C850);
-        GuiComponent.drawString(mat, this.font, nat[5] + ": " + VIT, x + dx, statYOffSet + 63, 0xF85888);
+        graphics.drawString(this.font, nat[0] + ": " + HP, x + dx, statYOffSet + 18, 0xFF0000);
+        graphics.drawString(this.font, nat[1] + ": " + ATT, x + dx, statYOffSet + 27, 0xF08030);
+        graphics.drawString(this.font, nat[2] + ": " + DEF, x + dx, statYOffSet + 36, 0xF8D030);
+        graphics.drawString(this.font, nat[3] + ": " + ATTSPE, x + dx, statYOffSet + 45, 0x6890F0);
+        graphics.drawString(this.font, nat[4] + ": " + DEFSPE, x + dx, statYOffSet + 54, 0x78C850);
+        graphics.drawString(this.font, nat[5] + ": " + VIT, x + dx, statYOffSet + 63, 0xF85888);
 
         dx = 20 + offsetX;
         byte[] stats2 = this.parent.pokemob.getIVs();
@@ -135,28 +133,28 @@ public class StatsInfo extends PokeInfoPage
         final int VIT2 = stats2[5] + 128;
 
         int shift = 88 + offsetX;
-        GuiComponent.drawString(mat, this.font, "" + HP, x + shift, statYOffSet + 18, 0xFF0000);
-        GuiComponent.drawString(mat, this.font, "" + ATT, x + shift, statYOffSet + 27, 0xF08030);
-        GuiComponent.drawString(mat, this.font, "" + DEF, x + shift, statYOffSet + 36, 0xF8D030);
-        GuiComponent.drawString(mat, this.font, "" + ATTSPE, x + shift, statYOffSet + 45, 0x6890F0);
-        GuiComponent.drawString(mat, this.font, "" + DEFSPE, x + shift, statYOffSet + 54, 0x78C850);
-        GuiComponent.drawString(mat, this.font, "" + VIT, x + shift, statYOffSet + 63, 0xF85888);
+        graphics.drawString(this.font, "" + HP, x + shift, statYOffSet + 18, 0xFF0000);
+        graphics.drawString(this.font, "" + ATT, x + shift, statYOffSet + 27, 0xF08030);
+        graphics.drawString(this.font, "" + DEF, x + shift, statYOffSet + 36, 0xF8D030);
+        graphics.drawString(this.font, "" + ATTSPE, x + shift, statYOffSet + 45, 0x6890F0);
+        graphics.drawString(this.font, "" + DEFSPE, x + shift, statYOffSet + 54, 0x78C850);
+        graphics.drawString(this.font, "" + VIT, x + shift, statYOffSet + 63, 0xF85888);
         shift += 21;
-        GuiComponent.drawString(mat, this.font, "" + HP2, x + shift, statYOffSet + 18, 0xFF0000);
-        GuiComponent.drawString(mat, this.font, "" + ATT2, x + shift, statYOffSet + 27, 0xF08030);
-        GuiComponent.drawString(mat, this.font, "" + DEF2, x + shift, statYOffSet + 36, 0xF8D030);
-        GuiComponent.drawString(mat, this.font, "" + ATTSPE2, x + shift, statYOffSet + 45, 0x6890F0);
-        GuiComponent.drawString(mat, this.font, "" + DEFSPE2, x + shift, statYOffSet + 54, 0x78C850);
-        GuiComponent.drawString(mat, this.font, "" + VIT2, x + shift, statYOffSet + 63, 0xF85888);
+        graphics.drawString(this.font, "" + HP2, x + shift, statYOffSet + 18, 0xFF0000);
+        graphics.drawString(this.font, "" + ATT2, x + shift, statYOffSet + 27, 0xF08030);
+        graphics.drawString(this.font, "" + DEF2, x + shift, statYOffSet + 36, 0xF8D030);
+        graphics.drawString(this.font, "" + ATTSPE2, x + shift, statYOffSet + 45, 0x6890F0);
+        graphics.drawString(this.font, "" + DEFSPE2, x + shift, statYOffSet + 54, 0x78C850);
+        graphics.drawString(this.font, "" + VIT2, x + shift, statYOffSet + 63, 0xF85888);
     }
 
     @Override
-    void drawInfo(final PoseStack mat, final int mouseX, final int mouseY, final float partialTicks)
+    void drawInfo(final GuiGraphics graphics, final int mouseX, final int mouseY, final float partialTicks)
     {
         final int x = (this.watch.width - GuiPokeWatch.GUIW) / 2;
         final int y = (this.watch.height - GuiPokeWatch.GUIH) / 2;
-        if (this.watch.canEdit(this.parent.pokemob)) this.drawInfo(mat, x, y);
-        else this.drawBaseStats(mat, x, y);
+        if (this.watch.canEdit(this.parent.pokemob)) this.drawInfo(graphics, x, y);
+        else this.drawBaseStats(graphics, x, y);
     }
 
 }
