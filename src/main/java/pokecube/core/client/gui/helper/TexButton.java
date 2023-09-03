0aa52e5ea5129ae3ddc91024c4b5672727d45b6c
WenXin2 <dwnewville@gmail.com>
2023-09-02 21:40:48 -0500

    Add TexButton Builder

--- src/main/java/pokecube/core/client/gui/helper/TexButton.java
+++ src/main/java/pokecube/core/client/gui/helper/TexButton.java
@@ -1,15 +1,22 @@
 package pokecube.core.client.gui.helper;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import javax.annotation.Nullable;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class TexButton extends Button
 {
@@ -125,7 +132,7 @@ public class TexButton extends Button
             RenderSystem.enableDepthTest();
             final int i = button.getTextureY();
 
-            //TODO: Check this
+            // TODO: Check this
             graphics.blit(new ResourceLocation(""), button.getX(), button.getY(), this.u, this.v + i * this.h, this.w, this.h);
         }
     }
@@ -141,18 +148,25 @@ public class TexButton extends Button
     IntFunc uEnd = IntFunc.DEFAULT;
 
     ImgRender render = new ImgRender()
-    {
-    };
+    {};
+    protected final TexButton.OnPress onPress;
+    protected final TexButton.CreateNarration createNarration;
+
+    public static TexButton.Builder builder(Component p_254439_, TexButton.OnPress p_254567_) {
+        return new TexButton.Builder(p_254439_, p_254567_);
+    }
 
     public TexButton(final int x, final int y, final int width, final int height, final Component title,
-            final OnPress pressedAction, Button.CreateNarration narration)
+            final OnPress pressedAction, TexButton.CreateNarration narration)
     {
-        super(x, y, width, height, title, pressedAction, narration);
+        super(x, y, width, height, title, (Button.OnPress) pressedAction, (Button.CreateNarration) narration);
+        this.onPress = pressedAction;
+        this.createNarration = narration;
     }
 
     public TexButton(Builder builder)
     {
-        this(builder.x, builder.y, builder.width, builder.height, builder.message, builder.onPress, builder.createNarration);
+        this(builder.x, builder.y, builder.width, builder.height, builder.name, builder.onPress, builder.createNarration);
         setTooltip(builder.tooltip);
     }
 
@@ -206,11 +220,114 @@ public class TexButton extends Button
             final String msg = this.getMessage().getString();
             final float dx = fontrenderer.width(msg) / 2f;
 
-            //TODO: Fix this
-            fontrenderer.draw(graphics, msg, this.x + this.getWidth() / 2 - dx, this.y + (this.getHeight() - 8) / 2,
-                    j | 255 << 24);
+            // TODO: Fix this
+            // fontrenderer.draw(graphics, msg, this.x + this.getWidth() / 2 - dx, this.y + (this.getHeight() - 8) / 2, j | 255 << 24);
         }
         // TODO: Check this
         if (this.isHoveredOrFocused()) this.renderWidget(graphics, mouseX, mouseY, partialTicks);
+    }
+
+    public void onPress() {
+        this.onPress.onPress(this);
+    }
+
+    protected MutableComponent createNarrationMessage() {
+        return this.createNarration.createNarrationMessage(() -> {
+            return super.createNarrationMessage();
+        });
+    }
+
+    public void updateWidgetNarration(NarrationElementOutput output) {
+        this.defaultButtonNarrationText(output);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static class Builder {
+        public final Component name;
+        public final TexButton.OnPress onPress;
+        @Nullable
+        public net.minecraft.client.gui.components.Tooltip tooltip;
+        public int x;
+        public int y;
+        public int width = 150;
+        public int height = 20;
+        boolean renderName = true;
+        public TexButton.CreateNarration createNarration;
+        public ResourceLocation texture = AbstractWidget.WIDGETS_LOCATION;
+        ImgRender render = new ImgRender()
+        {};
+
+        public Builder(Component name, TexButton.OnPress onPress) {
+            this.createNarration = (CreateNarration) TexButton.DEFAULT_NARRATION;
+            this.name = name;
+            this.onPress = onPress;
+        }
+
+        public TexButton.Builder pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+            return this;
+        }
+
+        public TexButton.Builder width(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public TexButton.Builder size(int width, int height) {
+            this.width = width;
+            this.height = height;
+            return this;
+        }
+
+        public TexButton.Builder bounds(int x, int y, int width, int height) {
+            return this.pos(x, y).size(width, height);
+        }
+
+        public TexButton.Builder tooltip(@Nullable net.minecraft.client.gui.components.Tooltip tooltip) {
+            this.tooltip = tooltip;
+            return this;
+        }
+
+        public TexButton.Builder createNarration(TexButton.CreateNarration narration) {
+            this.createNarration = narration;
+            return this;
+        }
+
+        public TexButton.Builder setTex(final ResourceLocation texture)
+        {
+            this.texture = texture;
+            return this;
+        }
+
+        public TexButton.Builder setRender(final ImgRender render)
+        {
+            this.render = render;
+            return this;
+        }
+
+        public TexButton.Builder noName()
+        {
+            this.renderName = false;
+            return this;
+        }
+
+        public TexButton build() {
+            return this.build(TexButton::new);
+        }
+
+        public TexButton build(Function<TexButton.Builder, TexButton> builder) {
+            return (TexButton)builder.apply(this);
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public interface OnPress {
+        void onPress(TexButton var1);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public interface CreateNarration {
+        MutableComponent createNarrationMessage(Supplier<MutableComponent> var1);
     }
 }
