b4560c5cb060e2557b2496049624873462ac0e20
WenXin2 <dwnewville@gmail.com>
2023-09-03 23:52:13 -0500

    Update GuiNBTButton.java

--- src/main/java/pokecube/nbtedit/gui/GuiNBTButton.java
+++ src/main/java/pokecube/nbtedit/gui/GuiNBTButton.java
@@ -1,12 +1,12 @@
 package pokecube.nbtedit.gui;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
 import pokecube.nbtedit.NBTStringHelper;
 import thut.lib.TComponent;
 
@@ -21,19 +21,20 @@ public class GuiNBTButton extends Button
 
     private long hoverTime;
 
-    public GuiNBTButton(final byte id, final int x, final int y, final OnPress onPress)
+    public GuiNBTButton(final byte id, final int x, final int y, final OnPress onPress, CreateNarration narration)
     {
-        super(x, y, GuiNBTButton.WIDTH, GuiNBTButton.HEIGHT, TComponent.translatable(""), onPress);
+        super(x, y, GuiNBTButton.WIDTH, GuiNBTButton.HEIGHT, TComponent.translatable(""), onPress, narration);
         this.id = id;
     }
 
-    public void drawToolTip(final PoseStack mat, final int mx, final int my)
+    public void drawToolTip(final GuiGraphics graphics, final int mx, final int my)
     {
         if (!(this.hoverTime != -1 && System.currentTimeMillis() - this.hoverTime > 300)) return;
         final String s = NBTStringHelper.getButtonName(this.id);
         final int width = this.mc.font.width(s);
-        GuiComponent.fill(mat, mx + 4, my + 7, mx + 5 + width, my + 17, 0xff000000);
-        this.mc.font.draw(mat, s, mx + 5, my + 8, 0xffffff);
+        graphics.fill( mx + 4, my + 7, mx + 5 + width, my + 17, 0xff000000);
+        // TODO: Fix this
+        // this.mc.font.draw(graphics, s, mx + 5, my + 8, 0xffffff);
     }
 
     public byte getId()
@@ -42,13 +43,13 @@ public class GuiNBTButton extends Button
     }
 
     @Override
-    public void renderButton(final PoseStack mat, final int mx, final int my, final float tick)
+    public void renderWidget(final GuiGraphics graphics, final int mx, final int my, final float tick)
     {
         // check if the mouse is over the button
         if (this.isHoveredOrFocused())
         {
             // Draw a background
-            GuiComponent.fill(mat, this.x, this.y, this.x + GuiNBTButton.WIDTH, this.y + GuiNBTButton.HEIGHT, 0x80ffffff);
+            graphics.fill(this.getX(), this.getY(), this.getX() + GuiNBTButton.WIDTH, this.getY() + GuiNBTButton.HEIGHT, 0x80ffffff);
             if (this.hoverTime == -1) this.hoverTime = System.currentTimeMillis();
         }
         else this.hoverTime = -1;
@@ -59,7 +60,8 @@ public class GuiNBTButton extends Button
             RenderSystem.setShader(GameRenderer::getPositionTexShader);
             RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
             RenderSystem.setShaderTexture(0, GuiNBTNode.WIDGET_TEXTURE);
-            this.blit(mat, this.x, this.y, (this.id - 1) * 9, 18, GuiNBTButton.WIDTH, GuiNBTButton.HEIGHT);
+            // TODO: Check this
+            graphics.blit(new ResourceLocation(""), this.getX(), this.getY(), (this.id - 1) * 9, 18, GuiNBTButton.WIDTH, GuiNBTButton.HEIGHT);
         }
     }
 }
