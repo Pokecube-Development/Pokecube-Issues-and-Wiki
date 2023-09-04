ccda6ed1d5e462b4237007c1de37ccf9e99a84b0
WenXin2 <dwnewville@gmail.com>
2023-09-04 01:27:52 -0500

    Update ConfigWearable.java

--- src/main/java/thut/wearables/impl/ConfigWearable.java
+++ src/main/java/thut/wearables/impl/ConfigWearable.java
@@ -1,10 +1,8 @@
 package thut.wearables.impl;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Quaternion;
-
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
@@ -13,6 +11,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -56,9 +55,9 @@ public class ConfigWearable implements IActiveWearable, ICapabilityProvider
 
     @OnlyIn(value = Dist.CLIENT)
     @Override
-    public void renderWearable(final PoseStack mat, final MultiBufferSource buff, final EnumWearable slot,
-            final int index, final LivingEntity wearer, final ItemStack stack, final float partialTicks,
-            final int brightness, final int overlay)
+    public void renderWearable(final GuiGraphics graphics, final MultiBufferSource buff, final EnumWearable slot,
+                               final int index, final LivingEntity wearer, final ItemStack stack, final float partialTicks,
+                               final int brightness, final int overlay)
     {
         // TODO way to register renderers for config wearables
 
@@ -66,9 +65,10 @@ public class ConfigWearable implements IActiveWearable, ICapabilityProvider
         if (stack.hasTag() && stack.getTag().contains("wslot"))
         {
 
-            mat.pushPose();
+            graphics.pose().pushPose();
 
-            mat.mulPose(new Quaternion(0, 0, 180, true));
+            // TODO: Fix this
+            // graphics.pose().mulPose(new Quaternionf(0, 0, 180, true));
 
             if (stack.getTag().contains("winfo"))
             {
@@ -76,50 +76,51 @@ public class ConfigWearable implements IActiveWearable, ICapabilityProvider
                 if (info.contains("scale"))
                 {
                     final float scale = info.getFloat("scale");
-                    mat.scale(scale, scale, scale);
+                    graphics.pose().scale(scale, scale, scale);
                 }
                 if (info.contains("shiftx"))
                 {
                     final float shift = info.getFloat("shiftx");
-                    mat.translate(shift, 0, 0);
+                    graphics.pose().translate(shift, 0, 0);
                 }
                 if (info.contains("shifty"))
                 {
                     final float shift = info.getFloat("shifty");
-                    mat.translate(0, shift, 0);
+                    graphics.pose().translate(0, shift, 0);
                 }
                 if (info.contains("shiftz"))
                 {
                     final float shift = info.getFloat("shiftz");
-                    mat.translate(0, 0, shift);
+                    graphics.pose().translate(0, 0, shift);
                 }
-                if (info.contains("rotx"))
-                {
-                    final float shift = info.getFloat("rotx");
-                    mat.mulPose(new Quaternion(shift, 0, 0, true));
-                }
-                if (info.contains("roty"))
-                {
-                    final float shift = info.getFloat("roty");
-                    mat.mulPose(new Quaternion(0, shift, 0, true));
-                }
-                if (info.contains("rotz"))
-                {
-                    final float shift = info.getFloat("rotz");
-                    mat.mulPose(new Quaternion(0, 0, shift, true));
-                }
+//                TODO: Fix this
+//                if (info.contains("rotx"))
+//                {
+//                    final float shift = info.getFloat("rotx");
+//                    graphics.pose().mulPose(new Quaternionf(shift, 0, 0, true));
+//                }
+//                if (info.contains("roty"))
+//                {
+//                    final float shift = info.getFloat("roty");
+//                    graphics.pose().mulPose(new Quaternionf(0, shift, 0, true));
+//                }
+//                if (info.contains("rotz"))
+//                {
+//                    final float shift = info.getFloat("rotz");
+//                    graphics.pose().mulPose(new Quaternionf(0, 0, shift, true));
+//                }
 
             }
 
-            mat.translate(-0.25f, 0, 0);
+            graphics.pose().translate(-0.25f, 0, 0);
             RenderSystem.setShader(GameRenderer::getPositionTexShader);
             RenderSystem.setShaderTexture(0, InventoryMenu.BLOCK_ATLAS);
 
             final ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
-            final BakedModel ibakedmodel = itemRenderer.getModel(stack, wearer.getLevel(), null, 0);
-            itemRenderer.render(stack, net.minecraft.client.renderer.block.model.ItemTransforms.TransformType.FIXED,
-                    true, mat, buff, 0, 0, ibakedmodel);
-            mat.popPose();
+            final BakedModel ibakedmodel = itemRenderer.getModel(stack, wearer.level(), null, 0);
+            itemRenderer.render(stack, ItemDisplayContext.FIXED,
+                    true, graphics.pose(), buff, 0, 0, ibakedmodel);
+            graphics.pose().popPose();
         }
 
     }
