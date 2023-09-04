8e829005d038fb305272d0ce918d206e8d337b73
WenXin2 <dwnewville@gmail.com>
2023-09-04 16:31:40 -0500

    Fix Trainer gui

--- src/main/java/pokecube/adventures/client/gui/trainer/editor/pages/Trainer.java
+++ src/main/java/pokecube/adventures/client/gui/trainer/editor/pages/Trainer.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Lists;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.Util;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.resources.language.I18n;
@@ -152,103 +153,103 @@ public class Trainer extends Page
             for (i = 0; i < this.parent.trainer.countPokemon(); i++)
             {
                 final int i2 = i;
-                this.addRenderableWidget(new Button(x + 20 + 50 * (i / 3), y - 10 + 20 * (i % 3), 50, 20,
-                        TComponent.literal("mob " + (i + 1)), b ->
-                        {
-                            this.parent.changePage(pokemobIndex);
-                            if (!(this.parent.current_page instanceof Pokemob page)) return;
-                            page.pokemob = PokecubeManager.itemToPokemob(this.parent.trainer.getPokemob(i2),
-                                    this.parent.entity.level());
-                            page.index = i2;
-                            page.deleteCallback = () -> {
-                                final PacketTrainer message = new PacketTrainer(PacketTrainer.UPDATEMOB);
-                                message.getTag().putInt("I", this.parent.entity.getId());
-                                message.getTag().putInt("__trainers__", page.index);
-                                PacketTrainer.ASSEMBLER.sendToServer(message);
-                                this.onClose();
-                                // This is needed to update that the pokemobs
-                                // have changed
-                                PacketTrainer.requestEdit(this.parent.entity);
-                            };
-                            page.closeCallback = () -> {
-                                this.parent.changePage(ourIndex);
-                            };
-                            // Re-call this to init the gui properly
-                            page.onPageOpened();
-                        }));
+
+                this.addRenderableWidget(new Button.Builder(TComponent.literal("mob " + (i + 1)), (b) -> {
+                    this.parent.changePage(pokemobIndex);
+                    if (!(this.parent.current_page instanceof Pokemob page)) return;
+                    page.pokemob = PokecubeManager.itemToPokemob(this.parent.trainer.getPokemob(i2),
+                            this.parent.entity.level());
+                    page.index = i2;
+                    page.deleteCallback = () -> {
+                        final PacketTrainer message = new PacketTrainer(PacketTrainer.UPDATEMOB);
+                        message.getTag().putInt("I", this.parent.entity.getId());
+                        message.getTag().putInt("__trainers__", page.index);
+                        PacketTrainer.ASSEMBLER.sendToServer(message);
+                        this.onClose();
+                        // This is needed to update that the pokemobs
+                        // have changed
+                        PacketTrainer.requestEdit(this.parent.entity);
+                    };
+                    page.closeCallback = () -> {
+                        this.parent.changePage(ourIndex);
+                    };
+                    // Re-call this to init the gui properly
+                    page.onPageOpened();
+                }).bounds(x + 20 + 50 * (i / 3), y - 10 + 20 * (i % 3), 50, 20).build());
             }
             if (this.parent.trainer.countPokemon() < 6)
             {
                 final int i2 = this.parent.trainer.countPokemon();
-                this.addRenderableWidget(new Button(x + 20 + 50 * (i2 / 3), y - 10 + 20 * (i2 % 3), 50, 20,
-                        TComponent.literal("mob +"), b ->
-                        {
-                            this.parent.changePage(pokemobIndex);
-                            if (!(this.parent.current_page instanceof Pokemob page)) return;
-                            page.index = i2;
-                            page.deleteCallback = () -> {
-                                this.parent.trainer.setPokemob(0, ItemStack.EMPTY);
-                                final PacketTrainer message = new PacketTrainer(PacketTrainer.UPDATEMOB);
-                                message.getTag().putInt("I", this.parent.entity.getId());
-                                message.getTag().putInt("__trainers__", page.index);
-                                PacketTrainer.ASSEMBLER.sendToServer(message);
-                                this.parent.changePage(ourIndex);
-                            };
-                            page.closeCallback = () -> {
-                                this.parent.changePage(ourIndex);
-                            };
-                            // Re-call this to init the gui properly
-                            page.onPageOpened();
-                        }));
+
+                this.addRenderableWidget(new Button.Builder(TComponent.literal("mob +"), (b) -> {
+                    this.parent.changePage(pokemobIndex);
+                    if (!(this.parent.current_page instanceof Pokemob page)) return;
+                    page.index = i2;
+                    page.deleteCallback = () -> {
+                        this.parent.trainer.setPokemob(0, ItemStack.EMPTY);
+                        final PacketTrainer message = new PacketTrainer(PacketTrainer.UPDATEMOB);
+                        message.getTag().putInt("I", this.parent.entity.getId());
+                        message.getTag().putInt("__trainers__", page.index);
+                        PacketTrainer.ASSEMBLER.sendToServer(message);
+                        this.parent.changePage(ourIndex);
+                    };
+                    page.closeCallback = () -> {
+                        this.parent.changePage(ourIndex);
+                    };
+                    // Re-call this to init the gui properly
+                    page.onPageOpened();
+                }).bounds(x + 20 + 50 * (i2 / 3), y - 10 + 20 * (i2 % 3), 50, 20).build());
             }
         }
 
-        this.addRenderableWidget(new Button(x - 9, y + dy - 1, 10, 10, TComponent.literal(">"), b -> {
+        this.addRenderableWidget(new Button.Builder(TComponent.literal(">"), (b) -> {
             this.index++;
             this.index = this.index % types.size();
             this.type.setValue(types.get(this.index));
             this.onUpdated();
+        }).bounds(x - 9, y + dy - 1, 10, 10).build());
 
-        }));
-        this.addRenderableWidget(new Button(x - 19, y + dy - 1, 10, 10, TComponent.literal("<"), b -> {
+        this.addRenderableWidget(new Button.Builder(TComponent.literal("<"), (b) -> {
             this.index--;
             if (this.index < 0) this.index = types.size() - 1;
             this.type.setValue(types.get(this.index));
             this.onUpdated();
-        }));
-        this.addRenderableWidget(new Button(x - 123, y + 55, 40, 20, TComponent.literal("Delete"), b -> {
+        }).bounds(x - 19, y + dy - 1, 10, 10).build());
+
+        this.addRenderableWidget(new Button.Builder(TComponent.translatable("traineredit.button.delete"), (b) -> {
             final PacketTrainer message = new PacketTrainer(PacketTrainer.KILLTRAINER);
             message.getTag().putInt("I", this.parent.entity.getId());
             PacketTrainer.ASSEMBLER.sendToServer(message);
             this.onClose();
-        }));
-        this.addRenderableWidget(
-                new Button(x - 19, y + dy + 9, 10, 10, TComponent.literal(this.male ? "\u2642" : "\u2640"), b ->
-                {
-                    this.male = !this.male;
-                    b.setMessage(TComponent.literal(this.male ? "\u2642" : "\u2640"));
-                    this.onUpdated();
-                }));
-        this.addRenderableWidget(
-                new Button(x + 60, y - 53, 60, 10, TComponent.literal(this.typename ? "Name Prefix" : "No Prefix"), b ->
-                {
-                    this.typename = !this.typename;
-                    b.setMessage(TComponent.literal(this.typename ? "Name Prefix" : "No Prefix"));
-                    this.onUpdated();
-                }));
-        this.addRenderableWidget(new Button(x + 80, y - 73, 40, 20, TComponent.literal("Apply"), b -> {
+        }).bounds(x - 123, y + 55, 40, 20).build());
+
+        this.addRenderableWidget(new Button.Builder(TComponent.literal(this.male ? "\u2642" : "\u2640"), (b) -> {
+            this.male = !this.male;
+            b.setMessage(TComponent.literal(this.male ? "\u2642" : "\u2640"));
             this.onUpdated();
-        }));
-        this.addRenderableWidget(new Button(x + 80, y + 55, 40, 20, TComponent.literal("Exit"), b -> {
+        }).bounds(x - 19, y + dy + 9, 10, 10).build());
+
+        this.addRenderableWidget(new Button.Builder(TComponent.literal(this.typename ? "Name Prefix" : "No Prefix"), (b) -> {
+            this.typename = !this.typename;
+            b.setMessage(TComponent.literal(this.typename ? "Name Prefix" : "No Prefix"));
+            this.onUpdated();
+        }).bounds(x + 60, y - 53, 60, 10).build());
+
+        this.addRenderableWidget(new Button.Builder(TComponent.translatable("traineredit.button.apply"), (b) -> {
+            this.onUpdated();
+        }).bounds(x + 80, y - 73, 40, 20).build());
+
+        this.addRenderableWidget(new Button.Builder(TComponent.translatable("traineredit.button.exit"), (b) -> {
             this.onUpdated();
             this.onClose();
-        }));
+        }).bounds(x + 80, y + 55, 40, 20).build());
 
         for (index = 0; index < EditorGui.PAGELIST.size(); index++)
             if (EditorGui.PAGELIST.get(index) == Rewards.class) break;
         final int rewardIndex = index;
         final int yOff = 15;
-        this.addRenderableWidget(new Button(x - 123, y + yOff, 60, 20, TComponent.literal("rewards"), b -> {
+
+        this.addRenderableWidget(new Button.Builder(TComponent.translatable("traineredit.button.rewards"), (b) -> {
             // Change to a rewards page
             this.parent.changePage(rewardIndex);
             if (!(this.parent.current_page instanceof Rewards page)) return;
@@ -256,11 +257,13 @@ public class Trainer extends Page
                 this.parent.changePage(ourIndex);
             };
             page.onPageOpened();
-        }));
+        }).bounds(x - 123, y + yOff, 60, 20).build());
+
         for (index = 0; index < EditorGui.PAGELIST.size(); index++)
             if (EditorGui.PAGELIST.get(index) == Messages.class) break;
         final int messIndex = index;
-        this.addRenderableWidget(new Button(x - 123, y + yOff - 20, 60, 20, TComponent.literal("messages"), b -> {
+
+        this.addRenderableWidget(new Button.Builder(TComponent.translatable("traineredit.button.messages"), (b) -> {
             // Change to a messages page
             // Change to a ai page
             this.parent.changePage(messIndex);
@@ -269,11 +272,13 @@ public class Trainer extends Page
                 this.parent.changePage(ourIndex);
             };
             page.onPageOpened();
-        }));
+        }).bounds(x - 123, y + yOff - 20, 60, 20).build());
+
         for (index = 0; index < EditorGui.PAGELIST.size(); index++)
             if (EditorGui.PAGELIST.get(index) == AI.class) break;
         final int aiIndex = index;
-        this.addRenderableWidget(new Button(x - 123, y + yOff + 20, 60, 20, TComponent.literal("ai"), b -> {
+
+        this.addRenderableWidget(new Button.Builder(TComponent.translatable("traineredit.button.ai"), (b) -> {
             // Change to a ai page
             this.parent.changePage(aiIndex);
             if (!(this.parent.current_page instanceof AI page)) return;
@@ -281,7 +286,7 @@ public class Trainer extends Page
                 this.parent.changePage(ourIndex);
             };
             page.onPageOpened();
-        }));
+        }).bounds(x - 123, y + yOff + 20, 60, 20).build());
     }
 
     private void onUpdated()
@@ -300,9 +305,9 @@ public class Trainer extends Page
     }
 
     @Override
-    public void render(final PoseStack matrixStack, final int mouseX, final int mouseY, final float partialTicks)
+    public void render(final GuiGraphics graphics, final int mouseX, final int mouseY, final float partialTicks)
     {
-        super.render(matrixStack, mouseX, mouseY, partialTicks);
+        super.render(graphics, mouseX, mouseY, partialTicks);
         final int x = this.parent.width / 2 - 18;
         final int y = this.parent.height / 2 - 72;
         final int dy = 10;
@@ -314,11 +319,12 @@ public class Trainer extends Page
             GuiPokemobHelper.renderMob(mob, x - 60, y + 80, 0, yaw, 0, yaw, 1f, partialTicks);
         }
 
-        this.font.draw(matrixStack, I18n.get("Trainer Type"), x + 20, y + dy * i++, 0xFFFFFFFF);
-        this.font.draw(matrixStack, I18n.get("Name"), x + 11, y + dy * i++, 0xFFFFFFFF);
-        this.font.draw(matrixStack, I18n.get("Trades List"), x, y + dy * i++, 0xFFFFFFFF);
-        this.font.draw(matrixStack, I18n.get("Player Texture"), x, y + dy * i++, 0xFFFFFFFF);
-        this.font.draw(matrixStack, I18n.get("Custom Texture"), x, y + dy * i++, 0xFFFFFFFF);
-        this.font.draw(matrixStack, I18n.get("Copied Mob"), x, y + dy * i++, 0xFFFFFFFF);
+//        TODO: Fix this
+//        this.font.draw(graphics, I18n.get("Trainer Type"), x + 20, y + dy * i++, 0xFFFFFFFF);
+//        this.font.draw(graphics, I18n.get("Name"), x + 11, y + dy * i++, 0xFFFFFFFF);
+//        this.font.draw(graphics, I18n.get("Trades List"), x, y + dy * i++, 0xFFFFFFFF);
+//        this.font.draw(graphics, I18n.get("Player Texture"), x, y + dy * i++, 0xFFFFFFFF);
+//        this.font.draw(graphics, I18n.get("Custom Texture"), x, y + dy * i++, 0xFFFFFFFF);
+//        this.font.draw(graphics, I18n.get("Copied Mob"), x, y + dy * i++, 0xFFFFFFFF);
     }
 }
