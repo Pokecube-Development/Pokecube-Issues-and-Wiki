e3ac2a12134336c1d2846509948f0c5ec1a63f15
WenXin2 <dwnewville@gmail.com>
2023-09-04 00:06:09 -0500

    Update TerrainChecker

--- src/main/java/thut/api/level/terrain/TerrainChecker.java
+++ src/main/java/thut/api/level/terrain/TerrainChecker.java
@@ -3,7 +3,6 @@ package thut.api.level.terrain;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Material;
 import thut.api.data.StringTag.StringValue;
 import thut.api.item.ItemList;
 import thut.api.util.JsonUtil;
@@ -20,16 +19,16 @@ public class TerrainChecker
 
     public static BiomeType INSIDE = BiomeType.getBiome("inside", true).setNoSave();
 
-    public static ResourceLocation CAVETAG = new ResourceLocation(ThutCore.MODID, "cave");
-    public static ResourceLocation FRUITTAG = new ResourceLocation(ThutCore.MODID, "fruit");
-    public static ResourceLocation GROUNDTAG = new ResourceLocation(ThutCore.MODID, "ground");
-    public static ResourceLocation INDUSTRIALTAG = new ResourceLocation(ThutCore.MODID, "industrial");
-    public static ResourceLocation PLANTEATTAG = new ResourceLocation(ThutCore.MODID, "plants_edible");
-    public static ResourceLocation PLANTCUTTAG = new ResourceLocation(ThutCore.MODID, "plants_cutable");
-    public static ResourceLocation ROCKTAG = new ResourceLocation(ThutCore.MODID, "rocks");
-    public static ResourceLocation SURFACETAG = new ResourceLocation(ThutCore.MODID, "surface");
-    public static ResourceLocation TERRAINTAG = new ResourceLocation(ThutCore.MODID, "terrain");
-    public static ResourceLocation WOODTAG = new ResourceLocation(ThutCore.MODID, "wood");
+    public static ResourceLocation CAVE_TAG = new ResourceLocation(ThutCore.MODID, "cave");
+    public static ResourceLocation FRUIT_TAG = new ResourceLocation(ThutCore.MODID, "fruit");
+    public static ResourceLocation GROUND_TAG = new ResourceLocation(ThutCore.MODID, "ground");
+    public static ResourceLocation INDUSTRIAL_TAG = new ResourceLocation(ThutCore.MODID, "industrial");
+    public static ResourceLocation PLANTS_EDIBLE_TAG = new ResourceLocation(ThutCore.MODID, "plants_edible");
+    public static ResourceLocation PLANTS_CUTABLE_TAG = new ResourceLocation(ThutCore.MODID, "plants_cutable");
+    public static ResourceLocation ROCK_TAG = new ResourceLocation(ThutCore.MODID, "rocks");
+    public static ResourceLocation SURFACE_TAG = new ResourceLocation(ThutCore.MODID, "surface");
+    public static ResourceLocation TERRAIN_TAG = new ResourceLocation(ThutCore.MODID, "terrain");
+    public static ResourceLocation WOOD_TAG = new ResourceLocation(ThutCore.MODID, "wood");
 
     public static ResourceLocation LEAVES = new ResourceLocation("minecraft:leaves");
     public static ResourceLocation FLOWERS = new ResourceLocation("minecraft:small_flowers");
@@ -58,60 +57,63 @@ public class TerrainChecker
 
     public static boolean isCave(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.CAVETAG, state);
+        return ItemList.is(TerrainChecker.CAVE_TAG, state);
     }
 
     public static boolean isGround(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.GROUNDTAG, state);
+        return ItemList.is(TerrainChecker.GROUND_TAG, state);
     }
 
     public static boolean isFruit(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.FRUITTAG, state);
+        return ItemList.is(TerrainChecker.FRUIT_TAG, state);
     }
 
     public static boolean isIndustrial(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.INDUSTRIALTAG, state);
+        return ItemList.is(TerrainChecker.INDUSTRIAL_TAG, state);
     }
 
-    private static boolean isPlant(final Material m)
-    {
-        return m == Material.PLANT || m == Material.REPLACEABLE_PLANT || m == Material.REPLACEABLE_WATER_PLANT
-                || m == Material.WATER_PLANT;
-    }
+//    TODO: Find replacement
+//    private static boolean isPlant(final Material m)
+//    {
+//        return m == Material.PLANT || m == Material.REPLACEABLE_PLANT || m == Material.REPLACEABLE_WATER_PLANT
+//                || m == Material.WATER_PLANT;
+//    }
 
     public static boolean isEdiblePlant(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.PLANTEATTAG, state)
-                || ThutCore.getConfig().autoPopulateLists && TerrainChecker.isPlant(state.getMaterial());
+        return ItemList.is(TerrainChecker.PLANTS_EDIBLE_TAG, state)
+                // TODO: Find replacement
+                /*|| ThutCore.getConfig().autoPopulateLists && TerrainChecker.isPlant(state.is(BlockTags.FLOWERS))*/;
     }
 
     public static boolean isCutablePlant(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.PLANTEATTAG, state) || ItemList.is(BlockTags.LEAVES.location(), state)
-                || ThutCore.getConfig().autoPopulateLists && TerrainChecker.isPlant(state.getMaterial());
+        return ItemList.is(TerrainChecker.PLANTS_CUTABLE_TAG, state) || ItemList.is(BlockTags.LEAVES.location(), state)
+                // TODO: Find replacement
+                /*|| ThutCore.getConfig().autoPopulateLists && TerrainChecker.isPlant(state.getMaterial())*/;
     }
 
     public static boolean isRock(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.ROCKTAG, state);
+        return ItemList.is(TerrainChecker.ROCK_TAG, state);
     }
 
     public static boolean isSurface(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.SURFACETAG, state);
+        return ItemList.is(TerrainChecker.SURFACE_TAG, state);
     }
 
     public static boolean isTerrain(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.TERRAINTAG, state);
+        return ItemList.is(TerrainChecker.TERRAIN_TAG, state);
     }
 
     public static boolean isWood(final BlockState state)
     {
-        return ItemList.is(TerrainChecker.WOODTAG, state);
+        return ItemList.is(TerrainChecker.WOOD_TAG, state);
     }
 
     public static boolean isLeaves(final BlockState state)
