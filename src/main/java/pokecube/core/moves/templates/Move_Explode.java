f8f740f53f194327745b37ab57a223a8762b3fce
WenXin2 <dwnewville@gmail.com>
2023-09-05 13:01:21 -0500

    Temp fix

--- src/main/java/pokecube/core/moves/templates/Move_Explode.java
+++ src/main/java/pokecube/core/moves/templates/Move_Explode.java
@@ -5,8 +5,14 @@ package pokecube.core.moves.templates;
 
 import java.util.BitSet;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
@@ -58,7 +64,9 @@ public class Move_Explode implements IMove
 
     }
 
-    public static final DamageSource SELFBOOM = new DamageSource("pokemob.exploded").setExplosion().bypassMagic();
+    // TODO: Fix .setExplosion().bypassMagic()
+    public static final ResourceKey<DamageType> SELF_BOOM = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(PokecubeCore.MODID, "pokemob.explosion"));
+//    public static final DamageSource SELF_BOOMS = new DamageSource((Holder<DamageType>) SELF_BOOM).is(DamageTypeTags.BYPASSES_ENCHANTMENTS).is(DamageTypeTags.IS_EXPLOSION).setExplosion().bypassMagic();
 
     public Move_Explode()
     {}
@@ -109,7 +117,7 @@ public class Move_Explode implements IMove
                 // First give it some health so it is alive
                 mob.setHealth(1);
                 // Now we kill the user via a damage source.
-                mob.hurt(Move_Explode.SELFBOOM, mob.getMaxHealth() * 1e5f);
+                mob.hurt(mob.level.damageSources().source(SELF_BOOM), mob.getMaxHealth() * 1e5f);
             }
         }
     };
