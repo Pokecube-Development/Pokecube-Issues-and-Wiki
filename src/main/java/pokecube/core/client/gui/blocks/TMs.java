5be9891b7cb0b6beb8fab0b85b813f8e8b93e02f
WenXin2 <dwnewville@gmail.com>
2023-09-18 17:24:48 -0500

    Tweaks

--- src/main/java/pokecube/core/client/gui/blocks/TMs.java
+++ src/main/java/pokecube/core/client/gui/blocks/TMs.java
@@ -9,7 +9,6 @@ import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import org.lwjgl.glfw.GLFW;
