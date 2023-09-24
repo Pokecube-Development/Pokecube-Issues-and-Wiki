8ddbbdf416e0eab96800fe6948b4ca08b0a00a0b
Thutmose <Thutmose@users.noreply.github.com>
2023-09-24 12:35:26 -0400

    adjustment to pokecube tossing

--- src/main/java/pokecube/core/items/pokecubes/Pokecube.java
+++ src/main/java/pokecube/core/items/pokecubes/Pokecube.java
@@ -366,7 +366,7 @@ public class Pokecube extends Item implements IPokecube
                     && this.getCaptureModifier(target, PokecubeItems.getCubeId(stack)) == 0)
                 target = null;
             boolean used = false;
-            final boolean filledOrSneak = filled || player.isShiftKeyDown() || dt > 10;
+            final boolean filledOrSneak = filled || player.isShiftKeyDown() || dt > 5;
             if (target != null && EntityPokecubeBase.SEEKING)
                 used = this.throwPokecubeAt(worldIn, player, stack, targetLocation, target) != null;
             else if (filledOrSneak || !EntityPokecubeBase.SEEKING)
