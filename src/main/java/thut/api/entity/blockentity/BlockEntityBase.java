b0789f3c51440defbff5fe9f5b79ddc92556dd42
prjohns <Thutmose@users.noreply.github.com>
2023-09-25 14:03:05 -0400

    some fixes for crafts

--- src/main/java/thut/api/entity/blockentity/BlockEntityBase.java
+++ src/main/java/thut/api/entity/blockentity/BlockEntityBase.java
@@ -19,6 +19,8 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
@@ -43,6 +45,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.entity.IEntityAdditionalSpawnData;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.network.NetworkHooks;
 import net.minecraftforge.network.PlayMessages.SpawnEntity;
 import thut.api.ThutCaps;
 import thut.api.entity.blockentity.block.TempBlock;
@@ -97,8 +100,7 @@ public abstract class BlockEntityBase extends Entity implements IEntityAdditiona
         DataSync data = DataSync_Impl.getData(event);
         if (data == null)
         {
-            data = new DataSync_Impl();
-            event.addCapability(DATASCAP, (DataSync_Impl) data);
+            event.addCapability(DATASCAP, new DataSync_Impl());
         }
     }
 
@@ -344,12 +346,11 @@ public abstract class BlockEntityBase extends Entity implements IEntityAdditiona
 
     abstract protected BlockEntityInteractHandler createInteractHandler();
 
-//    TODO: Find Replacement
-//    @Override
-//    public Packet<?> getAddEntityPacket()
-//    {
-//        return NetworkHooks.getEntitySpawningPacket(this);
-//    }
+    @Override
+    public Packet<ClientGamePacketListener> getAddEntityPacket()
+    {
+        return NetworkHooks.getEntitySpawningPacket(this);
+    }
 
     public final void doMotion()
     {
