076eb461101ac2f4ea48ae8211f47a991986f528
prjohns <Thutmose@users.noreply.github.com>
2023-10-02 11:39:25 -0400

    implements pokedex entry syncing on joining servers

--- src/main/java/thut/api/util/JsonUtil.java
+++ src/main/java/thut/api/util/JsonUtil.java
@@ -8,9 +8,12 @@ import com.google.gson.GsonBuilder;
 public class JsonUtil
 {
     public static final Gson gson;
+    public static final Gson smol_gson;
     static
     {
         gson = new GsonBuilder().registerTypeAdapter(QName.class, QNameAdaptor.INSTANCE).setPrettyPrinting()
+                .disableHtmlEscaping().setLenient().setExclusionStrategies(UnderscoreIgnore.INSTANCE).create();
+        smol_gson = new GsonBuilder().registerTypeAdapter(QName.class, QNameAdaptor.INSTANCE).setLenient()
                 .disableHtmlEscaping().setExclusionStrategies(UnderscoreIgnore.INSTANCE).create();
     }
 }
