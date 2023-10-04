a91e56579b54f3d901c76268cba58fb671b5feaf
WenXin2 <dwnewville@gmail.com>
2023-10-03 20:47:08 -0500

    Change empty bookshelves to chiseled

--- src/main/java/pokecube/core/blocks/bookshelves/GenericBookshelfEmptyTile.java
+++ src/main/java/pokecube/core/blocks/bookshelves/GenericBookshelfEmptyTile.java
@@ -1,5 +1,7 @@
 package pokecube.core.blocks.bookshelves;
 
+import com.mojang.logging.LogUtils;
+import java.util.Objects;
 import javax.annotation.Nullable;
 
 import net.minecraft.core.BlockPos;
@@ -10,6 +12,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -22,9 +25,12 @@ import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.ChiseledBookShelfBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.slf4j.Logger;
 import pokecube.core.PokecubeCore;
 import pokecube.core.PokecubeItems;
 import pokecube.core.handlers.ModTags;
@@ -33,19 +39,39 @@ import thut.lib.TComponent;
 
 public class GenericBookshelfEmptyTile extends RandomizableContainerBlockEntity implements WorldlyContainer
 {
-    public NonNullList<ItemStack> items = NonNullList.withSize(9, ItemStack.EMPTY);
+    public static final int MAX_BOOKS_IN_STORAGE = 12;
+    public NonNullList<ItemStack> items = NonNullList.withSize(12, ItemStack.EMPTY);
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private int lastInteractedSlot = -1;
     private Component name;
 
     private GenericBookshelfEmptyTile(final BlockEntityType<?> tileEntityType, final BlockPos pos,
                                       final BlockState state)
     {
         super(tileEntityType, pos, state);
-        this.items = NonNullList.withSize(9, ItemStack.EMPTY);
+        this.items = NonNullList.withSize(12, ItemStack.EMPTY);
     }
 
     public GenericBookshelfEmptyTile(final BlockPos pos, final BlockState state)
     {
         this(PokecubeItems.BOOKSHELF_EMPTY_TYPE.get(), pos, state);
+    }
+
+    private void updateState(int j) {
+        if (j >= 0 && j < MAX_BOOKS_IN_STORAGE) {
+            this.lastInteractedSlot = j;
+            BlockState blockstate = this.getBlockState();
+
+            for(int i = 0; i < GenericBookshelfEmpty.SLOT_OCCUPIED_PROPERTIES.size(); ++i) {
+                boolean flag = !this.getItem(i).isEmpty();
+                BooleanProperty booleanproperty = GenericBookshelfEmpty.SLOT_OCCUPIED_PROPERTIES.get(i);
+                blockstate = blockstate.setValue(booleanproperty, flag);
+            }
+
+            Objects.requireNonNull(this.level).setBlock(this.worldPosition, blockstate, 3);
+        } else {
+            LOGGER.error("Expected slot 0-11, got {}", (int)j);
+        }
     }
 
     @Override
@@ -54,6 +80,8 @@ public class GenericBookshelfEmptyTile extends RandomizableContainerBlockEntity 
         super.saveAdditional(saveCompoundNBT);
         if (!this.trySaveLootTable(saveCompoundNBT)) ContainerHelper.saveAllItems(saveCompoundNBT, this.items);
         if (this.name != null) saveCompoundNBT.putString("CustomName", Component.Serializer.toJson(this.name));
+        ContainerHelper.saveAllItems(saveCompoundNBT, this.items, true);
+        saveCompoundNBT.putInt("last_interacted_slot", this.lastInteractedSlot);
     }
 
     @Override
@@ -64,6 +92,9 @@ public class GenericBookshelfEmptyTile extends RandomizableContainerBlockEntity 
         if (!this.tryLoadLootTable(loadCompoundNBT)) ContainerHelper.loadAllItems(loadCompoundNBT, this.items);
         if (loadCompoundNBT.contains("CustomName", 8))
             this.name = Component.Serializer.fromJson(loadCompoundNBT.getString("CustomName"));
+        this.items.clear();
+        ContainerHelper.loadAllItems(loadCompoundNBT, this.items);
+        this.lastInteractedSlot = loadCompoundNBT.getInt("last_interacted_slot");
     }
 
     @Override
@@ -75,7 +106,7 @@ public class GenericBookshelfEmptyTile extends RandomizableContainerBlockEntity 
     @Override
     public int getContainerSize()
     {
-        return 9;
+        return MAX_BOOKS_IN_STORAGE;
     }
 
     public int addItem(ItemStack stack)
@@ -136,7 +167,7 @@ public class GenericBookshelfEmptyTile extends RandomizableContainerBlockEntity 
         }
         // place book
         else if (!playerHand.isEmpty() && this.canPlaceItem(number, playerHand) && hand == InteractionHand.MAIN_HAND
-                && number < 9)
+                && number < MAX_BOOKS_IN_STORAGE)
         {
             final ItemStack stack = playerHand.copy();
             stack.setCount(1);
@@ -157,8 +188,55 @@ public class GenericBookshelfEmptyTile extends RandomizableContainerBlockEntity 
     public boolean canPlaceItem(final int index, final ItemStack stack)
     {
         final Item book = stack.getItem();
-        return book instanceof BookItem || book instanceof EnchantedBookItem || stack.is(ItemTags.BOOKSHELF_BOOKS)
-                || stack.is(ModTags.BOOKS) || stack.is(ModTags.BOOKSHELF_ITEMS);
+        return (book instanceof BookItem || book instanceof EnchantedBookItem || stack.is(ItemTags.BOOKSHELF_BOOKS)
+                || stack.is(ModTags.BOOKS) || stack.is(ModTags.BOOKSHELF_ITEMS) && this.items.isEmpty());
+    }
+
+    public ItemStack getItem(int i) {
+        return this.items.get(i);
+    }
+
+    public ItemStack removeItem(int i, int j) {
+        ItemStack itemstack = Objects.requireNonNullElse(this.items.get(i), ItemStack.EMPTY);
+        this.items.set(i, ItemStack.EMPTY);
+        if (!itemstack.isEmpty()) {
+            this.updateState(i);
+        }
+
+        return itemstack;
+    }
+
+    public ItemStack removeItemNoUpdate(int i) {
+        return this.removeItem(i, 1);
+    }
+
+    public void setItem(int i, ItemStack stack)
+    {
+        final Item book = stack.getItem();
+//        if (book instanceof BookItem || book instanceof EnchantedBookItem || stack.is(ItemTags.BOOKSHELF_BOOKS)
+//                || stack.is(ModTags.BOOKS) || stack.is(ModTags.BOOKSHELF_ITEMS))
+//        {
+            this.items.set(i, stack);
+            this.updateState(i);
+//        }
+    }
+
+    public boolean canTakeItem(Container container, int i, ItemStack stack)
+    {
+        return container.hasAnyMatching((itemStack) ->
+        {
+            if (itemStack.isEmpty())
+            {
+                return true;
+            } else {
+                return ItemStack.isSameItemSameTags(stack, itemStack)
+                        && itemStack.getCount() + stack.getCount() <= Math.min(itemStack.getMaxStackSize(), container.getMaxStackSize());
+            }
+        });
+    }
+
+    public int getLastInteractedSlot() {
+        return this.lastInteractedSlot;
     }
 
     @Override
@@ -186,7 +264,7 @@ public class GenericBookshelfEmptyTile extends RandomizableContainerBlockEntity 
         {
             int number = 0;
             for (final ItemStack stack : this.getItems()) if (!stack.isEmpty()) number++;
-            if (this.lootTable != null) number = 9;
+            if (this.lootTable != null) number = MAX_BOOKS_IN_STORAGE;
             this.level.setBlock(this.getBlockPos(), this.getBlockState().setValue(GenericBookshelfEmpty.BOOKS, number),
                     3);
         }
