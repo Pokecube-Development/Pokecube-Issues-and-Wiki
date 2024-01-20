e2732c5b071ebf8665b20ac2bf4b24f3a775c5df
WenXin2 <dwnewville@gmail.com>
2024-01-20 02:07:07 -0600

    Add powered indicator

--- src/main/java/pokecube/core/blocks/repel/RepelBlock.java
+++ src/main/java/pokecube/core/blocks/repel/RepelBlock.java
@@ -1,20 +1,31 @@
 package pokecube.core.blocks.repel;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import pokecube.core.blocks.InteractableBlock;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import pokecube.core.blocks.InteractableHorizontalBlock;
 
 public class RepelBlock extends InteractableHorizontalBlock implements EntityBlock
 {
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
 
     public RepelBlock(final Properties properties)
     {
         super(properties);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(final StateDefinition.Builder<Block, BlockState> builder)
+    {
+        builder.add(FACING, POWERED);
     }
 
     @Override
@@ -34,11 +45,20 @@ public class RepelBlock extends InteractableHorizontalBlock implements EntityBlo
         {
             repel.enabled = false;
             repel.removeForbiddenSpawningCoord();
+            worldIn.setBlock(pos, state.setValue(POWERED, Boolean.FALSE), 3);
         }
         else
         {
             repel.enabled = true;
             repel.addForbiddenSpawningCoord();
+            worldIn.setBlock(pos, state.setValue(POWERED, Boolean.TRUE), 3);
         }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(final BlockPlaceContext context)
+    {
+        return this.defaultBlockState().setValue(POWERED, Boolean.TRUE)
+                .setValue(HorizontalDirectionalBlock.FACING, context.getHorizontalDirection().getOpposite());
     }
 }
