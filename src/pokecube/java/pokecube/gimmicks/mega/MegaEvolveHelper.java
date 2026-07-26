70fa6bd82f4c58a4bc24b567ecea98a8d07d19d2
Unoriginally <115102123+Unoriginally@users.noreply.github.com>
2026-07-26 14:50:30 +0100

    Made rayquaza learn dragon ascent instead of mega evolve when the mega evolve conditions for normal pokemobs are met

--- src/pokecube/java/pokecube/gimmicks/mega/MegaEvolveHelper.java
+++ src/pokecube/java/pokecube/gimmicks/mega/MegaEvolveHelper.java
@@ -22,8 +22,10 @@ import pokecube.api.entity.pokemob.commandhandlers.ChangeFormHandler;
 import pokecube.api.entity.pokemob.commandhandlers.ChangeFormHandler.IChangeHandler;
 import pokecube.api.events.pokemobs.ChangeForm;
 import pokecube.core.PokecubeCore;
+import pokecube.core.PokecubeItems;
 import pokecube.core.eventhandlers.PokemobEventsHandler.MegaEvoTicker;
 import pokecube.core.inventory.pc.PCContainer;
+import pokecube.core.items.ItemTM;
 import pokecube.core.items.megastuff.ItemMegawearable;
 import pokecube.gimmicks.mega.MegaCapability.MegaStone;
 import pokecube.gimmicks.mega.MegaCapability.MegaWearable;
@@ -132,14 +134,21 @@ public class MegaEvolveHelper
                         TComponent.translatable(newEntry.getUnlocalizedName()));
                 MegaEvolveHelper.megaEvolve(pokemob, newEntry, mess);
             }
-            else{
+            else {
                 // First, try transforming the item if it is a blank mega stone, if so, recall this function again.
                 var stack = pokemob.getHeldItem();
                 if (ItemList.is(BLANK_MEGASTONE, stack) && !stack.has(MEGA_STONE) && stack.getCount() == 1)
                 {
+                    if (pokemob.getDisplayName().getString().equalsIgnoreCase("rayquaza")) { // Rayquaza learns dragon ascent when mega evolve is attempted (Thanks for the idea Gliched!)
+                        ItemStack dragonAscentTM = new ItemStack(PokecubeItems.TM.get());
+                        ItemTM.addMoveToStack(dragonAscentTM, "dragon-ascent");
+                        if (ItemTM.feedToPokemob(dragonAscentTM, pokemob.getEntity())) pokemob.setHeldItem(ItemStack.EMPTY);
+                        return true;
+                    }
                     List<MegaEvoData.MegaRule> rules = MegaEvoData.RULES.getOrDefault(pokemob.getPokedexEntry(), Collections.emptyList());
                     if(!rules.isEmpty()&&pokemob.getHappiness()>250)
                     {
+                        PokecubeAPI.logInfo(pokemob.getDisplayName().getString());
                         Collections.shuffle(rules);
                         newEntry = rules.getFirst().getResult();
                         var stone = new MegaStone();
@@ -158,6 +167,7 @@ public class MegaEvolveHelper
                 }
                 thut.lib.ChatHelper.sendSystemMessage(player,
                         TComponent.translatable("pokemob.megaevolve.failed", pokemob.getDisplayName()));
+
             }
             return true;
         }
