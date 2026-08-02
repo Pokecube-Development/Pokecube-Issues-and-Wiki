19e3c88aa846f1787280c26dc26f2c49fc407d1b
Unoriginally <115102123+Unoriginally@users.noreply.github.com>
2026-08-02 21:18:01 +0100

    Moves RockHead data into its own class, fixes bug in which recoil moves are a death sentence

--- src/pokecube/java/pokecube/api/moves/utils/MoveApplication.java
+++ src/pokecube/java/pokecube/api/moves/utils/MoveApplication.java
@@ -396,29 +396,6 @@ public class MoveApplication implements Comparable<MoveApplication>
         RecoilApplier DEFAULT = new RecoilApplier()
         {};
 
-        RecoilApplier ROCKHEAD = new RecoilApplier() {
-            @Override
-            public void applyRecoil(Damage t) // Ignore negative recoil.
-            {
-                var moveAppl = t.move();
-                MoveEntry move = moveAppl.getMove();
-                int dealt = t.dealt;
-                float recoil = dealt * move.root_entry._drain / 100.0f;
-
-                IPokemob other = PokemobCaps.getPokemobFor(moveAppl.getTarget());
-                // This means the move heals as recoil.
-                if (recoil > 0)
-                {
-                    if (PokecubeCore.getConfig().debug_moves)
-                        PokecubeAPI.LOGGER.info("Applying recoil healing for move {} of amount {}", t.move().getName(),
-                                recoil);
-                    recoil = Math.min(recoil, moveAppl.getUser().getMaxHealth() - moveAppl.getUser().getHealth());
-                    if (recoil > 0) moveAppl.getUser().getEntity().heal(recoil);
-                    MovesUtils.sendPairedMessages(moveAppl.getUser().getEntity(), other, "pokemob.move.recoil.heal");
-                }
-            }
-        };
-
         default void applyRecoil(Damage t)
         {
             var moveAppl = t.move();
@@ -443,6 +420,7 @@ public class MoveApplication implements Comparable<MoveApplication>
                 else
                 {
                     Mob entity = moveAppl.getUser().getEntity();
+                    recoil = -Math.min(Math.abs(recoil), moveAppl.getUser().getMaxHealth() / 2.0f);
                     if (PokecubeCore.getConfig().debug_moves)
                         PokecubeAPI.LOGGER.info("Applying recoil damage for move {} of amount {}", t.move().getName(),
                                 recoil);
--- src/pokecube/java/pokecube/mobs/abilities/simple/RockHead.java
+++ src/pokecube/java/pokecube/mobs/abilities/simple/RockHead.java
@@ -1,17 +1,48 @@
 package pokecube.mobs.abilities.simple;
 
+import pokecube.api.PokecubeAPI;
 import pokecube.api.data.abilities.Ability;
 import pokecube.api.data.abilities.AbilityProvider;
 import pokecube.api.entity.pokemob.IPokemob;
+import pokecube.api.entity.pokemob.PokemobCaps;
+import pokecube.api.moves.MoveEntry;
 import pokecube.api.moves.utils.MoveApplication;
+import pokecube.core.PokecubeCore;
+import pokecube.core.moves.MovesUtils;
 
 @AbilityProvider(name = "rock-head")
 public class RockHead extends Ability
 {
+    public static MoveApplication.RecoilApplier ROCKHEAD = new MoveApplication.RecoilApplier() {
+        @Override
+        public void applyRecoil(MoveApplication.Damage t) // Ignore negative recoil.
+        {
+            if (PokecubeCore.getConfig().debug_moves)
+                PokecubeAPI.LOGGER.info("User of {} has rock head - ignoring recoil damage", t.move().getName());
+
+            var moveAppl = t.move();
+            MoveEntry move = moveAppl.getMove();
+            int dealt = t.dealt();
+            float recoil = dealt * move.root_entry._drain / 100.0f;
+
+            IPokemob other = PokemobCaps.getPokemobFor(moveAppl.getTarget());
+            // This means the move heals as recoil.
+            if (recoil > 0)
+            {
+                if (PokecubeCore.getConfig().debug_moves)
+                    PokecubeAPI.LOGGER.info("Applying recoil healing for move {} of amount {}", t.move().getName(),
+                            recoil);
+                recoil = Math.min(recoil, moveAppl.getUser().getMaxHealth() - moveAppl.getUser().getHealth());
+                if (recoil > 0) moveAppl.getUser().getEntity().heal(recoil);
+                MovesUtils.sendPairedMessages(moveAppl.getUser().getEntity(), other, "pokemob.move.recoil.heal");
+            }
+        }
+    };
+
     @Override
     public void preMoveUse(final IPokemob mob, final MoveApplication move)
     {
-        move.recoil = MoveApplication.RecoilApplier.ROCKHEAD;
+        move.recoil = ROCKHEAD;
     }
 
 }
