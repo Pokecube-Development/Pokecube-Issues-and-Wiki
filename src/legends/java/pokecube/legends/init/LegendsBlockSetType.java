c35baeab947ff5d6f230b073687204f39264f2f1
Thutmose <Thutmose@users.noreply.github.com>
2026-08-05 15:04:31 -0400

    fixes legends custom signs

--- src/legends/java/pokecube/legends/init/LegendsBlockSetType.java
+++ src/legends/java/pokecube/legends/init/LegendsBlockSetType.java
@@ -72,20 +72,20 @@ public class LegendsBlockSetType
                     SoundEvents.METAL_PRESSURE_PLATE_CLICK_OFF, SoundEvents.METAL_PRESSURE_PLATE_CLICK_ON,
                     SoundEvents.STONE_BUTTON_CLICK_OFF, SoundEvents.STONE_BUTTON_CLICK_ON));
 
-    public static final WoodType AGED_WOOD_TYPE = WoodType.register(new WoodType("aged", AGED_BLOCK_SET));
+    public static final WoodType AGED_WOOD_TYPE = WoodType.register(new WoodType("pokecube_legends:aged", AGED_BLOCK_SET));
     public static final WoodType CONCRETE_WOOD_TYPE = WoodType.register(
-            new WoodType("concrete", CONCRETE_BLOCK_SET, SoundType.STONE, SoundType.HANGING_SIGN,
+            new WoodType("pokecube_legends:concrete", CONCRETE_BLOCK_SET, SoundType.STONE, SoundType.HANGING_SIGN,
                     SoundEvents.FENCE_GATE_CLOSE, SoundEvents.FENCE_GATE_OPEN));
     public static final WoodType CONCRETE_DENSE_WOOD_TYPE = WoodType.register(
-            new WoodType("concrete_dense", CONCRETE_DENSE_BLOCK_SET, SoundType.STONE, SoundType.HANGING_SIGN,
+            new WoodType("pokecube_legends:concrete_dense", CONCRETE_DENSE_BLOCK_SET, SoundType.STONE, SoundType.HANGING_SIGN,
                     SoundEvents.FENCE_GATE_CLOSE, SoundEvents.FENCE_GATE_OPEN));
     public static final WoodType CORRUPTED_WOOD_TYPE = WoodType.register(
-            new WoodType("corrupted", CORRUPTED_BLOCK_SET, SoundType.NETHER_WOOD, SoundType.NETHER_WOOD_HANGING_SIGN,
+            new WoodType("pokecube_legends:corrupted", CORRUPTED_BLOCK_SET, SoundType.NETHER_WOOD, SoundType.NETHER_WOOD_HANGING_SIGN,
                     SoundEvents.NETHER_WOOD_FENCE_GATE_CLOSE, SoundEvents.NETHER_WOOD_FENCE_GATE_OPEN));
     public static final WoodType DISTORTIC_WOOD_TYPE = WoodType.register(
-            new WoodType("distortic", DISTORTIC_BLOCK_SET, SoundType.NETHER_WOOD, SoundType.NETHER_WOOD_HANGING_SIGN,
+            new WoodType("pokecube_legends:distortic", DISTORTIC_BLOCK_SET, SoundType.NETHER_WOOD, SoundType.NETHER_WOOD_HANGING_SIGN,
                     SoundEvents.NETHER_WOOD_FENCE_GATE_CLOSE, SoundEvents.NETHER_WOOD_FENCE_GATE_OPEN));
-    public static final WoodType INVERTED_WOOD_TYPE = WoodType.register(new WoodType("inverted", INVERTED_BLOCK_SET));
-    public static final WoodType MIRAGE_WOOD_TYPE = WoodType.register(new WoodType("mirage", MIRAGE_BLOCK_SET));
-    public static final WoodType TEMPORAL_WOOD_TYPE = WoodType.register(new WoodType("temporal", TEMPORAL_BLOCK_SET));
+    public static final WoodType INVERTED_WOOD_TYPE = WoodType.register(new WoodType("pokecube_legends:inverted", INVERTED_BLOCK_SET));
+    public static final WoodType MIRAGE_WOOD_TYPE = WoodType.register(new WoodType("pokecube_legends:mirage", MIRAGE_BLOCK_SET));
+    public static final WoodType TEMPORAL_WOOD_TYPE = WoodType.register(new WoodType("pokecube_legends:temporal", TEMPORAL_BLOCK_SET));
 }
