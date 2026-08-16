1dac0fb0c620862ba1e946b14af4a8c25d0af535
Thutmose <Thutmose@users.noreply.github.com>
2026-08-16 15:24:01 -0400

    re-ogranise these event handlers

--- src/pokecube/java/pokecube/gimmicks/secret_bases/SecretBases.java
+++ src/pokecube/java/pokecube/gimmicks/secret_bases/SecretBases.java
@@ -3,12 +3,15 @@ package pokecube.gimmicks.secret_bases;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.GlobalPos;
+import net.minecraft.core.SectionPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -16,6 +19,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.AABB;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -23,10 +27,14 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.entity.EntityEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.neoforge.event.tick.LevelTickEvent;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import pokecube.core.PokecubeCore;
 import pokecube.core.PokecubeItems;
+import pokecube.core.eventhandlers.EventsHandler;
 import pokecube.core.init.CoreCreativeTabs;
 import pokecube.core.network.packets.PacketPokedex;
 import pokecube.gimmicks.secret_bases.blocks.BaseBlock;
@@ -34,6 +42,8 @@ import pokecube.gimmicks.secret_bases.blocks.BaseTile;
 import pokecube.gimmicks.secret_bases.command.SecretBase;
 import pokecube.gimmicks.secret_bases.dimension.SecretBaseDimension;
 import thut.api.attachments.Ownable;
+import thut.api.entity.teleporting.TeleDest;
+import thut.api.entity.teleporting.ThutTeleporter;
 import thut.lib.RegHelper;
 
 import java.util.function.Supplier;
@@ -113,5 +123,111 @@ public class SecretBases
         {
             CoreCreativeTabs.addAfter(event, Items.LODESTONE, SECRET_BASE);
         }
+    }
+
+    @SubscribeEvent
+    public static void onWorldTick(final LevelTickEvent.Pre event)
+    {
+        final Level world = event.getLevel();
+        if (world.getWorldBorder().getSize() != SecretBaseDimension.WORLDSIZE
+                && world.dimension().compareTo(SecretBaseDimension.WORLD_KEY) == 0)
+            world.getWorldBorder().setSize(SecretBaseDimension.WORLDSIZE);
+    }
+
+    @SubscribeEvent
+    public static void onWorldLoad(final LevelEvent.Load event)
+    {
+        final Level world = (Level) event.getLevel();
+        if (world.getWorldBorder().getSize() != SecretBaseDimension.WORLDSIZE
+                && world.dimension().compareTo(SecretBaseDimension.WORLD_KEY) == 0)
+            world.getWorldBorder().setSize(SecretBaseDimension.WORLDSIZE);
+    }
+
+    @SubscribeEvent
+    public static void onEnterChunk(final EntityEvent.EnteringSection event)
+    {
+        final Level world = event.getEntity().level;
+        // Only wrap in secret bases, only if chunk changes, and only server
+        // side.
+        if (world.dimension() != SecretBaseDimension.WORLD_KEY || !event.didChunkChange() || world.isClientSide)
+            return;
+
+        SectionPos newPos = event.getNewPos();
+        SectionPos oldPos = event.getOldPos();
+
+        boolean moveX = oldPos.getX() != newPos.getX();
+        boolean moveZ = oldPos.getZ() != newPos.getZ();
+        if (!(moveX || moveZ)) return;
+
+        int x = newPos.getX() / 16;
+        int z = newPos.getZ() / 16;
+
+        final int dx = newPos.getX() % 16;
+        final int dz = newPos.getZ() % 16;
+
+        // Middle of base, don't care
+        if (dx == 0 && dz == 0) return;
+
+        if (dx > 0) if (dx < 8) x += 1;
+        if (dx < 0) if (dx < -7) x -= 1;
+
+        if (dz > 0) if (dz < 8) z += 1;
+        if (dz < 0) if (dz < -7) z -= 1;
+
+        final ChunkPos nearestBase = new ChunkPos(x << 4, z << 4);
+
+        // We need to shunt it back to nearest valid point.
+        final AABB chunkBox = getBaseBox(nearestBase);
+
+        final BlockPos mob = event.getEntity().blockPosition();
+
+        double nx = mob.getX();
+        double nz = mob.getZ();
+
+        if (moveX)
+        {
+            if (nx <= chunkBox.minX + 1)
+            {
+                nx = chunkBox.maxX - 1;
+                if (chunkBox.maxX < 0)
+                {
+                    nx -= 2;
+                }
+            }
+            else if (nx >= chunkBox.maxX - 1)
+            {
+                nx = chunkBox.minX + 1;
+            }
+        }
+        if (moveZ)
+        {
+            if (nz <= chunkBox.minZ + 1)
+            {
+                nz = chunkBox.maxZ - 1;
+                if (chunkBox.maxZ < 0)
+                {
+                    nz -= 2;
+                }
+            }
+            else if (nz >= chunkBox.maxZ - 1)
+            {
+                nz = chunkBox.minZ + 1;
+            }
+        }
+        final BlockPos pos = new BlockPos((int) nx, mob.getY(), (int) nz);
+
+        final TeleDest dest = new TeleDest().setPos(GlobalPos.of(world.dimension(), pos));
+        EventsHandler.Schedule(world, w -> {
+            event.getEntity().setDeltaMovement(0, 0, 0);
+            ThutTeleporter.transferTo(event.getEntity(), dest);
+            return true;
+        });
+    }
+
+    private static AABB getBaseBox(final ChunkPos nearestBase)
+    {
+        final BlockPos pos1 = nearestBase.getWorldPosition();
+        final BlockPos pos2 = pos1.offset(16, 255, 16);
+        return AABB.encapsulatingFullBlocks(pos1, pos2);
     }
 }
--- src/pokecube/java/pokecube/gimmicks/secret_bases/SecretBasesClient.java
+++ src/pokecube/java/pokecube/gimmicks/secret_bases/SecretBasesClient.java
@@ -1,11 +1,14 @@
 package pokecube.gimmicks.secret_bases;
 
+import net.minecraft.world.level.Level;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import pokecube.core.PokecubeCore;
 import pokecube.core.client.gui.watch.SecretBaseRadarPage;
+import pokecube.gimmicks.secret_bases.dimension.SecretBaseDimension;
 
 @EventBusSubscriber(modid = PokecubeCore.MODID, value = Dist.CLIENT)
 public class SecretBasesClient
@@ -15,5 +18,15 @@ public class SecretBasesClient
     {
         SecretBaseRadarPage.RADAR_MODES.put("_bases_",
                 SecretBaseRadarPage.DEFAULT = new SecretBaseRadarPage.RadarMode("base", "_bases_", 1));
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(final ClientTickEvent.Pre event)
+    {
+        final Level world = PokecubeCore.proxy.getWorld();
+        if (world == null) return;
+        if (world.getWorldBorder().getSize() != SecretBaseDimension.WORLDSIZE
+                && world.dimension().compareTo(SecretBaseDimension.WORLD_KEY) == 0)
+            world.getWorldBorder().setSize(SecretBaseDimension.WORLDSIZE);
     }
 }
