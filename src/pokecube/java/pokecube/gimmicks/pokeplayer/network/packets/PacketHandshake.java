b96b9f4d8de0c4a962c1141846fbbac5a24a0ec1
Unoriginally <115102123+Unoriginally@users.noreply.github.com>
2026-09-03 01:30:54 +0100

    Changes opening pokemob inventory as a pokeplayer to a config (fixes not being able to use it when looking at a block), first pass at fixing the pokeplayer syncing issues (#1313)

--- src/pokecube/java/pokecube/gimmicks/pokeplayer/network/packets/PacketHandshake.java
+++ src/pokecube/java/pokecube/gimmicks/pokeplayer/network/packets/PacketHandshake.java
@@ -5,36 +5,29 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import pokecube.api.PokecubeAPI;
-import pokecube.api.blocks.IHealer;
+import pokecube.api.entity.pokemob.IPokemob;
+import pokecube.api.entity.pokemob.PokemobCaps;
 import pokecube.core.PokecubeCore;
-import pokecube.core.ai.brain.BrainUtils;
 import thut.api.ThutCaps;
-import thut.api.entity.EntityProvider;
+import thut.api.attachments.TrackedAttachment;
 import thut.core.common.network.Packet;
 
-/// Very barebones packet, if you would like a more complex example see
+/// Very barebones packet, if you would like another example see
 /// PacketPokemobGui or PacketTMs in pokecube.core.network.
-/// More specifically, this packet is sent from client to server to allow
-/// pokeplayers to end all battles they are in.
-/// This is needed as using moves happens involuntarily in a battle
-/// and so could waste hunger.
-public class PacketBattleCancel extends Packet
+/// More specifically, this packet is sent from client to server to tell
+/// the server to check whether the CopyMob of the player has changed.
+public class PacketHandshake extends Packet
 {
     /// Empty constructor in this case.
-    public PacketBattleCancel()
+    public PacketHandshake()
     {}
 
-    /// Functions like these are good for implicit exception handling
-    public static void sendCancelPacket()
+    /// Functions like these are important for error checking.
+    /// Neoforge raises an exception if you have no exception handling.
+    public static void sendPacket()
     {
-        try {
-            PokecubeCore.packets.sendToServer(new PacketBattleCancel());
-        }
-        catch (Exception e) {
-            PokecubeAPI.logInfo("Pokeplayer cancelling Battle has failed! Check for key conflicts.");
-        }
+        PokecubeCore.packets.sendToServer(new PacketHandshake());
     }
 
 
@@ -44,15 +37,20 @@ public class PacketBattleCancel extends Packet
 
     @Override
     /// Handling for when the packet is received on the server.
-    /// Here, a method is called for cancelling all battles as a pokeplayer.
+    /// Here, we mark the pokemob and player as needing to be synced.
     public void handleServer(final ServerPlayer player)
     {
         var copy = ThutCaps.getCopyMob(player);
         if (copy == null) return;
-        LivingEntity pokemob = copy.getCopiedMob();
+        IPokemob pokemob = PokemobCaps.getPokemobFor(copy.getCopiedMob());
         if (pokemob == null) return;
 
-        BrainUtils.deagro(pokemob);
+        if (copy instanceof TrackedAttachment tracked)
+        {
+            tracked.markDirty();
+            player.getPersistentData().putBoolean("pokeplayer:needs_sync", true);
+            pokemob.markDirty();
+        }
     }
 
     @Override
@@ -61,7 +59,7 @@ public class PacketBattleCancel extends Packet
     {}
 
     /// Type of packet. No extra data required, just make a unique name (and make sure it starts with pokecube:, the game crashes otherwise)
-    private final static Type<Packet> TYPE = new Type<Packet>(ResourceLocation.parse("pokecube:battle_cancel"));
+    private final static Type<Packet> TYPE = new Type<Packet>(ResourceLocation.parse("pokecube:pokeplayer_handshake"));
 
     @Override
     public Type<? extends CustomPacketPayload> type()
