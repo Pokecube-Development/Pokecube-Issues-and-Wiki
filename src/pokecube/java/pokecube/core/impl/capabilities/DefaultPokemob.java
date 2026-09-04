070cd5b2e69f7edb93e356dd9b01be936a18d38b
Thutmose <Thutmose@users.noreply.github.com>
2026-09-04 11:20:45 -0400

    cleanup

--- src/pokecube/java/pokecube/core/impl/capabilities/DefaultPokemob.java
+++ src/pokecube/java/pokecube/core/impl/capabilities/DefaultPokemob.java
@@ -2,27 +2,11 @@ package pokecube.core.impl.capabilities;
 
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.animal.Sheep;
-import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import pokecube.api.PokecubeAPI;
-import pokecube.api.data.PokedexEntry.InteractionLogic.Interaction;
 import pokecube.api.entity.pokemob.IPokemob;
 import pokecube.api.entity.pokemob.ai.AIRoutine;
-import pokecube.api.entity.pokemob.ai.GeneralStates;
-import pokecube.api.utils.TagNames;
 import pokecube.core.impl.capabilities.impl.PokemobSexed;
-import thut.api.Tracker;
-import thut.api.attachments.Shearable;
-import thut.api.item.ItemList;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DefaultPokemob extends PokemobSexed implements IPokemob
 {
