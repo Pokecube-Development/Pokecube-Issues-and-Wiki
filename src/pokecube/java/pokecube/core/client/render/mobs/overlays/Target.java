8402fb29841799e6f5f707931dc5f3dd02741876
Thutmose <Thutmose@users.noreply.github.com>
2026-09-06 02:28:48 -0400

    adjustments to shader settings here

--- src/pokecube/java/pokecube/core/client/render/mobs/overlays/Target.java
+++ src/pokecube/java/pokecube/core/client/render/mobs/overlays/Target.java
@@ -27,7 +27,9 @@ public class Target
 {
     public static final Function<ResourceLocation, RenderType> TARGET_ICON_TYPE_NODEPTH = Util.memoize(texture -> {
         RenderType.CompositeState rendertype$compositestate = RenderType.CompositeState.builder()
-                .setShaderState(RenderType.RENDERTYPE_ENTITY_SOLID_SHADER).setDepthTestState(RenderType.NO_DEPTH_TEST)
+                .setShaderState(RenderType.RENDERTYPE_ENTITY_SOLID_SHADER)
+                .setDepthTestState(RenderType.NO_DEPTH_TEST)
+                .setTransparencyState(RenderType.TRANSLUCENT_TRANSPARENCY)
                 .setTextureState(new RenderStateShard.TextureStateShard(texture, false, false))
                 .setCullState(RenderType.NO_CULL).setLightmapState(RenderType.LIGHTMAP).createCompositeState(true);
         return RenderType.create("target_info_icon", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256,
