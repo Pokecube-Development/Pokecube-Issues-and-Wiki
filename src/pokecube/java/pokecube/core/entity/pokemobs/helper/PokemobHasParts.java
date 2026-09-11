e46fe00606d9b898a1859a7aa8131166d2f5c1c2
Thutmose <Thutmose@users.noreply.github.com>
2026-09-11 17:45:45 -0400

    fixes some NPEs when parts are not loaded yet

--- src/pokecube/java/pokecube/core/entity/pokemobs/helper/PokemobHasParts.java
+++ src/pokecube/java/pokecube/core/entity/pokemobs/helper/PokemobHasParts.java
@@ -216,6 +216,7 @@ public abstract class PokemobHasParts extends PokemobCombat implements IBBPartMu
     @Override
     public void setPose(Pose pose)
     {
+        if (this.getUseParts() == null) this.initParts();
         // NO-OP, we handle pose differently
         if (this.getUseParts().isEmpty()) super.setPose(pose);
     }
@@ -297,6 +298,7 @@ public abstract class PokemobHasParts extends PokemobCombat implements IBBPartMu
     @Override
     protected void pushEntities()
     {
+        if (this.getUseParts() == null) this.initParts();
         if (this.getUseParts().isEmpty()) super.pushEntities();
     }
 
@@ -304,6 +306,7 @@ public abstract class PokemobHasParts extends PokemobCombat implements IBBPartMu
     public void push(final Entity entityIn)
     {
         if (entityIn.is(this)) return;
+        if (this.getUseParts() == null) this.initParts();
         if (!this.getUseParts().isEmpty())
         {
             for (final PokemobPart part : this.getUseParts())
