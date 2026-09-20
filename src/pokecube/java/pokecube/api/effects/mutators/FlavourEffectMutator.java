8cdba983afac3d7a1c6ffa159617f3b4ba478641
Thutmose <Thutmose@users.noreply.github.com>
2026-09-20 17:33:25 -0400

    fixes several of these effects being too strong

--- src/pokecube/java/pokecube/api/effects/mutators/FlavourEffectMutator.java
+++ src/pokecube/java/pokecube/api/effects/mutators/FlavourEffectMutator.java
@@ -18,11 +18,7 @@ public class FlavourEffectMutator implements EffectMutator
         {
             int i = pokemob.getEntity().tickCount % 5;
             var flav = pokemob.getFlavourAmount(i);
-            if (flav > 0)
-            {
-                base.values.rgba = FLAVCOLOURS[i];
-                base.values.density = 0.1f / flav;
-            }
+            if (flav > 0) base.values.rgba = FLAVCOLOURS[i];
         }
     }
 
